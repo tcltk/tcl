e3e07eaf708446876b2ebb0374208e38c8d4a2de
rjohnson <rjohnson>
1999-03-13 23:49:48 +0000

    Improved Tcl Style.

--- library/init.tcl
+++ library/init.tcl
@@ -3,7 +3,7 @@
 # Default system startup file for Tcl-based applications.  Defines
 # "unknown" procedure and auto-load facilities.
 #
-# RCS: @(#) $Id: init.tcl,v 1.1.2.6 1999/02/10 23:31:20 stanton Exp $
+# RCS: @(#) $Id: init.tcl,v 1.1.2.7 1999/03/13 23:49:48 rjohnson Exp $
 #
 # Copyright (c) 1991-1993 The Regents of the University of California.
 # Copyright (c) 1994-1996 Sun Microsystems, Inc.
@@ -372,7 +372,8 @@ proc auto_load_index {} {
 }
 
 # auto_qualify --
-# compute a fully qualified names list for use in the auto_index array.
+#
+# Compute a fully qualified names list for use in the auto_index array.
 # For historical reasons, commands in the global namespace do not have leading
 # :: in the index key. The list has two elements when the command name is
 # relative (no leading ::) and the namespace is not the global one. Otherwise
@@ -431,7 +432,8 @@ proc auto_qualify {cmd namespace} {
 }
 
 # auto_import --
-# invoked during "namespace import" to make see if the imported commands
+#
+# Invoked during "namespace import" to make see if the imported commands
 # reside in an autoloaded library.  If so, the commands are loaded so
 # that they will be available for the import links.  If not, then this
 # procedure does nothing.
