53116ea217fe857a107d1c8f7087a3f0df4feaa3
jingham <jingham>
1999-03-24 05:21:07 +0000

    Reverting Serial port changes.  This will be implemented as an extension instead of using special cookies in the open command.

--- mac/tclMacChan.c
+++ mac/tclMacChan.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclMacChan.c,v 1.3.2.1 1999/03/22 05:42:19 jingham Exp $
+ * RCS: @(#) $Id: tclMacChan.c,v 1.3.2.2 1999/03/24 05:21:16 jingham Exp $
  */
 
 #include "tclInt.h"
@@ -91,7 +91,8 @@ typedef struct FileEvent {
  * This is defined in tclMacSerial.c.
  */
 
-EXTERN Tcl_Channel TclMacOpenSerialChannel _ANSI_ARGS_((char *fileName, int *errorCode));
+EXTERN Tcl_Channel TclMacOpenSerialChannel _ANSI_ARGS_((Tcl_Interp *interp,
+	char *fileName, int *errorCode));
 
 /*
  * Static routines for this file:
@@ -777,16 +778,13 @@ TclpOpenFileChannel(
      * Look for the magic cookies that refer to the modem ports.
      */
     
-    chan = TclMacOpenSerialChannel(fileName, &errorCode);
-    if (chan == NULL) {
-        nativeName = Tcl_TranslateFileName(interp, fileName, &buffer);
-        if (nativeName == NULL) {
-	    return NULL;
-        }
-
-        chan = OpenFileChannel(nativeName, mode, permissions, &errorCode);
-        Tcl_DStringFree(&buffer);
+    nativeName = Tcl_TranslateFileName(interp, fileName, &buffer);
+    if (nativeName == NULL) {
+	return NULL;
     }
+
+    chan = OpenFileChannel(nativeName, mode, permissions, &errorCode);
+    Tcl_DStringFree(&buffer);
     
     if (chan == NULL) {
 	Tcl_SetErrno(errorCode);
