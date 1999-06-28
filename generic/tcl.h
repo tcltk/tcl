c258b333c62e72fc2f07289988d0989a8742f6df
wart <wart>
1999-06-28 19:02:52 +0000

    Documented 2 more files that need to be updated when the Tcl version number changes.

--- generic/tcl.h
+++ generic/tcl.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.49 1999/06/26 00:01:47 redman Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.50 1999/06/28 19:02:52 wart Exp $
  */
 
 #ifndef _TCL
@@ -49,6 +49,8 @@ extern "C" {
  * win/README.binary
  * win/README		(only if major.minor changes, not patchlevel)
  * unix/README		(only if major.minor changes, not patchlevel)
+ * tools/tcl.hpj.in	(for windows installer)
+ * tools/tcl.wse.in	(for windows installer)
  */
 
 #define TCL_MAJOR_VERSION   8
