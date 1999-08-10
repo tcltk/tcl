7d7cec255a20bb85fda8ff499fd6b1b621c277af
redman <redman>
1999-08-10 18:50:09 +0000

    Update version numbers to 8.2b3 and update ChangeLog and changes files.

--- generic/tcl.h
+++ generic/tcl.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.56 1999/08/02 17:45:36 redman Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.56.2.1 1999/08/10 18:50:14 redman Exp $
  */
 
 #ifndef _TCL
@@ -56,10 +56,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   2
 #define TCL_RELEASE_LEVEL   TCL_BETA_RELEASE
-#define TCL_RELEASE_SERIAL  2
+#define TCL_RELEASE_SERIAL  3
 
 #define TCL_VERSION	    "8.2"
-#define TCL_PATCH_LEVEL	    "8.2b2"
+#define TCL_PATCH_LEVEL	    "8.2b3"
 
 /*
  * The following definitions set up the proper options for Windows
