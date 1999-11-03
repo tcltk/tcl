72aebeb0e0a8853e2c1417c5c2d1b732ee6afd6d
hobbs <hobbs>
1999-11-03 02:00:43 +0000

    * library/dde1.1/pkgIndex.tcl: 	* library/reg1.0/pkgIndex.tcl: fixed packaging to use the debug 	versions of the libraries when compiled with symbols [Bug: 3254]

--- library/dde/pkgIndex.tcl
+++ library/dde/pkgIndex.tcl
@@ -1,1 +1,5 @@
-package ifneeded dde 1.1 "load [list [file join $dir tcldde82.dll]] dde"
+if {[info exists tcl_platform(debug)]} {
+    package ifneeded dde 1.1 [list load [file join $dir tcldde82d.dll] dde]
+} else {
+    package ifneeded dde 1.1 [list load [file join $dir tcldde82.dll] dde]
+}
--- library/reg1.0/pkgIndex.tcl
+++ library/reg1.0/pkgIndex.tcl
@@ -1,1 +1,7 @@
-package ifneeded registry 1.0 "load [list [file join $dir tclreg82.dll]] registry"
+if {[info exists tcl_platform(debug)]} {
+    package ifneeded registry 1.0 \
+	    [list load [file join $dir tclreg82d.dll] registry]
+} else {
+    package ifneeded registry 1.0 \
+	    [list load [file join $dir tclreg82.dll] registry]
+}
