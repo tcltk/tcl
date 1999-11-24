b0146405ee44708ec4566bd59d2caef255bb316b
wart <wart>
1999-11-24 01:52:29 +0000

    Changed version number on dlls

--- library/dde1.1/pkgIndex.tcl
+++ library/dde1.1/pkgIndex.tcl
@@ -1,1 +1,1 @@
-package ifneeded dde 1.1 "load [list [file join $dir tcldde82.dll]] dde"
+package ifneeded dde 1.1 "load [list [file join $dir tcldde83.dll]] dde"
--- library/reg/pkgIndex.tcl
+++ library/reg/pkgIndex.tcl
@@ -1,1 +1,1 @@
-package ifneeded registry 1.0 "load [list [file join $dir tclreg82.dll]] registry"
+package ifneeded registry 1.0 "load [list [file join $dir tclreg83.dll]] registry"
