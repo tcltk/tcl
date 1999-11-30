d7393c5ccae5443faac4b81275778462c714ca12
hobbs <hobbs>
1999-11-30 08:37:02 +0000

    * library/dde1.1/pkgIndex.tcl: 	* library/reg1.0/pkgIndex.tcl: added supported for debugged 	versions of the libraries

--- library/dde/pkgIndex.tcl
+++ library/dde/pkgIndex.tcl
@@ -1,1 +1,5 @@
-package ifneeded dde 1.1 "load [list [file join $dir tcldde83.dll]] dde"
+if {[info exists tcl_platform(debug)]} {
+    package ifneeded dde 1.1 [list load [file join $dir tcldde83d.dll] dde]
+} else {
+    package ifneeded dde 1.1 [list load [file join $dir tcldde83.dll] dde]
+}
--- library/reg/pkgIndex.tcl
+++ library/reg/pkgIndex.tcl
@@ -1,1 +1,7 @@
-package ifneeded registry 1.0 "load [list [file join $dir tclreg83.dll]] registry"
+if {[info exists tcl_platform(debug)]} {
+    package ifneeded registry 1.0 \
+            [list load [file join $dir tclreg83d.dll] registry]
+} else {
+    package ifneeded registry 1.0 \
+            [list load [file join $dir tclreg83.dll] registry]
+}
