25cce6507c11e03f4462de76b675f6365ce9981d
ericm <ericm>
2000-02-07 19:15:25 +0000

    Removing struct namespace stuff.

--- library/struct1.0/struct.tcl
+++
@@ -1,8 +0,0 @@
-package provide struct 1.0
-source [file join [file dirname [info script]] stack.tcl]
-source [file join [file dirname [info script]] queue.tcl]
-namespace eval struct {
-	namespace export *
-	namespace import stack::*
-	namespace import queue::*
-}
