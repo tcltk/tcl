a44349c8166358f92b65f61682ea4a484df881b4
hobbs <hobbs>
2000-04-10 18:17:38 +0000

    * generic/tcl.h: removed specific typedef of Tcl_ThreadCreateProc 	for Windows as the build choked.  It seems to be OK using the Unix 	fallback (void).

--- generic/tcl.h
+++ generic/tcl.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.65 2000/04/09 16:04:17 kupries Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.66 2000/04/10 18:17:41 hobbs Exp $
  */
 
 #ifndef _TCL
@@ -395,8 +395,6 @@ typedef struct Tcl_Var_ *Tcl_Var;
 
 #ifdef MAC_TCL
 typedef pascal void *(Tcl_ThreadCreateProc) _ANSI_ARGS_((ClientData clientData));
-#elif defined __WIN32__
-typedef unsigned __stdcall (Tcl_ThreadCreateProc) _ANSI_ARGS_((ClientData clientData));
 #else
 typedef void (Tcl_ThreadCreateProc) _ANSI_ARGS_((ClientData clientData));
 #endif
