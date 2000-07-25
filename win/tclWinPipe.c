551149cabc973f19e88cecad80764ba06aca2966
ericm <ericm>
2000-07-25 18:38:36 +0000

    * win/tclWinPipe.c (BuildCommandLine): Added Tcl_DStringFree call to prevent potential memory leaks [Bug: 6041].

--- win/tclWinPipe.c
+++ win/tclWinPipe.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclWinPipe.c,v 1.12 2000/06/13 20:30:23 ericm Exp $
+ * RCS: @(#) $Id: tclWinPipe.c,v 1.13 2000/07/25 18:38:36 ericm Exp $
  */
 
 #include "tclWinInt.h"
@@ -1569,6 +1569,7 @@ BuildCommandLine(
 	    Tcl_DStringAppend(&ds, "\"", 1);
 	}
     }
+    Tcl_DStringFree(linePtr);
     Tcl_WinUtfToTChar(Tcl_DStringValue(&ds), Tcl_DStringLength(&ds), linePtr);
     Tcl_DStringFree(&ds);
 }
