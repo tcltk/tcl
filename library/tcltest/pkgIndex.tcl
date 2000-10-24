e1a51d633dfb0a7c6be68548f1bae3cadd57bb24
jenn <jenn>
2000-10-24 22:30:17 +0000

    * tests/all.tcl: Removed support for tcltest 1.0.
    * tests/tcltest.test:
    * library/tcltest1.0/tcltest.tcl:
    * library/tcltest1.0/pkgIndex.tcl:
    * docs/tcltest.n: Moved tcltest2 code so that it's the standard
    version of tcltest.  Removed all tcltest2 files
    (tests/tcltest2.test, library/tcltest1.0/tcltest2.tcl,
    docs/tcltest2.n).

--- library/tcltest/pkgIndex.tcl
+++ library/tcltest/pkgIndex.tcl
@@ -8,5 +8,4 @@
 # script is sourced, the variable $dir must contain the
 # full path name of this file's directory.
 
-package ifneeded tcltest 1.0 [list source [file join $dir tcltest.tcl]]
-package ifneeded tcltest 2.0 [list source [file join $dir tcltest2.tcl]]
+package ifneeded tcltest 2.0 [list source [file join $dir tcltest.tcl]]
