1575eff5fe24ad8305e7de90788f20e9dd6701d4
hobbs <hobbs>
2000-10-31 00:48:53 +0000

    * unix/tclUnixInit.c: added default encoding map from 	"ja_JP.eucJP" to "euc-jp". (takahashi)

--- unix/tclUnixInit.c
+++ unix/tclUnixInit.c
@@ -7,7 +7,7 @@
  * Copyright (c) 1999 by Scriptics Corporation.
  * All rights reserved.
  *
- * RCS: @(#) $Id: tclUnixInit.c,v 1.19 2000/05/18 21:36:35 hobbs Exp $
+ * RCS: @(#) $Id: tclUnixInit.c,v 1.20 2000/10/31 00:48:53 hobbs Exp $
  */
 
 #include "tclInt.h"
@@ -58,6 +58,7 @@ typedef struct LocaleTable {
 static CONST LocaleTable localeTable[] = {
     {"ja_JP.SJIS",	"shiftjis"},
     {"ja_JP.EUC",	"euc-jp"},
+    {"ja_JP.eucJP",     "euc-jp"},
     {"ja_JP.JIS",	"iso2022-jp"},
     {"ja_JP.mscode",	"shiftjis"},
     {"ja_JP.ujis",	"euc-jp"},
