aa50720a02659c2d35ffa7a600962b6e42edc480
Miguel Sofer <miguel.sofer@gmail.com>
2001-05-15 14:19:13 +0000

    Patch from [Bug: 231259]

--- generic/tclNamesp.c
+++ generic/tclNamesp.c
@@ -19,7 +19,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclNamesp.c,v 1.22 2001/04/25 09:44:49 dkf Exp $
+ * RCS: @(#) $Id: tclNamesp.c,v 1.23 2001/05/15 14:19:13 msofer Exp $
  */
 
 #include "tclInt.h"
@@ -2283,6 +2283,17 @@ TclResetShadowedCmdRefs(interp, newCmdPtr)
             hPtr = Tcl_FindHashEntry(&shadowNsPtr->cmdTable, cmdName);
             if (hPtr != NULL) {
                 nsPtr->cmdRefEpoch++;
+
+		/* 
+		 * If the shadowed command was compiled to bytecodes, we
+		 * invalidate all the bytecodes in nsPtr, to force a new
+		 * compilation. We use the resolverEpoch to signal the need
+		 * for a fresh compilation of every bytecode.
+		 */
+
+		if ((((Command *) hPtr)->compileProc) != NULL) {
+		    nsPtr->resolverEpoch++;
+		}
             }
         }
 
