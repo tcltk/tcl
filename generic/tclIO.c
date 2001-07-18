6cc38d5ff36cfbb941affae49796516273ec66a7
andreas_kupries <akupries@shaw.ca>
2001-07-18 17:13:25 +0000

    2001-07-18  Andreas Kupries <andreas_kupries@users.sourceforge.net>
    	* generic/tclIO.c: Aftermath to [SF #427196]. Squash empty buffers
    	  if they are smaller than the requested buffersize, to prevent
    	  reusage of old buffers and to honor changes in the requested
    	  buffersize made by the user.

--- generic/tclIO.c
+++ generic/tclIO.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclIO.c,v 1.31 2001/07/17 17:53:43 andreas_kupries Exp $
+ * RCS: @(#) $Id: tclIO.c,v 1.32 2001/07/18 17:13:25 andreas_kupries Exp $
  */
 
 #include "tclInt.h"
@@ -1696,6 +1696,17 @@ RecycleBuffer(statePtr, bufPtr, mustDiscard)
      */
 
     if (mustDiscard) {
+        ckfree((char *) bufPtr);
+        return;
+    }
+
+    /*
+     * Only save buffers which are at least as big as the requested
+     * buffersize for the channel. This is to honor dynamic changes
+     * of the buffersize made by the user.
+     */
+
+    if ((bufPtr->bufLength - BUFFER_PADDING) < statePtr->bufSize) {
         ckfree((char *) bufPtr);
         return;
     }
@@ -4994,6 +5005,19 @@ GetInput(chanPtr)
     } else {
 	bufPtr = statePtr->saveInBufPtr;
 	statePtr->saveInBufPtr = NULL;
+
+	/*
+	 * Check the actual buffersize against the requested
+	 * buffersize. Buffers which are smaller than requested aare
+	 * squashed. This is done to honor dynamic changes of the
+	 * buffersize made by the user.
+	 */
+
+	if ((bufPtr != NULL) && ((bufPtr->bufLength - BUFFER_PADDING) < statePtr->bufSize)) {
+	  ckfree((char *) bufPtr);
+	  bufPtr = NULL;
+	}
+
 	if (bufPtr == NULL) {
 	    bufPtr = AllocChannelBuffer(statePtr->bufSize);
 	}
