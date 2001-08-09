9b507861f168196d1b6bb228f1cd99bc7669fc33
dgp@users.sourceforge.net <dgp>
2001-08-09 01:06:42 +0000

    Added checks for package dependencies.         Bumped patchlevels of changed packages:  http 2.3.2, msgcat 1.2.2,         opt 0.4.3, tcltest 2.0.1.  [Patch 448931]

--- library/msgcat/pkgIndex.tcl
+++ library/msgcat/pkgIndex.tcl
@@ -1,1 +1,2 @@
-package ifneeded msgcat 1.2.1 [list source [file join $dir msgcat.tcl]]
+if {![package vsatisfies [package provide Tcl] 8.2]} {return}
+package ifneeded msgcat 1.2.2 [list source [file join $dir msgcat.tcl]]
