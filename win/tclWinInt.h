9c129407152d9ea9c27de684ccf0b63a5960daf1
andreas_kupries <akupries@shaw.ca>
2001-09-07 17:08:50 +0000

    * All the changes below serve to fix bug [219148] which reports a 	  80x performance hit for file I/O on Win* systems. On my system 	  it was closer to a 120x hit. Problem report by Uwe Traum <no 	  email address available>.
    	  The fix goes like this: The obstacle is 'FlushFileBuffers',
    	  executed whenever Tcl writes data to the OS, as Tcl has to wait
    	  for the disk to complete I/O, and disks are slow. We remove that
    	  obstacle. This opens another problem, [file size] reports back
    	  wrong numbers. So for [file size] we add the call back in. As
    	  optimization we keep track of the channels which were written to
    	  and flush only these.
    
    	* win/tclWinFile.c (TclpObjStat): Added a call to
    	  'TclWinFlushDirtyChannels'. This ensures that [file size] and
    	  related commands report the correct size of a file even if Tcl
    	  has recently written to it. Unixoid OS's always report the
    	  correct size even for files with pending data, but Win*
    	  syssystem don't. They only report what is actually on disk.
    
    	* win/tclWinInt.h: Added declaration of
    	  'TclWinFlushDirtyChannels', making it available to other parts
    	  of the tcl core.
    
    	* win/tclWinChan.c (TclWinFlushDirtyChannels): New, internal,
    	  procedure. Goes through the list of open file channels and
    	  forces the OS to flush its file buffers for all which were
    	  written to since the last call of this function. This is an
    	  expensive operation as Tcl has to wait for the OS to complete
    	  actual writes to the disk.
    
    	  (FileInfo): Added dirty flag required by the procedure above.
    
    	  (FileOutputProc): Removed flushing of file buffers, setting the
    	  dirty flag instead. This means that the previously incurred
    	  delays do not happen anymore.
    
    	  (TclWinOpenFileChannel): Added initialization of 'dirty' flag.

--- win/tclWinInt.h
+++ win/tclWinInt.h
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclWinInt.h,v 1.9 2001/03/26 11:08:16 dkf Exp $
+ * RCS: @(#) $Id: tclWinInt.h,v 1.10 2001/09/07 17:08:50 andreas_kupries Exp $
  */
 
 #ifndef _TCLWININT
@@ -99,6 +99,8 @@ EXTERN TclWinProcs *tclWinProcs;
  */
 
 EXTERN void		TclWinInit(HINSTANCE hInst);
+
+EXTERN void TclWinFlushDirtyChannels _ANSI_ARGS_((void));
 
 # undef TCL_STORAGE_CLASS
 # define TCL_STORAGE_CLASS DLLIMPORT
