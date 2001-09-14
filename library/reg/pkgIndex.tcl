6bf33fafa3cb59c443e11a36316b099fc94727b3
andreas_kupries <akupries@shaw.ca>
2001-09-14 01:00:59 +0000

    * win/Makefile.in: 	* win/configure.in: 	* win/makefile.bc: 	* win/makefile.vc:         * library/reg/pkgIndex.tcl 	* library/dde/pkgIndex.tcl: Fixed version numbers from bogus tcl 	  versions to independent versions for dde and registry packages.

--- library/reg/pkgIndex.tcl
+++ library/reg/pkgIndex.tcl
@@ -1,8 +1,8 @@
 if {![package vsatisfies [package provide Tcl] 8]} {return}
 if {[info exists tcl_platform(debug)]} {
     package ifneeded registry 1.0 \
-            [list load [file join $dir tclreg84d.dll] registry]
+            [list load [file join $dir tclreg10d.dll] registry]
 } else {
     package ifneeded registry 1.0 \
-            [list load [file join $dir tclreg84.dll] registry]
+            [list load [file join $dir tclreg10.dll] registry]
 }
