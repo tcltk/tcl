7716b3fbd2f8e0ec48234295f43467899a571f5a
andreask@activestate.com <andreas_kupries>
2001-09-14 01:00:59 +0000

    * win/Makefile.in: 	* win/configure.in: 	* win/makefile.bc: 	* win/makefile.vc:         * library/reg/pkgIndex.tcl 	* library/dde/pkgIndex.tcl: Fixed version numbers from bogus tcl 	  versions to independent versions for dde and registry packages.

--- library/dde/pkgIndex.tcl
+++ library/dde/pkgIndex.tcl
@@ -1,6 +1,6 @@
 if {![package vsatisfies [package provide Tcl] 8]} {return}
 if {[info exists tcl_platform(debug)]} {
-    package ifneeded dde 1.1 [list load [file join $dir tcldde84d.dll] dde]
+    package ifneeded dde 1.2 [list load [file join $dir tcldde12d.dll] dde]
 } else {
-    package ifneeded dde 1.1 [list load [file join $dir tcldde84.dll] dde]
+    package ifneeded dde 1.2 [list load [file join $dir tcldde12.dll] dde]
 }
