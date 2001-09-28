78d2177415091477dfad3c2c5c866a14c529d7bc
andreas_kupries <akupries@shaw.ca>
2001-09-28 16:30:12 +0000

    * generic/tclIO.c: added (on behalf of Kevin Kenny 	  <kennykb@users.sourceforge.net>) two more '(ClientData)' casts 	  on calls to Tcl_Preserve and Tcl_Release -- ones that Vince 	  apparently missed.

--- generic/tclIO.c
+++ generic/tclIO.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclIO.c,v 1.20.2.10 2001/09/28 02:26:35 dgp Exp $
+ * RCS: @(#) $Id: tclIO.c,v 1.20.2.11 2001/09/28 16:30:12 andreas_kupries Exp $
  */
 
 #include "tclInt.h"
@@ -6311,11 +6311,11 @@ ChannelTimerProc(clientData)
 	    (Tcl_ChannelBlockModeProc(chanPtr->typePtr) == NULL)) {
 	    statePtr->flags |= CHANNEL_TIMER_FEV;
 	}
-	Tcl_Preserve(statePtr);
+	Tcl_Preserve((ClientData) statePtr);
 	Tcl_NotifyChannel((Tcl_Channel)chanPtr, TCL_READABLE);
 
 	statePtr->flags &= ~CHANNEL_TIMER_FEV; 
-	Tcl_Release(statePtr);
+	Tcl_Release((ClientData) statePtr);
     } else {
 	statePtr->timer = NULL;
 	UpdateInterest(chanPtr);
