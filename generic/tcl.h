5d91220e677b61dfbf0a4d91bd970ef71e18b0c2
hobbs <hobbs>
2001-10-15 17:34:35 +0000

    * generic/tcl.h: moved #define of WIN32 to tcl.h where __WIN32__ 	is defined and added #ifndef check.

--- generic/tcl.h
+++ generic/tcl.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.103 2001/09/28 01:21:53 dgp Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.104 2001/10/15 17:34:35 hobbs Exp $
  */
 
 #ifndef _TCL
@@ -74,6 +74,9 @@ extern "C" {
 #   if defined(_WIN32) || defined(WIN32) || \
        defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
 #	define __WIN32__
+#	ifndef WIN32
+#	    define WIN32
+#	endif
 #   endif
 #endif
 
