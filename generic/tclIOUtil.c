0b572399d0bb37e2313c196b90cf5dfcf57cd58c
dkf <donal.k.fellows@manchester.ac.uk>
2001-11-25 17:27:47 +0000

    Fixed fault in new Tcl_Stat implementation, and improved docs.

--- generic/tclIOUtil.c
+++ generic/tclIOUtil.c
@@ -17,7 +17,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclIOUtil.c,v 1.20.6.5 2001/11/23 22:56:24 dkf Exp $
+ * RCS: @(#) $Id: tclIOUtil.c,v 1.20.6.6 2001/11/25 17:27:48 dkf Exp $
  */
 
 #include "tclInt.h"
@@ -91,13 +91,18 @@ Tcl_Stat(path, oldStyleBuf)
     if (ret != -1) {
 #ifndef TCL_WIDE_INT_IS_LONG
 #   define OUT_OF_RANGE(x) \
-	(((Tcl_WideInt)(x))<LLONG_MIN||((Tcl_WideInt)(x))>LLONG_MAX)
+	(((Tcl_WideInt)(x)) < Tcl_LongAsWide(LONG_MIN) || \
+	 ((Tcl_WideInt)(x)) > Tcl_LongAsWide(LONG_MAX))
+#   define OUT_OF_URANGE(x) \
+	(((Tcl_WideUInt)(x)) > (Tcl_WideUInt)ULONG_MAX)
 
 	/*
 	 * Perform the result-buffer overflow check manually.
+	 *
+	 * Note that ino_t/ino64_t is unsigned...
 	 */
 
-        if (OUT_OF_RANGE(buf.st_ino) || OUT_OF_RANGE(buf.st_size)
+        if (OUT_OF_URANGE(buf.st_ino) || OUT_OF_RANGE(buf.st_size)
 		|| OUT_OF_RANGE(buf.st_blocks)) {
 	    errno = EOVERFLOW;
 	    return -1;
