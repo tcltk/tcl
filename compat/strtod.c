730ef3578f5ee4acee78ad32b67c0dd4e963d911
dgp <dgp@users.sourceforge.net>
2002-02-25 14:26:12 +0000

    simplified the #include lines

--- compat/strtod.c
+++ compat/strtod.c
@@ -9,18 +9,12 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: strtod.c,v 1.5 2002/02/25 10:36:32 dkf Exp $
+ * RCS: @(#) $Id: strtod.c,v 1.6 2002/02/25 14:26:12 dgp Exp $
  */
 
-#include "tcl.h"
+#include "tclInt.h"
 #include "tclPort.h"
-#ifdef NO_STDLIB_H
-#   include "../compat/stdlib.h"
-#else
-#   include <stdlib.h>
-#endif
 #include <ctype.h>
-#include "tclPort.h"
 
 #ifndef TRUE
 #define TRUE 1
