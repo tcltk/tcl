a6e2d59eac3f27e97b2d8a1bb60b5fcab0b368bd
andreask@activestate.com <andreas_kupries>
2002-10-28 21:14:11 +0000

    * library/dde/pkgIndex.tcl: 	* library/reg/pkgIndex.tcl: Changed the hardwired debug suffix 	  (d) to the correct suffix (g).

--- library/reg/pkgIndex.tcl
+++ library/reg/pkgIndex.tcl
@@ -1,7 +1,7 @@
 if {![package vsatisfies [package provide Tcl] 8]} {return}
 if {[info exists tcl_platform(debug)]} {
     package ifneeded registry 1.1 \
-            [list load [file join $dir tclreg11d.dll] registry]
+            [list load [file join $dir tclreg11g.dll] registry]
 } else {
     package ifneeded registry 1.1 \
             [list load [file join $dir tclreg11.dll] registry]
