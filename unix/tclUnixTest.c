97377ca7783a7603bc1e0d4fe849be698e64b7f0
Kevin B Kenny <kennykb@acm.org>
2003-02-15 22:30:29 +0000

    Corrected a couple of typos in error messages. [Bug 596027]

--- unix/tclUnixTest.c
+++ unix/tclUnixTest.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclUnixTest.c,v 1.13 2002/08/20 03:03:54 andreas_kupries Exp $
+ * RCS: @(#) $Id: tclUnixTest.c,v 1.14 2003/02/15 22:30:29 kennykb Exp $
  */
 
 #include "tclInt.h"
@@ -274,7 +274,7 @@ TestfilehandlerCmd(clientData, interp, argc, argv)
     } else if (strcmp(argv[1], "fill") == 0) {
 	if (argc != 3) {
 	    Tcl_AppendResult(interp, "wrong # arguments: should be \"",
-                    argv[0], " empty index\"", (char *) NULL);
+                    argv[0], " fill index\"", (char *) NULL);
 	    return TCL_ERROR;
 	}
 
@@ -287,7 +287,7 @@ TestfilehandlerCmd(clientData, interp, argc, argv)
 	
 	if (argc != 3) {
 	    Tcl_AppendResult(interp, "wrong # arguments: should be \"",
-                    argv[0], " empty index\"", (char *) NULL);
+                    argv[0], " fillpartial index\"", (char *) NULL);
 	    return TCL_ERROR;
 	}
 
