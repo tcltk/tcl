cefcc05cd44b587cb2f9f673b2f3cc472e2c80db
dgp <dgp@users.sourceforge.net>
2003-04-07 20:02:48 +0000

    * generic/tclCompCmds.c (TclCompileIfCmd):  Corrected string limits of         arguments interpolated in error messages. [Bug 711371]

--- generic/tclCompCmds.c
+++ generic/tclCompCmds.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCompCmds.c,v 1.39 2003/02/07 01:07:05 mdejong Exp $
+ * RCS: @(#) $Id: tclCompCmds.c,v 1.39.2.1 2003/04/07 20:03:05 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -1196,8 +1196,8 @@ TclCompileIfCmd(interp, parsePtr, envPtr)
 	}
 	if (wordIdx >= numWords) {
 	    sprintf(buffer,
-	            "wrong # args: no expression after \"%.30s\" argument",
-		    word);
+	            "wrong # args: no expression after \"%.*s\" argument",
+		    numBytes, word);
 	    Tcl_ResetResult(interp);
 	    Tcl_AppendToObj(Tcl_GetObjResult(interp), buffer, -1);
 	    code = TCL_ERROR;
@@ -1259,7 +1259,7 @@ TclCompileIfCmd(interp, parsePtr, envPtr)
 	tokenPtr = testTokenPtr + (testTokenPtr->numComponents + 1);
 	wordIdx++;
 	if (wordIdx >= numWords) {
-	    sprintf(buffer, "wrong # args: no script following \"%.20s\" argument", testTokenPtr->start);
+	    sprintf(buffer, "wrong # args: no script following \"%.*s\" argument", testTokenPtr->size, testTokenPtr->start);
 	    Tcl_ResetResult(interp);
 	    Tcl_AppendToObj(Tcl_GetObjResult(interp), buffer, -1);
 	    code = TCL_ERROR;
