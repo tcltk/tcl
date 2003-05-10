47452aaf17505d1b46ce8886997da740e17e3b57
Joe Mistachkin <joe@mistachkin.com>
2003-05-10 04:25:49 +0000

    fix bad cvs lf conversion

--- win/tclAppInit.c
+++ win/tclAppInit.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclAppInit.c,v 1.12 2003/05/10 02:52:54 mistachkin Exp $
+ * RCS: @(#) $Id: tclAppInit.c,v 1.13 2003/05/10 04:25:49 mistachkin Exp $
  */
 
 #include "tcl.h"
@@ -376,7 +376,7 @@ setargv(argcPtr, argvPtr)
     *argcPtr = argc;
     *argvPtr = argv;
 }
-
+
 /*
  *----------------------------------------------------------------------
  *
@@ -401,7 +401,7 @@ asyncExit (ClientData clientData, Tcl_Interp *interp, int code)
     /* NOTREACHED */
     return code;
 }
-
+
 /*
  *----------------------------------------------------------------------
  *
