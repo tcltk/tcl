bd23f3d38d90df4d13ed707fec50e231ce016152
Joe Mistachkin <joe@mistachkin.com>
2003-05-10 04:57:40 +0000

    fix bad cvs lf conversion

--- generic/tclThreadAlloc.c
+++ generic/tclThreadAlloc.c
@@ -11,7 +11,8 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclThreadAlloc.c,v 1.4.2.1 2003/05/10 00:09:08 mistachkin Exp $ */
+ * RCS: @(#) $Id: tclThreadAlloc.c,v 1.4.2.2 2003/05/10 04:57:40 mistachkin Exp $ 
+ */
 
 #if defined(TCL_THREADS) && defined(USE_THREAD_ALLOC)
 
