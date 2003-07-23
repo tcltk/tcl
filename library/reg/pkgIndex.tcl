826c606c461ac74ce4dc35721618bf429be8336d
patthoyts@users.sourceforge.net <patthoyts>
2003-07-23 20:57:56 +0000

    * win/tclWinReg.c:             Incremented the version to 1.1.2. * library/reg/pkgIndex.tcl:

--- library/reg/pkgIndex.tcl
+++ library/reg/pkgIndex.tcl
@@ -1,8 +1,8 @@
 if {![package vsatisfies [package provide Tcl] 8]} {return}
 if {[info exists ::tcl_platform(debug)]} {
-    package ifneeded registry 1.1.1 \
+    package ifneeded registry 1.1.2 \
             [list load [file join $dir tclreg11g.dll] registry]
 } else {
-    package ifneeded registry 1.1.1 \
+    package ifneeded registry 1.1.2 \
             [list load [file join $dir tclreg11.dll] registry]
 }
