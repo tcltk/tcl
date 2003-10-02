fce5bf25b63ac085a6a713187d3469d63874b318
dgp <dgp>
2003-10-02 23:07:32 +0000

    * README:               Bumped patch level to 8.4.5 to prepare         * generic/tcl.h:        for next patch release.         * tools/tcl.wse.in:         * unix/configure.in:         * unix/tcl.spec:         * win/README.binary:         * win/configure.in:
            * unix/configure:       autoconf (2.13)
            * win/configure:
    
            * library/http/http.tcl:        Bumped to http 2.4.5
            * library/http/pkgIndex.tcl:

--- generic/tcl.h
+++ generic/tcl.h
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.153.2.6 2003/07/16 22:06:04 hobbs Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.153.2.7 2003/10/02 23:07:33 dgp Exp $
  */
 
 #ifndef _TCL
@@ -58,10 +58,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   4
 #define TCL_RELEASE_LEVEL   TCL_FINAL_RELEASE
-#define TCL_RELEASE_SERIAL  4
+#define TCL_RELEASE_SERIAL  5
 
 #define TCL_VERSION	    "8.4"
-#define TCL_PATCH_LEVEL	    "8.4.4"
+#define TCL_PATCH_LEVEL	    "8.4.5"
 
 /*
  * The following definitions set up the proper options for Windows
