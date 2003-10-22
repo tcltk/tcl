a67da24acd76ee66fd2306972d0291db0e451d12
andreas_kupries <andreas_kupries>
2003-10-22 22:35:45 +0000

    * generic/tclIOUtil.c (FsListMounts, FsAddMountsToGlobResult): New 	  functions. See below for context. 	  (Tcl_FSMatchInDirectory): Modified to call on the new functions 	  (above) to handle the mountpoints in the glob'bed directory 	  correctly. Part of the patch by Vincent Darly to solve the 	  [Bug 800106] for the 8.4.x series.
    	* generic/tcl.h (TCL_GLOB_TYPE_MOUNT): New definition. Part of the
    	  patch by Vincent Darly to solve [Bug 800106] for the 8.4.x series.

--- generic/tclTest.c
+++ generic/tclTest.c
@@ -14,7 +14,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclTest.c,v 1.62.2.2 2003/10/08 14:21:20 dkf Exp $
+ * RCS: @(#) $Id: tclTest.c,v 1.62.2.3 2003/10/22 22:35:46 andreas_kupries Exp $
  */
 
 #define TCL_TEST
@@ -6091,16 +6091,21 @@ TestReportOpenFileChannel(interp, fileName, mode, permissions)
 static int
 TestReportMatchInDirectory(interp, resultPtr, dirPtr, pattern, types)
     Tcl_Interp *interp;		/* Interpreter to receive results. */
-    Tcl_Obj *resultPtr;		/* Directory separators to pass to TclDoGlob. */
+    Tcl_Obj *resultPtr;		/* Object to lappend results. */
     Tcl_Obj *dirPtr;	        /* Contains path to directory to search. */
     CONST char *pattern;	/* Pattern to match against. */
     Tcl_GlobTypeData *types;	/* Object containing list of acceptable types.
 				 * May be NULL. */
 {
-    TestReport("matchindirectory",dirPtr, NULL);
-    return Tcl_FSMatchInDirectory(interp, resultPtr, 
-				  TestReportGetNativePath(dirPtr), pattern, 
-				  types);
+    if (types != NULL && types->type & TCL_GLOB_TYPE_MOUNT) {
+	TestReport("matchmounts",dirPtr, NULL);
+	return TCL_OK;
+    } else {
+	TestReport("matchindirectory",dirPtr, NULL);
+	return Tcl_FSMatchInDirectory(interp, resultPtr, 
+				      TestReportGetNativePath(dirPtr), pattern, 
+				      types);
+    }
 }
 static int
 TestReportChdir(dirName)
