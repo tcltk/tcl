21b974e4ba53062d6e65d7ee912fb97aa14ab330
davygrvy <davygrvy@pobox.com>
2004-04-20 02:10:38 +0000

    Added an exit handler in tclWinInit() so tclWinTCharEncoding could be freed for Tcl_Finalize.

--- win/tclWin32Dll.c
+++ win/tclWin32Dll.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclWin32Dll.c,v 1.32 2004/01/21 19:59:34 vincentdarley Exp $
+ * RCS: @(#) $Id: tclWin32Dll.c,v 1.33 2004/04/20 02:10:38 davygrvy Exp $
  */
 
 #include "tclWinInt.h"
@@ -156,6 +156,7 @@ static TclWinProcs unicodeProcs = {
 
 TclWinProcs *tclWinProcs;
 static Tcl_Encoding tclWinTCharEncoding;
+Tcl_ExitProc TclWinUninit;
 
 /*
  * The following declaration is for the VC++ DLL entry point.
@@ -397,6 +398,34 @@ TclWinInit(hInst)
     }
 
     tclWinProcs = &asciiProcs;
+    Tcl_CreateExitHandler(TclWinUninit, NULL);
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * TclWinUninit --
+ *
+ *	Exit handler callback to remove private components used in this
+ *	file.
+ *
+ * Results:
+ *	None.
+ *
+ * Side effects:
+ *	Tcl_WinTCharToUtf and Tcl_WinUtfToTChar will not work during
+ *	the latter half of Tcl_Finalize.
+ *
+ *----------------------------------------------------------------------
+ */
+
+void
+TclWinUninit (ClientData clientData)
+{
+    if (tclWinTCharEncoding) {
+	Tcl_FreeEncoding(tclWinTCharEncoding);
+	tclWinTCharEncoding = NULL;
+    }
 }
 
 /*
