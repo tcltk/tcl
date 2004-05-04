e2d0d91fa792e2f97a832e3b025c543dfbddf697
Miguel Sofer <miguel.sofer@gmail.com>
2004-05-04 02:38:06 +0000

    * generic/tclCompile.c (TclCompileScript): setting the compilation 	namespace outside of the loop.

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCompile.c,v 1.62 2004/05/03 19:59:17 msofer Exp $
+ * RCS: @(#) $Id: tclCompile.c,v 1.63 2004/05/04 02:38:07 msofer Exp $
  */
 
 #include "tclInt.h"
@@ -1001,6 +1001,12 @@ TclCompileScript(interp, script, numBytes, envPtr)
 	    EnterCmdStartData(envPtr, currCmdIndex,
 	            (parse.commandStart - envPtr->source), startCodeOffset);
 
+	    if (envPtr->procPtr != NULL) {
+		cmdNsPtr = envPtr->procPtr->cmdPtr->nsPtr;
+	    } else {
+		cmdNsPtr = NULL; /* use current NS */
+	    }
+
 	    /*
 	     * Each iteration of the following loop compiles one word
 	     * from the command.
@@ -1029,12 +1035,6 @@ TclCompileScript(interp, script, numBytes, envPtr)
 		     */
 
 		    if ((wordIdx == 0) && !expand) {
-			if (envPtr->procPtr != NULL) {
-			    cmdNsPtr = envPtr->procPtr->cmdPtr->nsPtr;
-			} else {
-			    cmdNsPtr = NULL; /* use current NS */
-			}
-
 			/*
 			 * We copy the string before trying to find the command
 			 * by name.  We used to modify the string in place, but
