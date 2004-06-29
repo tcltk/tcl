6e1fd820cba4c1c418981963fa2fe62ea112b69d
dkf <donal.k.fellows@manchester.ac.uk>
2004-06-29 09:34:41 +0000

    Fix [Bug 981733]; bizarre resolver brokenness strikes again!

--- library/safe.tcl
+++ library/safe.tcl
@@ -12,7 +12,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: safe.tcl,v 1.13 2004/03/17 18:14:14 das Exp $
+# RCS: @(#) $Id: safe.tcl,v 1.14 2004/06/29 09:34:44 dkf Exp $
 
 #
 # The implementation is based on namespaces. These naming conventions
@@ -36,6 +36,10 @@ namespace eval ::safe {
     # Setup the arguments parsing
     #
     ####
+
+    # Make sure that our temporary variable is local to this
+    # namespace.  [Bug 981733]
+    variable temp
 
     # Share the descriptions
     set temp [::tcl::OptKeyRegister {
@@ -321,7 +325,7 @@ namespace eval ::safe {
 
 	# determine and store the access path if empty
 	if {[string equal "" $access_path]} {
-	    set access_path [uplevel #0 set auto_path]
+	    set access_path [uplevel \#0 set auto_path]
 	    # Make sure that tcl_library is in auto_path
 	    # and at the first position (needed by setAccessPath)
 	    set where [lsearch -exact $access_path [info library]]
