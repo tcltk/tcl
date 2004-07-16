e40644c547171476e6a3c82a26de2be520bada29
andreas_kupries <akupries@shaw.ca>
2004-07-16 17:38:27 +0000

    * unix/tclUnixNotfy.c (NotifierThreadProc): Accepted Joe 	  Mistachkin's patch for [Tcl SF Bug 990500], properly closing the 	  notifier thread when its exits.

--- unix/tclUnixNotfy.c
+++ unix/tclUnixNotfy.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclUnixNotfy.c,v 1.16 2004/06/22 13:09:00 vasiljevic Exp $
+ * RCS: @(#) $Id: tclUnixNotfy.c,v 1.17 2004/07/16 17:38:28 andreas_kupries Exp $
  */
 
 #include "tclInt.h"
@@ -1030,5 +1030,7 @@ NotifierThreadProc(clientData)
     triggerPipe = -1;
     Tcl_ConditionNotify(&notifierCV);
     Tcl_MutexUnlock(&notifierMutex);
+
+    TclpThreadExit (0);
 }
 #endif
