adbb2953c1197efe1d2e0a8a553ead801119931d
dgp <dgp>
2004-09-30 22:45:10 +0000

    * generic/tclNamespace.c (TclTeardownNamespace): Tcl_Obj-ified         * tests/namespace.test (namespace-8.5,6):       the save/restore         of ::errorInfo and ::errorCode during global namespace teardown.         Revised the comment to clarify why this is done, and added tests         that will fail if this is not done.
            * generic/tclResult.c (TclTransferResult):      Added safety
            checks so that unexpected undefined ::errorInfo or ::errorCode
            will not lead to a segfault.
    
            * generic/tclVar.c (CallVarTraces):     Save/restore the flag
            * tests/var.test (var-16.1):    values that define part of the
            interpreter state during variable traces.  [Bug 10381021].

--- generic/tclVar.c
+++ generic/tclVar.c
@@ -15,7 +15,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclVar.c,v 1.69.2.6 2004/08/16 14:18:26 msofer Exp $
+ * RCS: @(#) $Id: tclVar.c,v 1.69.2.7 2004/09/30 22:45:15 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -4133,6 +4133,7 @@ CallVarTraces(iPtr, arrayPtr, varPtr, part1, part2, flags, leaveErrMsg)
     int copiedName;
     int code = TCL_OK;
     int disposeFlags = 0;
+    int saveErrFlags = iPtr->flags;
 
     /*
      * If there are already similar trace procedures active for the
@@ -4255,6 +4256,9 @@ CallVarTraces(iPtr, arrayPtr, varPtr, part1, part2, flags, leaveErrMsg)
      */
 
     done:
+    if (code == TCL_OK) {
+	iPtr->flags = saveErrFlags;
+    }
     if (code == TCL_ERROR) {
 	if (leaveErrMsg) {
 	    CONST char *type = "";
