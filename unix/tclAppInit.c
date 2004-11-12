e2e93d05e1e299a95888ec68e42d16160d63d30c
dgp <dgp>
2004-11-12 22:52:27 +0000

    * unix/tclAppInit.c:    Removed tclConfig.h #include, now that tcl.h                                 takes care of it for us.

--- unix/tclAppInit.c
+++ unix/tclAppInit.c
@@ -11,15 +11,8 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclAppInit.c,v 1.14 2004/11/12 18:07:55 rmax Exp $
+ * RCS: @(#) $Id: tclAppInit.c,v 1.15 2004/11/12 22:52:30 dgp Exp $
  */
-
-/*
- * The core stores all its #defs in config.h for clarity...
- */
-#ifdef HAVE_CONFIG_H
-#include "tclConfig.h"
-#endif
 
 #include "tcl.h"
 
