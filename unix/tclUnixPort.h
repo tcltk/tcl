d6a522034dba3eed8776e4752ea99295ac8a936c
dkf <donal.k.fellows@manchester.ac.uk>
2004-11-12 14:18:11 +0000

    * unix/tcl.m4, unix/tclUnixPort.h: Check for pthread_attr_get_np         in <pthread.h> before forcing the use of <pthread_np.h> to make         things work on NetBSD 2.0.  [Bug 1064882]

--- unix/tclUnixPort.h
+++ unix/tclUnixPort.h
@@ -19,7 +19,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclUnixPort.h,v 1.38 2004/06/23 00:24:42 dkf Exp $
+ * RCS: @(#) $Id: tclUnixPort.h,v 1.39 2004/11/12 14:18:29 dkf Exp $
  */
 
 #ifndef _TCLUNIXPORT
@@ -577,8 +577,14 @@ EXTERN char *          	TclpInetNtoa(struct in_addr);
 #	define NO_REALPATH
 #   endif
 #   ifdef HAVE_PTHREAD_ATTR_GET_NP
-#	include <pthread_np.h>
 #	define TclpPthreadGetAttrs	pthread_attr_get_np
+#	ifdef ATTRGETNP_NOT_DECLARED
+/*
+ * Assume it is in pthread_np.h if it isn't in pthread.h. [Bug 1064882]
+ * We might need to revisit this in the future. :^(
+ */
+#	    include <pthread_np.h>
+#	endif
 #   else
 #	ifdef HAVE_PTHREAD_GETATTR_NP
 #	    define TclpPthreadGetAttrs	pthread_getattr_np
