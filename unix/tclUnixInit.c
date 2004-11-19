cb927916f6fcd539ca6911ee2e9b477718a37d81
das <das>
2004-11-19 06:28:29 +0000

    * macosx/Makefile: 	* unix/configure.in: 	* unix/tclUnixInit.c (MacOSXGetLibraryPath): changed detection 	of tcl framework build when determining tclLibPath from overloaded 	TCL_LIBRARY to configuration define TCL_FRAMEWORK. [Bug 1068088]
    	* unix/configure: autoconf-2.57
    	* unix/tclConfig.h.in: autoheader-2.57

--- unix/tclUnixInit.c
+++ unix/tclUnixInit.c
@@ -7,7 +7,7 @@
  * Copyright (c) 1999 by Scriptics Corporation.
  * All rights reserved.
  *
- * RCS: @(#) $Id: tclUnixInit.c,v 1.50 2004/11/12 19:08:10 das Exp $
+ * RCS: @(#) $Id: tclUnixInit.c,v 1.51 2004/11/19 06:28:31 das Exp $
  */
 
 #include "tclInt.h"
@@ -1218,10 +1218,10 @@ static int
 MacOSXGetLibraryPath(Tcl_Interp *interp, int maxPathLen, char *tclLibPath)
 {
     int foundInFramework = TCL_ERROR;
-    if (strcmp(defaultLibraryDir, "@TCL_IN_FRAMEWORK@") == 0) {
-	foundInFramework = Tcl_MacOSXOpenVersionedBundleResources(interp,
-	    "com.tcltk.tcllibrary", TCL_VERSION, 0, maxPathLen, tclLibPath);
-    }
+#ifdef TCL_FRAMEWORK
+    foundInFramework = Tcl_MacOSXOpenVersionedBundleResources(interp, 
+	"com.tcltk.tcllibrary", TCL_VERSION, 0, maxPathLen, tclLibPath);
+#endif
     return foundInFramework;
 }
 #endif /* HAVE_CFBUNDLE */
