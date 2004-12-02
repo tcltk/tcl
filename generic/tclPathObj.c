c8982057316913717da2080a4b014dedaa402e18
Kevin B Kenny <kennykb@acm.org>
2004-12-02 19:24:12 +0000

    merge to kennykb-numerics-branch-20041202a

--- generic/tclPathObj.c
+++ generic/tclPathObj.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclPathObj.c,v 1.38 2004/11/22 12:53:06 vincentdarley Exp $
+ * RCS: @(#) $Id: tclPathObj.c,v 1.38.2.1 2004/12/02 19:24:14 kennykb Exp $
  */
 
 #include "tclInt.h"
@@ -1232,6 +1232,12 @@ TclNewFSPathObj(Tcl_Obj *dirPtr, CONST char *addStrRep, int len)
  *      inside the directory.  Returns a Tcl_Obj representing filename 
  *      of the path relative to the directory.
  *      
+ *      In the case where the resulting path would start with a '~', we
+ *      take special care to return an ordinary string.  This means to
+ *      use that path (and not have it interpreted as a user name),
+ *      one must prepend './'.  This may seem strange, but that is how
+ *      'glob' is currently defined.
+ *      
  * Results:
  *      NULL on error, otherwise a valid object, typically with
  *      refCount of zero, which it is assumed the caller will
@@ -1272,6 +1278,16 @@ TclFSMakePathRelative(interp, pathPtr, cwdPtr)
 		    pathPtr->typePtr->updateStringProc(pathPtr);
 		}
 		TclFreeIntRep(pathPtr);
+	    }
+	    /* Now pathPtr is a string object */
+	    
+	    if (Tcl_GetString(pathPtr)[0] == '~') {
+		/* 
+		 * If the first character of the path is a tilde,
+		 * we must just return the path as is, to agree
+		 * with the defined behaviour of 'glob'.
+		 */
+		return pathPtr;
 	    }
 
 	    fsPathPtr = (FsPath*)ckalloc((unsigned)sizeof(FsPath));
