f198740c86117c5d124f1131ff3127ba20141c68
davidw <davidw>
2005-02-05 09:15:37 +0000

    * doc/Thread.3: Fixed sentence describing flags for Tcl_CreateThread.
    * doc/FileSystem.3: Cleaned up typo in Tcl_FSNewNativePath
      documentation.
    
    * generic/tclPathObj.c: Cleaned up typo in comment.

--- generic/tclPathObj.c
+++ generic/tclPathObj.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclPathObj.c,v 1.39 2004/12/02 18:49:21 vincentdarley Exp $
+ * RCS: @(#) $Id: tclPathObj.c,v 1.40 2005/02/05 09:15:42 davidw Exp $
  */
 
 #include "tclInt.h"
@@ -1416,7 +1416,7 @@ TclFSMakePathFromNormalized(interp, pathPtr, nativeRep)
  *
  * Tcl_FSNewNativePath --
  *
- *      This function performs the something like that reverse of the 
+ *      This function performs the something like the reverse of the 
  *      usual obj->path->nativerep conversions.  If some code retrieves
  *      a path in native form (from, e.g. readlink or a native dialog),
  *      and that path is to be used at the Tcl level, then calling
