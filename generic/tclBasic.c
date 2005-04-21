9f8b61718dc2c0f7eaa78193dbe4b545e2368feb
dgp@users.sourceforge.net <dgp>
2005-04-21 21:23:54 +0000

    * generic/tclBasic.c:   Updated callers to call new routine.         * generic/tclCompCmds.c:        Updated callers to call new routine.

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclBasic.c,v 1.145 2005/04/19 16:32:53 dgp Exp $
+ * RCS: @(#) $Id: tclBasic.c,v 1.146 2005/04/21 21:24:03 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -4042,20 +4042,7 @@ Tcl_ExprBoolean(interp, string, ptr)
 	    /*
 	     * Store a boolean based on the expression result.
 	     */
-
-	    if (resultPtr->typePtr == &tclIntType) {
-		*ptr = (resultPtr->internalRep.longValue != 0);
-	    } else if (resultPtr->typePtr == &tclDoubleType) {
-		*ptr = (resultPtr->internalRep.doubleValue != 0.0);
-	    } else if (resultPtr->typePtr == &tclWideIntType) {
-#ifndef TCL_WIDE_INT_IS_LONG
-		*ptr = (resultPtr->internalRep.wideValue != 0);
-#else
-		*ptr = (resultPtr->internalRep.longValue != 0);
-#endif
-	    } else {
-		result = Tcl_GetBooleanFromObj(interp, resultPtr, ptr);
-	    }
+	    result = TclGetTruthValueFromObj(interp, resultPtr, ptr);
 	    Tcl_DecrRefCount(resultPtr);  /* discard the result object */
 	}
 	if (result != TCL_OK) {
@@ -4165,13 +4152,7 @@ Tcl_ExprBooleanObj(interp, objPtr, ptr)
 
     result = Tcl_ExprObj(interp, objPtr, &resultPtr);
     if (result == TCL_OK) {
-	if (resultPtr->typePtr == &tclIntType) {
-	    *ptr = (resultPtr->internalRep.longValue != 0);
-	} else if (resultPtr->typePtr == &tclDoubleType) {
-	    *ptr = (resultPtr->internalRep.doubleValue != 0.0);
-	} else {
-	    result = Tcl_GetBooleanFromObj(interp, resultPtr, ptr);
-	}
+	result = TclGetTruthValueFromObj(interp, resultPtr, ptr);
 	Tcl_DecrRefCount(resultPtr);  /* discard the result object */
     }
     return result;
