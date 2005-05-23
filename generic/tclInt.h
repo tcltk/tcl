4a3b074f12e5ead7fffcac29f10ca842f81dbb26
das <das>
2005-05-23 20:19:44 +0000

    * generic/tclIOUtil.c (TclLoadFile): 	* generic/tclInt.h: 	* unix/tcl.m4: 	* unix/tclLoadDyld.c: added support for [load]ing .bundle binaries in 	addition to .dylib's: .bundle's can be [unload]ed (unlike .dylib's), 	and can be [load]ed from memory, e.g. directly from VFS without 	needing to be written out to a temporary location first. [Bug 1202209] 	* unix/configure: autoconf-2.59 	* unix/tclConfig.h.in: autoheader-2.59

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclInt.h,v 1.230 2005/05/18 15:43:36 dgp Exp $
+ * RCS: @(#) $Id: tclInt.h,v 1.231 2005/05/23 20:19:45 das Exp $
  */
 
 #ifndef _TCLINT
@@ -2075,6 +2075,14 @@ MODULE_SCOPE int	TclpDlopen _ANSI_ARGS_((Tcl_Interp *interp,
 			    Tcl_FSUnloadFileProc **unloadProcPtr));
 MODULE_SCOPE int	TclpUtime _ANSI_ARGS_((Tcl_Obj *pathPtr,
 			    struct utimbuf *tval));
+#ifdef TCL_LOAD_FROM_MEMORY
+MODULE_SCOPE void*	TclpLoadMemoryGetBuffer _ANSI_ARGS_((
+			    Tcl_Interp *interp, int size));
+MODULE_SCOPE int	TclpLoadMemory _ANSI_ARGS_((Tcl_Interp *interp, 
+			    void *buffer, int size, int codeSize, 
+			    Tcl_LoadHandle *loadHandle, 
+			    Tcl_FSUnloadFileProc **unloadProcPtr));
+#endif
 
 /*
  *----------------------------------------------------------------
