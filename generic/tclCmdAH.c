b70197afbdb6e71461ae861830db8f544fc47505
dgp <dgp@users.sourceforge.net>
2005-07-05 21:18:14 +0000

    * generic/tclCmdAH.c    New "encoding" Tcl_ObjType (not registered)         * generic/tclEncoding.c that permits longer lifetimes of the         * generic/tclInt.h      Tcl_Encoding values kept as intreps of         Tcl_Obj's.  Reduces the need for repeated reading of encoding         definition files from the filesystem.  [Bug 1077262]

--- generic/tclCmdAH.c
+++ generic/tclCmdAH.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCmdAH.c,v 1.27.2.12 2005/06/17 23:26:20 dkf Exp $
+ * RCS: @(#) $Id: tclCmdAH.c,v 1.27.2.13 2005/07/05 21:18:23 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -440,24 +440,21 @@ Tcl_EncodingObjCmd(dummy, interp, objc, objv)
     switch ((enum options) index) {
 	case ENC_CONVERTTO:
 	case ENC_CONVERTFROM: {
-	    char *name;
 	    Tcl_Obj *data;
 	    if (objc == 3) {
-		name = NULL;
+		encoding = Tcl_GetEncoding(interp, NULL);
 		data = objv[2];
 	    } else if (objc == 4) {
-		name = Tcl_GetString(objv[2]);
+		if (TclGetEncodingFromObj(interp, objv[2], &encoding)
+			!= TCL_OK) {
+		    return TCL_ERROR;
+		}
 		data = objv[3];
 	    } else {
 		Tcl_WrongNumArgs(interp, 2, objv, "?encoding? data");
 		return TCL_ERROR;
 	    }
 	    
-	    encoding = Tcl_GetEncoding(interp, name);
-	    if (!encoding) {
-		return TCL_ERROR;
-	    }
-
 	    if ((enum options) index == ENC_CONVERTFROM) {
 		/*
 		 * Treat the string as binary data.
