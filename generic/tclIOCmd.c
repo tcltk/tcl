7accc4f0bd0282225df557f6bf216b7b53943247
donal.k.fellows@manchester.ac.uk <dkf>
2005-07-17 21:54:32 +0000

    D'oh

--- generic/tclIOCmd.c
+++ generic/tclIOCmd.c
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclIOCmd.c,v 1.28 2005/07/17 21:17:41 dkf Exp $
+ * RCS: @(#) $Id: tclIOCmd.c,v 1.29 2005/07/17 21:54:32 dkf Exp $
  */
 
 #include "tclInt.h"
@@ -1395,7 +1395,7 @@ Tcl_SocketObjCmd(notUsed, interp, objc, objv)
 	host = Tcl_GetString(objv[a]);
 	a++;
     } else {
-	Interp *iPtr;
+	Interp *iPtr = (Interp *) interp;
 
     wrongNumArgs:
 	Tcl_WrongNumArgs(interp, 1, objv,
