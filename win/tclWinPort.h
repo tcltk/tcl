3ec1d4efb6e7742a0b382484e5e695b2895285b7
hobbs <hobbs>
2005-10-05 08:03:35 +0000

    * tests/env.test (env-6.1): 	* win/tclWinPort.h: define USE_PUTENV_FOR_UNSET 1 	* generic/tclEnv.c (TclSetEnv, TclUnsetEnv): add 	USE_PUTENV_FOR_UNSET to existing USE_PUTENV define to account for 	various systems that have putenv(), but can't unset env vars with 	it.  Note difference between Windows and Linux for actually 	unsetting the env var (use of '='). 	Correct the resizing of the environ array.  We assume that we are 	in full ownership, but that's not correct.[Bug 979640]

--- win/tclWinPort.h
+++ win/tclWinPort.h
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclWinPort.h,v 1.44 2005/06/01 11:00:36 dkf Exp $
+ * RCS: @(#) $Id: tclWinPort.h,v 1.45 2005/10/05 08:03:35 hobbs Exp $
  */
 
 #ifndef _TCLWINPORT
@@ -445,7 +445,8 @@
  * the C level environment in synch with the system level environment.
  */
 
-#define USE_PUTENV	1
+#define USE_PUTENV		1
+#define USE_PUTENV_FOR_UNSET	1
 
 /*
  * Msvcrt's putenv() copies the string rather than takes ownership of it.
