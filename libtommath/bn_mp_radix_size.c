6b9c02979fe9734e07542c8a072f0de83e1cd19b
Kevin B Kenny <kennykb@acm.org>
2005-12-27 18:10:11 +0000

    Merge changes

--- libtommath/bn_mp_radix_size.c
+++ libtommath/bn_mp_radix_size.c
@@ -36,7 +36,7 @@ int mp_radix_size (mp_int * a, int radix, int *size)
   }
 
   if (mp_iszero(a) == MP_YES) {
-     *size = 2;
+    *size = 2;
     return MP_OKAY;
   }
 
@@ -83,5 +83,4 @@ int mp_radix_size (mp_int * a, int radix, int *size)
 #endif
 
 /* $Source: /root/tcl/repos-to-convert/tcl/libtommath/bn_mp_radix_size.c,v $ */
-/* $Revision: 1.3 $ */
-/* $Date: 2005/09/26 18:27:14 $ */
+/* Tom's revision is 1.3 */
