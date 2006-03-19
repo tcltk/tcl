ec41d80222b8608df8ee2792070461040b24b067
vincentdarley <vincentdarley>
2006-03-19 22:47:28 +0000

    backport of file writable fixes

--- generic/tclTest.c
+++ generic/tclTest.c
@@ -14,7 +14,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclTest.c,v 1.62.2.11 2005/12/15 04:08:26 das Exp $
+ * RCS: @(#) $Id: tclTest.c,v 1.62.2.12 2006/03/19 22:47:29 vincentdarley Exp $
  */
 
 #define TCL_TEST
@@ -207,8 +207,6 @@ static int		TestcmdinfoCmd _ANSI_ARGS_((ClientData dummy,
 static int		TestcmdtokenCmd _ANSI_ARGS_((ClientData dummy,
 			    Tcl_Interp *interp, int argc, CONST char **argv));
 static int		TestcmdtraceCmd _ANSI_ARGS_((ClientData dummy,
-			    Tcl_Interp *interp, int argc, CONST char **argv));
-static int		TestchmodCmd _ANSI_ARGS_((ClientData dummy,
 			    Tcl_Interp *interp, int argc, CONST char **argv));
 static int		TestcreatecommandCmd _ANSI_ARGS_((ClientData dummy,
 			    Tcl_Interp *interp, int argc, CONST char **argv));
@@ -580,8 +578,6 @@ Tcltest_Init(interp)
     Tcl_CreateCommand(interp, "testchannel", TestChannelCmd,
             (ClientData) 0, (Tcl_CmdDeleteProc *) NULL);
     Tcl_CreateCommand(interp, "testchannelevent", TestChannelEventCmd,
-            (ClientData) 0, (Tcl_CmdDeleteProc *) NULL);
-    Tcl_CreateCommand(interp, "testchmod", TestchmodCmd,
             (ClientData) 0, (Tcl_CmdDeleteProc *) NULL);
     Tcl_CreateCommand(interp, "testcmdtoken", TestcmdtokenCmd, (ClientData) 0,
 	    (Tcl_CmdDeleteProc *) NULL);
@@ -3999,65 +3995,6 @@ TestpanicCmd(dummy, interp, argc, argv)
     return TCL_OK;
 }
 
-/*
- *---------------------------------------------------------------------------
- *
- * TestchmodCmd --
- *
- *	Implements the "testchmod" cmd.  Used when testing "file"
- *	command.  The only attribute used by the Mac and Windows platforms
- *	is the user write flag; if this is not set, the file is
- *	made read-only.  Otehrwise, the file is made read-write.
- *
- * Results:
- *	A standard Tcl result.
- *
- * Side effects:
- *	Changes permissions of specified files.
- *
- *---------------------------------------------------------------------------
- */
- 
-static int
-TestchmodCmd(dummy, interp, argc, argv)
-    ClientData dummy;			/* Not used. */
-    Tcl_Interp *interp;			/* Current interpreter. */
-    int argc;				/* Number of arguments. */
-    CONST char **argv;			/* Argument strings. */
-{
-    int i, mode;
-    char *rest;
-
-    if (argc < 2) {
-	usage:
-	Tcl_AppendResult(interp, "wrong # args: should be \"", argv[0],
-		" mode file ?file ...?", (char *) NULL);
-	return TCL_ERROR;
-    }
-
-    mode = (int) strtol(argv[1], &rest, 8);
-    if ((rest == argv[1]) || (*rest != '\0')) {
-	goto usage;
-    }
-
-    for (i = 2; i < argc; i++) {
-        Tcl_DString buffer;
-	CONST char *translated;
-        
-        translated = Tcl_TranslateFileName(interp, argv[i], &buffer);
-        if (translated == NULL) {
-            return TCL_ERROR;
-        }
-	if (chmod(translated, (unsigned) mode) != 0) {
-	    Tcl_AppendResult(interp, translated, ": ", Tcl_PosixError(interp),
-		    (char *) NULL);
-	    return TCL_ERROR;
-	}
-        Tcl_DStringFree(&buffer);
-    }
-    return TCL_OK;
-}
-
 static int
 TestfileCmd(dummy, interp, argc, argv)
     ClientData dummy;			/* Not used. */
