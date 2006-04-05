d5f020bc80e7c2578764bd8c53f8dba426333323
dgp@users.sourceforge.net <dgp>
2006-04-05 16:52:56 +0000

    * library/reg/pkgIndex.tcl:     Bump to registry 1.2 because         * win/tclWinReg.c:      Registry_Unload() is a new public routine         * win/Makefile.in:      compared to the 1.1.* releases.
            * win/configure.in:     Bump package version numbers.
            * win/configure:        autoconf 2.59

--- win/tclWinReg.c
+++ win/tclWinReg.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclWinReg.c,v 1.36 2006/04/05 16:32:44 dgp Exp $
+ * RCS: @(#) $Id: tclWinReg.c,v 1.37 2006/04/05 16:52:56 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -238,7 +238,7 @@ Registry_Init(
     cmd = Tcl_CreateObjCommand(interp, "registry", RegistryObjCmd,
 	(ClientData)interp, DeleteCmd);
     Tcl_SetAssocData(interp, REGISTRY_ASSOC_KEY, NULL, (ClientData)cmd);
-    return Tcl_PkgProvide(interp, "registry", "1.1.6");
+    return Tcl_PkgProvide(interp, "registry", "1.2");
 }
 
 /*
