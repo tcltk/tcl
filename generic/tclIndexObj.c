76f4155bed63421e80bb3c574c6276fffa614d02
dgp@users.sourceforge.net <dgp>
2006-04-05 15:17:37 +0000

    * generic/tclIndexObj.c (Tcl_GetIndexFromObjStruct): Allow empty         strings to be matched by the Tcl_GetIndexFromObj machinery, in         the same manner as any other key. [Bug 1464039]

--- generic/tclIndexObj.c
+++ generic/tclIndexObj.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclIndexObj.c,v 1.29 2006/04/05 12:48:25 dkf Exp $
+ * RCS: @(#) $Id: tclIndexObj.c,v 1.30 2006/04/05 15:17:39 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -223,14 +223,10 @@ Tcl_GetIndexFromObjStruct(
     }
 
     /*
-     * Check if we were instructed to disallow abbreviations. Note that we do
-     * not allow the empty string as an abbreviation of anything; it is only
-     * processed by this function as a non-error case if the table of strings
-     * has an entry in it that is itself an empty string. This only matters in
-     * the case where the table has a singleton entry.
+     * Check if we were instructed to disallow abbreviations.
      */
 
-    if ((flags & TCL_EXACT) || (key[0] == '\0') || (numAbbrev != 1)) {
+    if ((flags & TCL_EXACT) || (numAbbrev != 1)) {
 	goto error;
     }
 
