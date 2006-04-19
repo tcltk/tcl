0a6ee6bc70975e81d1c46a39c443665fa500a20c
Kevin B Kenny <kennykb@acm.org>
2006-04-19 16:43:01 +0000

    fix for Martin Lemburg's DST conversion bug. http://groups.google.com/group/comp.lang.tcl/browse_thread/thread/9a8b15a4dfc0b7a0

--- library/clock.tcl
+++ library/clock.tcl
@@ -13,7 +13,7 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: clock.tcl,v 1.29 2005/12/27 17:39:02 kennykb Exp $
+# RCS: @(#) $Id: clock.tcl,v 1.30 2006/04/19 16:43:03 kennykb Exp $
 #
 #----------------------------------------------------------------------
 
@@ -3947,12 +3947,12 @@ proc ::tcl::clock::DeterminePosixDSTTime { z bound y } {
 	# Time was specified as a day of the week within a month
 
 	dict set date month [dict get $z ${bound}Month]
-	dict set date dayOfWeekInMonth [dict get $z ${bound}WeekOfMonth]
-	set dow [dict get $z ${bound}DayOfWeek]
-	if { $dow >= 5 } {
-	    set dow -1
+	dict set date dayOfWeek [dict get $z ${bound}DayOfWeek]
+	set dowim [dict get $z ${bound}WeekOfMonth]
+	if { $dowim >= 5 } {
+	    set dowim -1
 	}
-	dict set date dayOfWeek $dow
+	dict set date dayOfWeekInMonth $dowim
 	set date [GetJulianDayFromEraYearMonthWeekDay $date[set date {}] 2361222]
 
     }
