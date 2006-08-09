b38a450603626b71d0d5cd7c02e27baba12d9a98
dgp@users.sourceforge.net <dgp>
2006-08-09 18:12:41 +0000

    * generic/tclEncoding.c:        Replace buffer copy in for loop         with call to memcpy().  Thanks to afredd. [Patch 1530262]

--- generic/tclEncoding.c
+++ generic/tclEncoding.c
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclEncoding.c,v 1.16.2.10 2006/04/05 00:06:02 andreas_kupries Exp $
+ * RCS: @(#) $Id: tclEncoding.c,v 1.16.2.11 2006/08/09 18:12:42 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -1882,9 +1882,7 @@ BinaryProc(clientData, src, srcLen, flags, statePtr, dst, dstLen,
     *srcReadPtr = srcLen;
     *dstWrotePtr = srcLen;
     *dstCharsPtr = srcLen;
-    for ( ; --srcLen >= 0; ) {
-	*dst++ = *src++;
-    }
+    memcpy((void *) dst, (void *) src, (size_t) srcLen);
     return result;
 }
 
