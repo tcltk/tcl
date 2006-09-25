9c03213885f33e5070777fd10fa8fccbc2afcd5e
kennykb <kennykb>
2006-09-25 14:59:43 +0000

    yet another stab at 1564777

--- generic/tclNotify.c
+++ generic/tclNotify.c
@@ -14,7 +14,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclNotify.c,v 1.23 2006/09/25 14:58:03 kennykb Exp $
+ * RCS: @(#) $Id: tclNotify.c,v 1.24 2006/09/25 14:59:43 kennykb Exp $
  */
 
 #include "tclInt.h"
@@ -561,7 +561,7 @@ Tcl_DeleteEvents(
 	    hold = evPtr;
 	    evPtr = evPtr->nextPtr;
 	    ckfree((char *) hold);
-
+
 	} else {
 
 	    /* Event is to be retained. */
