63819167063bbaa8f911319dfe1186f810be2770
dgp <dgp@users.sourceforge.net>
2006-10-23 19:42:17 +0000

    * README:               Bump version number to 8.5a6         * generic/tcl.h:         * tools/tcl.wse.in:         * unix/configure.in:         * unix/tcl.spec:         * win/README.binary:         * win/configure.in:
            * unix/configure:       autoconf-2.59
            * win/configure:

--- generic/tcl.h
+++ generic/tcl.h
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.220 2006/10/22 00:13:29 msofer Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.221 2006/10/23 19:42:18 dgp Exp $
  */
 
 #ifndef _TCL
@@ -63,10 +63,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   5
 #define TCL_RELEASE_LEVEL   TCL_ALPHA_RELEASE
-#define TCL_RELEASE_SERIAL  5
+#define TCL_RELEASE_SERIAL  6
 
 #define TCL_VERSION	    "8.5"
-#define TCL_PATCH_LEVEL	    "8.5a5"
+#define TCL_PATCH_LEVEL	    "8.5a6"
 
 /*
  * The following definitions set up the proper options for Windows compilers.
