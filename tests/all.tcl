ab1c4492da589018017db574cbe1e7121db6e233
hobbs <hobbs>
2006-11-03 00:34:51 +0000

    * doc/ParseCmd.3, doc/Tcl.n, doc/eval.n, doc/exec.n: 	* doc/fconfigure.n, doc/interp.n, doc/unknown.n: 	* library/auto.tcl, library/init.tcl, library/package.tcl: 	* library/safe.tcl, library/tm.tcl, library/msgcat/msgcat.tcl: 	* tests/all.tcl, tests/basic.test, tests/cmdInfo.test: 	* tests/compile.test, tests/encoding.test, tests/execute.test: 	* tests/fCmd.test, tests/http.test, tests/init.test: 	* tests/interp.test, tests/io.test, tests/ioUtil.test: 	* tests/iogt.test, tests/namespace-old.test, tests/namespace.test: 	* tests/parse.test, tests/pkg.test, tests/pkgMkIndex.test: 	* tests/proc.test, tests/reg.test, tests/trace.test: 	* tests/upvar.test, tests/winConsole.test, tests/winFCmd.test: 	* tools/tclZIC.tcl: 	* generic/tclParse.c (Tcl_ParseCommand): Replace {expand} with {*} 	officially (TIP #293).  Leave -DALLOW_EXPAND=0|1 option to keep 	{expand} syntax for transition users. [Bug 1589629]

--- tests/all.tcl
+++ tests/all.tcl
@@ -10,10 +10,10 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 # 
-# RCS: @(#) $Id: all.tcl,v 1.18 2004/10/30 02:16:52 dgp Exp $
+# RCS: @(#) $Id: all.tcl,v 1.19 2006/11/03 00:34:52 hobbs Exp $
 
 package require Tcl 8.5
 package require tcltest 2.2
 namespace import tcltest::*
-configure {expand}$argv -testdir [file dir [info script]]
+configure {*}$argv -testdir [file dir [info script]]
 runAllTests
