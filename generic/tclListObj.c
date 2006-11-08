d3c98cb4ac5b38b9fadbe00e5b9161c19d4d4ea7
dkf <donal.k.fellows@manchester.ac.uk>
2006-11-08 13:47:07 +0000

    Minor simplification of memcpy usage

--- generic/tclListObj.c
+++ generic/tclListObj.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclListObj.c,v 1.33 2006/08/10 12:15:31 dkf Exp $
+ * RCS: @(#) $Id: tclListObj.c,v 1.34 2006/11/08 13:47:07 dkf Exp $
  */
 
 #include "tclInt.h"
@@ -859,8 +859,7 @@ Tcl_ListObjReplace(
 	     */
 
 	    if (first > 0) {
-		memcpy((VOID *) elemPtrs, (VOID *) oldPtrs,
-			(size_t) (first * sizeof(Tcl_Obj *)));
+		memcpy(elemPtrs, oldPtrs, (size_t) first * sizeof(Tcl_Obj *));
 	    }
 
 	    /*
@@ -880,9 +879,8 @@ Tcl_ListObjReplace(
 	    start = (first + count);
 	    numAfterLast = (numElems - start);
 	    if (numAfterLast > 0) {
-		memcpy((VOID *) &(elemPtrs[first + objc]),
-			(VOID *) &(oldPtrs[start]),
-			(size_t) (numAfterLast * sizeof(Tcl_Obj *)));
+		memcpy(elemPtrs + first + objc, oldPtrs + start,
+			(size_t) numAfterLast * sizeof(Tcl_Obj *));
 	    }
 
 	    ckfree((char *) oldListRepPtr);
@@ -1563,7 +1561,7 @@ SetListFromAny(
 
 	s = ckalloc((unsigned) elemSize + 1);
 	if (hasBrace) {
-	    memcpy((void *) s, (void *) elemStart, (size_t) elemSize);
+	    memcpy(s, elemStart, (size_t) elemSize);
 	    s[elemSize] = 0;
 	} else {
 	    elemSize = TclCopyAndCollapse(elemSize, elemStart, s);
