2552c3b55665456e393c36dc42e1dd515ad721f4
das <das>
2006-12-01 06:06:01 +0000

    fix warnings

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCompile.c,v 1.101 2006/11/28 22:20:28 andreas_kupries Exp $
+ * RCS: @(#) $Id: tclCompile.c,v 1.102 2006/12/01 06:06:01 das Exp $
  */
 
 #include "tclInt.h"
@@ -1281,8 +1281,8 @@ TclCompileScript(
 
 			TclMarkList (interp, start, end,
 				     &(exp     [wordIdx]),
-				     &(expLen  [wordIdx]),
-				     &(expItem [wordIdx]));
+				     (CONST int**)&(expLen  [wordIdx]),
+				     (CONST char ***)&(expItem [wordIdx]));
 
 			eliterals += exp [wordIdx] ? exp[wordIdx] : 1;
 
