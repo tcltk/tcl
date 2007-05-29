52f149dff3bf97d543f3ac4d2f0115d6e847ce00
dgp@users.sourceforge.net <dgp>
2007-05-29 14:05:53 +0000

    restore line endings

--- unix/dltest/pkgb.c
+++ unix/dltest/pkgb.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: pkgb.c,v 1.6 2007/05/02 21:28:09 dkf Exp $
+ * RCS: @(#) $Id: pkgb.c,v 1.7 2007/05/29 14:05:53 dgp Exp $
  */
 #include "tcl.h"
 
