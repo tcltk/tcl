0ea1a219a60d3bc60a8ee100c215597ee9d3e9d7
Miguel Sofer <miguel.sofer@gmail.com>
2007-06-15 19:58:12 +0000

    * generic/tclCompile.c: reverted TclEvalObjvInternal and 	* generic/tclExecute.c: INST_INVOKE to essentially what they were 	* generic/tclBasic.c:   previous to the commit of 2007-04-03 	[Patch 1693802] and the subsequent optimisations, as they break 	the new trace tests described below.
    	* generic/trace.test: added tests 36 to 38 for dynamic trace
    	creation and addition. These tests expose a change in dynamics due
    	to a recent round of optimisations. The "correct" behaviour is not
    	described in docs nor TIP 62.

--- generic/tclCompile.h
+++ generic/tclCompile.h
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCompile.h,v 1.72 2007/06/10 23:15:05 msofer Exp $
+ * RCS: @(#) $Id: tclCompile.h,v 1.73 2007/06/15 19:58:13 msofer Exp $
  */
 
 #ifndef _TCLCOMPILATION
@@ -808,9 +808,6 @@ typedef struct {
 MODULE_SCOPE int	TclEvalObjvInternal(Tcl_Interp *interp,
 			    int objc, Tcl_Obj *const objv[],
 			    CONST char *command, int length, int flags);
-MODULE_SCOPE int	TclEvalObjvKnownCommand(Tcl_Interp *interp,
-	                    int objc, Tcl_Obj *const objv[],
-	                    Command *cmdPtr);
 /*
  *----------------------------------------------------------------
  * Procedures exported by the engine to be used by tclBasic.c
