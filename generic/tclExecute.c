cde53eef4a50075210e17e328d9431c7b71cec6d
dgp@users.sourceforge.net <dgp>
2007-06-22 17:09:15 +0000

    * generic/tclExecute.c: Revised TclStackRealloc() signature to better         * generic/tclInt.h:     parallel (and fall back on) Tcl_Realloc.
            * generic/tclNamesp.c (TclResetShadowesCmdRefs):        Replaced
            ckrealloc based allocations with TclStackRealloc allocations.

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclExecute.c,v 1.302 2007/06/21 17:45:40 msofer Exp $
+ * RCS: @(#) $Id: tclExecute.c,v 1.303 2007/06/22 17:09:17 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -444,8 +444,6 @@ static void		DeleteExecStack(ExecStack *esPtr);
 static Tcl_Obj **	StackAllocWords(Tcl_Interp *interp, int numWords);
 static Tcl_Obj **	StackReallocWords(Tcl_Interp *interp, int numWords);
 
-/* Move to internal stubs? For now, unused */
-extern char *		TclStackRealloc(Tcl_Interp *interp, int numBytes);
 
 /*
  *----------------------------------------------------------------------
@@ -878,14 +876,33 @@ TclStackAlloc(
     return (void *) StackAllocWords(interp, numWords);
 }
 
-char *
+void *
 TclStackRealloc(
     Tcl_Interp *interp,
+    void *ptr,
     int numBytes)
 {
-    int numWords = (numBytes + sizeof(void *) - 1)/sizeof(void *);
+    Interp *iPtr;
+    ExecEnv *eePtr;
+    ExecStack *esPtr;
+    Tcl_Obj **markerPtr;
+    int numWords;
 
-    return (char *) StackReallocWords(interp, numWords);
+    if (interp == NULL) {
+	return (void *) Tcl_Realloc((char *) ptr, numBytes);
+    }
+
+    iPtr = (Interp *) interp;
+    eePtr = iPtr->execEnvPtr;
+    esPtr = eePtr->execStackPtr;
+    markerPtr = esPtr->markerPtr;
+
+    if ((markerPtr+1) != (Tcl_Obj **)ptr) {
+	Tcl_Panic("TclStackRealloc: incorrect ptr.  Call out of sequence?");
+    }
+
+    numWords = (numBytes + (sizeof(Tcl_Obj *) - 1))/sizeof(Tcl_Obj *);
+    return (void *) StackReallocWords(interp, numWords);
 }
 
 /*
