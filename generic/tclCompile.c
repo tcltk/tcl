739dddcdc3c5a8aaace337b88988f589060e2046
Miguel Sofer <miguel.sofer@gmail.com>
2007-09-10 21:47:19 +0000

    * generic/tclCompile.c: fix tclInstructionTable entry for 	dictUpdateEnd
    	* generic/tclExecute.c: remove unneeded setting of 'cleanup'
    	variable before jumping to checkForCatch.

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCompile.c,v 1.131 2007/09/09 17:02:33 dgp Exp $
+ * RCS: @(#) $Id: tclCompile.c,v 1.132 2007/09/10 21:47:20 msofer Exp $
  */
 
 #include "tclInt.h"
@@ -358,7 +358,7 @@ InstructionDesc tclInstructionTable[] = {
 	 * of keys (popped from the stack) must be the same length as the list
 	 * of variables.
 	 * Stack:  ... keyList => ... */
-    {"dictUpdateEnd",	  9,    -1,	   1,	{OPERAND_LVT4, OPERAND_AUX4}},
+    {"dictUpdateEnd",	  9,    -1,	   2,	{OPERAND_LVT4, OPERAND_AUX4}},
 	/* Reflect the state of local variables (described in the aux data
 	 * referred to by the second immediate argument) back to the state of
 	 * the dictionary in the variable referred to by the first immediate
