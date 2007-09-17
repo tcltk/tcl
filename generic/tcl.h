8d21468c9b4009bc55790cc75194bb1b70fdb6bd
dgp <dgp>
2007-09-17 14:50:42 +0000

    * generic/tcl.h:        Revised Tcl_InitStubs() to restore Tcl 8.4         * generic/tclPkg.c:     source compatibility with callers of         * generic/tclStubLib.c: Tcl_InitStubs(interp, TCL_VERSION, 1).         [Bug 1578344].

--- generic/tcl.h
+++ generic/tcl.h
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.234 2007/07/31 17:03:35 msofer Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.235 2007/09/17 14:50:43 dgp Exp $
  */
 
 #ifndef _TCL
@@ -2216,7 +2216,7 @@ EXTERN CONST char*	TclTomMathInitializeStubs(Tcl_Interp* interp,
  */
 
 #define Tcl_InitStubs(interp, version, exact) \
-    Tcl_PkgRequire(interp, "Tcl", version, exact)
+    Tcl_PkgInitStubsCheck(interp, version, exact)
 
 #endif
 
@@ -2231,6 +2231,9 @@ EXTERN CONST char*	TclTomMathInitializeStubs(Tcl_Interp* interp,
 
 EXTERN void Tcl_Main _ANSI_ARGS_((int argc, char **argv,
 	Tcl_AppInitProc *appInitProc));
+
+EXTERN CONST char *Tcl_PkgInitStubsCheck _ANSI_ARGS_((Tcl_Interp *interp,
+			    CONST char *version, int exact));
 
 /*
  * Include the public function declarations that are accessible via the stubs
