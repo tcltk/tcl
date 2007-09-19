186faaabe6ce03309afbf2f1bc6a412ad7d9a357
dgp <dgp>
2007-09-19 17:42:04 +0000

    * README:               Bump version number to 8.5b1         * generic/tcl.h:        Merge from core-stabilizer-branch.         * library/init.tcl:     Stabilizing toward 8.5b1 release now done         * tools/tcl.wse.in:     on the HEAD.  core-stabilizer-branch is         * unix/configure.in:    now suspended.         * unix/tcl.spec:         * win/configure.in:

--- library/init.tcl
+++ library/init.tcl
@@ -3,7 +3,7 @@
 # Default system startup file for Tcl-based applications.  Defines
 # "unknown" procedure and auto-load facilities.
 #
-# RCS: @(#) $Id: init.tcl,v 1.93 2007/08/23 20:00:20 dgp Exp $
+# RCS: @(#) $Id: init.tcl,v 1.94 2007/09/19 17:42:11 dgp Exp $
 #
 # Copyright (c) 1991-1993 The Regents of the University of California.
 # Copyright (c) 1994-1996 Sun Microsystems, Inc.
@@ -17,7 +17,7 @@
 if {[info commands package] == ""} {
     error "version mismatch: library\nscripts expect Tcl version 7.5b1 or later but the loaded version is\nonly [info patchlevel]"
 }
-package require -exact Tcl 8.5a7
+package require -exact Tcl 8.5b1
 
 # Compute the auto path to use in this interpreter.
 # The values on the path come from several locations:
