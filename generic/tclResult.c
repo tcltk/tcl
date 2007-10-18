30c0b8730e43299b5ef40df635465de692804c28
dgp <dgp>
2007-10-18 21:16:17 +0000

    * generic/tclResult.c (TclMergeReturnOptions):  Make sure any -code         values get pulled out of the dictionary, even if they are integer         valued.
            * generic/tclCompCmds.c (TclCompileReturnCmd):  Added code to
            more optimally compile [return -level 0 $x] to "push $x". [RFE 1794073]

--- generic/tclResult.c
+++ generic/tclResult.c
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclResult.c,v 1.38 2007/09/07 15:51:26 dgp Exp $
+ * RCS: @(#) $Id: tclResult.c,v 1.39 2007/10/18 21:16:18 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -1352,6 +1352,8 @@ TclMergeReturnOptions(
 		    "continue, or an integer", NULL);
 	    goto error;
 	}
+    }
+    if (valuePtr != NULL) {
 	Tcl_DictObjRemove(NULL, returnOpts, keys[KEY_CODE]);
     }
 
