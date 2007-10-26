1261c4d03e8f217c4b5fcb3665474f4b9be52b39
dgp@users.sourceforge.net <dgp>
2007-10-26 15:17:53 +0000

    * README:               Bump version number to 8.5b2.         * generic/tcl.h:         * library/init.tcl:         * tools/tcl.wse.in:         * unix/configure.in:         * unix/tcl.spec:         * win/configure.in:
            * unix/configure:       autoconf (2.59)
            * win/configure:

--- library/init.tcl
+++ library/init.tcl
@@ -3,7 +3,7 @@
 # Default system startup file for Tcl-based applications.  Defines
 # "unknown" procedure and auto-load facilities.
 #
-# RCS: @(#) $Id: init.tcl,v 1.95 2007/10/02 18:27:30 dgp Exp $
+# RCS: @(#) $Id: init.tcl,v 1.96 2007/10/26 15:17:56 dgp Exp $
 #
 # Copyright (c) 1991-1993 The Regents of the University of California.
 # Copyright (c) 1994-1996 Sun Microsystems, Inc.
@@ -17,7 +17,7 @@
 if {[info commands package] == ""} {
     error "version mismatch: library\nscripts expect Tcl version 7.5b1 or later but the loaded version is\nonly [info patchlevel]"
 }
-package require -exact Tcl 8.5b1.1
+package require -exact Tcl 8.5b2
 
 # Compute the auto path to use in this interpreter.
 # The values on the path come from several locations:
