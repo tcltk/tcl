e781ae26f0348107cdad2baf793334449de5ab27
hobbs <hobbs>
2007-11-08 00:50:31 +0000

    * generic/tclStubInit.c: 	* generic/tclInt.decls, generic/tclIntDecls.h: added TclByteArrayMatch 	* generic/tclUtil.c (TclByteArrayMatch):       for efficient glob 	* generic/tclExecute.c (TclExecuteByteCode):   matching of ByteArray 	Tcl_Objs, used in INST_STR_MATCH. [Bug 1827996]

--- generic/tclIntDecls.h
+++ generic/tclIntDecls.h
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclIntDecls.h,v 1.105 2007/09/06 18:13:20 dgp Exp $
+ * RCS: @(#) $Id: tclIntDecls.h,v 1.106 2007/11/08 00:50:32 hobbs Exp $
  */
 
 #ifndef _TCLINTDECLS
@@ -1057,6 +1057,12 @@ EXTERN void		TclInitVarHashTable (TclVarHashTable * tablePtr,
 EXTERN void		TclBackgroundException (Tcl_Interp * interp, 
 				int code);
 #endif
+#ifndef TclByteArrayMatch_TCL_DECLARED
+#define TclByteArrayMatch_TCL_DECLARED
+/* 237 */
+EXTERN int		TclByteArrayMatch (CONST char * string, int strLen, 
+				CONST char * pattern, int ptnLen, int nocase);
+#endif
 
 typedef struct TclIntStubs {
     int magic;
@@ -1314,6 +1320,7 @@ typedef struct TclIntStubs {
     Var * (*tclVarHashCreateVar) (TclVarHashTable * tablePtr, const char * key, int * newPtr); /* 234 */
     void (*tclInitVarHashTable) (TclVarHashTable * tablePtr, Namespace * nsPtr); /* 235 */
     void (*tclBackgroundException) (Tcl_Interp * interp, int code); /* 236 */
+    int (*tclByteArrayMatch) (CONST char * string, int strLen, CONST char * pattern, int ptnLen, int nocase); /* 237 */
 } TclIntStubs;
 
 #ifdef __cplusplus
@@ -2046,6 +2053,10 @@ extern TclIntStubs *tclIntStubsPtr;
 #ifndef TclBackgroundException
 #define TclBackgroundException \
 	(tclIntStubsPtr->tclBackgroundException) /* 236 */
+#endif
+#ifndef TclByteArrayMatch
+#define TclByteArrayMatch \
+	(tclIntStubsPtr->tclByteArrayMatch) /* 237 */
 #endif
 
 #endif /* defined(USE_TCL_STUBS) && !defined(USE_TCL_STUB_PROCS) */
