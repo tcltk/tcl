8a9e4fb81b61e1bfda6de94889397b54cb559792
dgp@users.sourceforge.net <dgp>
2007-11-19 18:14:48 +0000

    * README:               Bump version number to 8.5b3.         * generic/tcl.h:         * library/init.tcl:         * tools/tcl.wse.in:         * unix/configure.in:         * unix/tcl.spec:         * win/configure.in:
            * unix/configure:       autoconf (2.59)
            * win/configure:

--- generic/tcl.h
+++ generic/tcl.h
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.243 2007/10/30 19:03:56 dgp Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.244 2007/11/19 18:14:48 dgp Exp $
  */
 
 #ifndef _TCL
@@ -60,10 +60,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   5
 #define TCL_RELEASE_LEVEL   TCL_BETA_RELEASE
-#define TCL_RELEASE_SERIAL  2
+#define TCL_RELEASE_SERIAL  3
 
 #define TCL_VERSION	    "8.5"
-#define TCL_PATCH_LEVEL	    "8.5b2.1"
+#define TCL_PATCH_LEVEL	    "8.5b3"
 
 /*
  * The following definitions set up the proper options for Windows compilers.
--- unix/tcl.spec
+++ unix/tcl.spec
@@ -1,11 +1,11 @@
-# $Id: tcl.spec,v 1.31 2007/10/30 19:03:58 dgp Exp $
+# $Id: tcl.spec,v 1.32 2007/11/19 18:14:51 dgp Exp $
 # This file is the basis for a binary Tcl RPM for Linux.
 
 %{!?directory:%define directory /usr/local}
 
 Name:          tcl
 Summary:       Tcl scripting language development environment
-Version:       8.5b2.1
+Version:       8.5b3
 Release:       2
 License:       BSD
 Group:         Development/Languages
