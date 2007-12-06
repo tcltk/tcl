848feb14b7437643dc34ed0abb7ef75a2ae8da8c
hobbs <hobbs>
2007-12-06 19:23:39 +0000

    add TclGetChannelFromObj decl

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclInt.h,v 1.354 2007/12/06 16:14:33 dkf Exp $
+ * RCS: @(#) $Id: tclInt.h,v 1.355 2007/12/06 19:23:40 hobbs Exp $
  */
 
 #ifndef _TCLINT
@@ -2468,6 +2468,9 @@ MODULE_SCOPE int	TclFSFileAttrIndex(Tcl_Obj *pathPtr,
 			    const char *attributeName, int *indexPtr);
 MODULE_SCOPE int *      TclGetAsyncReadyPtr(void);
 MODULE_SCOPE Tcl_Obj *	TclGetBgErrorHandler(Tcl_Interp *interp);
+MODULE_SCOPE int	TclGetChannelFromObj(Tcl_Interp *interp,
+			    Tcl_Obj *objPtr, Tcl_Channel *chanPtr,
+			    int *modePtr, int flags);
 MODULE_SCOPE int	TclGetNumberFromObj(Tcl_Interp *interp,
 			    Tcl_Obj *objPtr, ClientData *clientDataPtr,
 			    int *typePtr);
