e8f0dd74ac1caadb6eee359f46b680a721e7f5ad
dgp <dgp@users.sourceforge.net>
2008-01-31 03:07:09 +0000

    merge updates from HEAD

--- generic/tclInterp.c
+++ generic/tclInterp.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclInterp.c,v 1.22.2.24 2007/12/06 06:51:40 dgp Exp $
+ * RCS: @(#) $Id: tclInterp.c,v 1.22.2.25 2008/01/31 03:07:10 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -1193,7 +1193,7 @@ Tcl_GetAlias(
 	*argvPtr = (const char **)
 		ckalloc((unsigned) sizeof(const char *) * (objc - 1));
 	for (i = 1; i < objc; i++) {
-	    *argvPtr[i - 1] = TclGetString(objv[i]);
+	    (*argvPtr)[i - 1] = TclGetString(objv[i]);
 	}
     }
     return TCL_OK;
