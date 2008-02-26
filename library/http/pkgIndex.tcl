4cf28d0b54e8647b6f1ebe2eb8668f57b3dec65e
patthoyts@users.sourceforge.net <patthoyts>
2008-02-26 19:52:52 +0000

    Set version 2.5.5 bug #1868845 - it is better to do the [eof] check after trying to read from the socket. No clashes found in testing.  Added http::meta command to access the http headers.

--- library/http/pkgIndex.tcl
+++ library/http/pkgIndex.tcl
@@ -9,4 +9,4 @@
 # full path name of this file's directory.
 
 if {![package vsatisfies [package provide Tcl] 8.4]} {return}
-package ifneeded http 2.5.4 [list tclPkgSetup $dir http 2.5.4 {{http.tcl source {::http::config ::http::formatQuery ::http::geturl ::http::reset ::http::wait ::http::register ::http::unregister ::http::mapReply}}}]
+package ifneeded http 2.5.5 [list tclPkgSetup $dir http 2.5.5 {{http.tcl source {::http::config ::http::formatQuery ::http::geturl ::http::reset ::http::wait ::http::register ::http::unregister ::http::mapReply}}}]
