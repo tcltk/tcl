289c72bd9d2fc8e7c469803a9f020636c6267c1f
dgp <dgp>
2008-04-11 16:57:32 +0000

    * README:               Bump version number to 8.4.19         * generic/tcl.h:         * tools/tcl.wse.in:         * unix/configure.in:         * unix/tcl.spec:         * win/README.binary:         * win/configure.in:
            * unix/configure:       autoconf-2.13
            * win/configure:
    
            * changes:              updates for 8.4.19 release.

--- generic/tcl.h
+++ generic/tcl.h
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.153.2.34 2008/02/06 15:25:15 dgp Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.153.2.35 2008/04/11 16:57:38 dgp Exp $
  */
 
 #ifndef _TCL
@@ -59,10 +59,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   4
 #define TCL_RELEASE_LEVEL   TCL_FINAL_RELEASE
-#define TCL_RELEASE_SERIAL  18
+#define TCL_RELEASE_SERIAL  19
 
 #define TCL_VERSION	    "8.4"
-#define TCL_PATCH_LEVEL	    "8.4.18"
+#define TCL_PATCH_LEVEL	    "8.4.19"
 
 /*
  * The following definitions set up the proper options for Windows
