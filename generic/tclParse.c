89a9e0dd71bca0ecc5f87508db1b101eda76688d
dgp <dgp@users.sourceforge.net>
2008-05-21 20:38:06 +0000

    * generic/tclParse.c (ParseComment):    The new TclParseAllWhiteSpace()         * tests/parse.test (parse-15.60):       routine has no mechanism to         return the "incomplete" status of "\\\n" so calling this routine         anywhere that can be reached within a Tcl_ParseCommand() call is a         mistake.  In particular, ParseComment() must not use it.  [Bug 1968882].

--- generic/tclParse.c
+++ generic/tclParse.c
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclParse.c,v 1.62 2008/01/23 21:58:36 dgp Exp $
+ * RCS: @(#) $Id: tclParse.c,v 1.62.2.1 2008/05/21 20:38:09 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -954,9 +954,12 @@ ParseComment(
 	char type;
 	int scanned;
 
-	scanned = TclParseAllWhiteSpace(p, numBytes);
-	p += scanned;
-	numBytes -= scanned;
+	do {
+	    scanned = ParseWhiteSpace(p, numBytes,
+		    &parsePtr->incomplete, &type);
+	    p += scanned;
+	    numBytes -= scanned;
+	} while (numBytes && (*p == '\n') && (p++,numBytes--));
 
 	if ((numBytes == 0) || (*p != '#')) {
 	    break;
@@ -1871,7 +1874,7 @@ Tcl_SubstObj(
     int length, tokensLeft, code;
     Tcl_Token *endTokenPtr;
     Tcl_Obj *result, *errMsg = NULL;
-    CONST char *p = TclGetStringFromObj(objPtr, &length);
+    const char *p = TclGetStringFromObj(objPtr, &length);
     Tcl_Parse *parsePtr = (Tcl_Parse *)
 	    TclStackAlloc(interp, sizeof(Tcl_Parse));
 
