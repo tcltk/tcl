8f8c4fc92943cd0497881b0185ec142142698b4b
dgp <dgp@users.sourceforge.net>
2008-06-20 20:48:47 +0000

    * changes:      Updates for 8.6a1 release.
            * generic/tclInterp.c:  Fixed completely boneheaded mistake that
            * tests/interp.test:    [interp bgerror $slave] and [$slave bgerror]
            would always act like [interp bgerror {}].  [Bug 1999035].
    
            * tests/chanio.test:    Corrected flawed tests revealed by a -debug 1
            * tests/cmdAH.test:     -singleproc 1 test suite run.
            * tests/event.test:
            * tests/interp.test:
            * tests/io.test:
            * tests/ioTrans.test:
            * tests/namespace.test:

--- generic/tclInterp.c
+++ generic/tclInterp.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclInterp.c,v 1.85 2008/06/13 05:45:13 mistachkin Exp $
+ * RCS: @(#) $Id: tclInterp.c,v 1.86 2008/06/20 20:48:47 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -2130,9 +2130,9 @@ SlaveBgerror(
 		    NULL);
 	    return TCL_ERROR;
 	}
-	TclSetBgErrorHandler(interp, objv[0]);
+	TclSetBgErrorHandler(slaveInterp, objv[0]);
     }
-    Tcl_SetObjResult(interp, TclGetBgErrorHandler(interp));
+    Tcl_SetObjResult(interp, TclGetBgErrorHandler(slaveInterp));
     return TCL_OK;
 }
 
