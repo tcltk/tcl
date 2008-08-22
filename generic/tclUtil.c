89caec97fd7d5e85719e6a561e81ca0b5204b09a
dgp <dgp@users.sourceforge.net>
2008-08-22 17:20:35 +0000

    * generic/tclUtil.c (TclReToGlob):      Added missing set of the         *exactPtr value to really fix [Bug 2065115].         * tests/regexpComp.test:        Correct duplicate test names.

--- generic/tclUtil.c
+++ generic/tclUtil.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclUtil.c,v 1.97.2.3 2008/08/21 23:42:22 hobbs Exp $
+ * RCS: @(#) $Id: tclUtil.c,v 1.97.2.4 2008/08/22 17:20:37 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -3300,6 +3300,9 @@ TclReToGlob(
 	}
 	*dsStr++ = '*';
 	Tcl_DStringSetLength(dsPtr, dsStr - dsStrStart);
+	if (exactPtr) {
+	    *exactPtr = 0;
+	}
 	return TCL_OK;
     }
 
