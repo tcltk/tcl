d4b00565523814090bf2a6ed78b5e3bf01648487
Joe Mistachkin <joe@mistachkin.com>
2008-10-06 18:42:11 +0000

    Fix for [Bug 1934200]

--- tools/man2tcl.c
+++ tools/man2tcl.c
@@ -15,7 +15,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: man2tcl.c,v 1.13.2.1 2008/10/02 18:56:30 mistachkin Exp $
+ * RCS: @(#) $Id: man2tcl.c,v 1.13.2.2 2008/10/06 18:42:11 mistachkin Exp $
  */
 
 static char sccsid[] = "@(#) man2tcl.c 1.3 95/08/12 17:34:08";
@@ -352,6 +352,7 @@ DoText(
 		sscanf(p,"%d",&ch);
 		PRINT(("text \\u%04x\n", ch));
 		while(*p&&*p!='\'') p++;
+		p++;  
 	    } else if (*p != 0) {
 		PRINT(("char {\\%c}\n", *p));
 		p++;
