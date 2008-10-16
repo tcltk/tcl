39b6d092d658e887cf880a14a10c6f3d67aea183
dgp@users.sourceforge.net <dgp>
2008-10-16 16:07:06 +0000

    * library/init.tcl:     Revised [unknown] so that it carefully         preserves the state of the ::errorInfo and ::errorCode variables         at the start of auto-loading and restores that state before the         autoloaded command is evaluated.  [Bug 2140628]

--- library/init.tcl
+++ library/init.tcl
@@ -3,7 +3,7 @@
 # Default system startup file for Tcl-based applications.  Defines
 # "unknown" procedure and auto-load facilities.
 #
-# RCS: @(#) $Id: init.tcl,v 1.104.2.7 2008/10/10 18:16:48 dgp Exp $
+# RCS: @(#) $Id: init.tcl,v 1.104.2.8 2008/10/16 16:07:06 dgp Exp $
 #
 # Copyright (c) 1991-1993 The Regents of the University of California.
 # Copyright (c) 1994-1996 Sun Microsystems, Inc.
@@ -276,8 +276,13 @@ proc unknown args {
 	    unset UnknownPending
 	}
 	if {$msg} {
-	    catch {set ::errorCode $savedErrorCode}
-	    catch {set ::errorInfo $savedErrorInfo}
+	    unset -nocomplain ::errorCode ::errorInfo
+	    if {[info exists savedErrorCode]} {
+		set ::errorCode $savedErrorCode
+	    }
+	    if {[info exists savedErrorInfo]} {
+		set ::errorInfo $savedErrorInfo
+	    }
 	    set code [catch {uplevel 1 $args} msg opts]
 	    if {$code ==  1} {
 		#
