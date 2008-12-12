38737f9e1aae2c011fa9b3f0bb33697c5ec6dc6c
ferrieux@users.sourceforge.net <ferrieux>
2008-12-12 16:07:17 +0000

    Fix missing CLOEXEC on internal pipes [2417695] and [chan pipe] fds.

--- unix/tclUnixPipe.c
+++ unix/tclUnixPipe.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclUnixPipe.c,v 1.44 2008/07/21 21:46:47 das Exp $
+ * RCS: @(#) $Id: tclUnixPipe.c,v 1.45 2008/12/12 16:07:18 ferrieux Exp $
  */
 
 #include "tclInt.h"
@@ -797,6 +797,9 @@ Tcl_CreatePipe(
 		Tcl_PosixError(interp), NULL);
 	return TCL_ERROR;
     }
+
+    fcntl(fileNums[0], F_SETFD, FD_CLOEXEC);
+    fcntl(fileNums[1], F_SETFD, FD_CLOEXEC);
 
     *rchan = Tcl_MakeFileChannel((ClientData) INT2PTR(fileNums[0]),
 	    TCL_READABLE);
