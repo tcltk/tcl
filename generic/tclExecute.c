be8be38182e4b46e281707f2c7c4886c65ee7e62
dkf <dkf>
2008-12-15 23:26:58 +0000

    Fix [Bug 2431847]

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclExecute.c,v 1.369.2.4 2008/08/04 04:48:14 dgp Exp $
+ * RCS: @(#) $Id: tclExecute.c,v 1.369.2.5 2008/12/15 23:26:59 dkf Exp $
  */
 
 #include "tclInt.h"
@@ -6678,7 +6678,10 @@ TclExecuteByteCode(
 		    "%u => ERROR reading leaf dictionary key \"%s\": ",
 		    opnd, O2S(dictPtr)), Tcl_GetObjResult(interp));
 	} else {
-	    /*Tcl_ResetResult(interp);*/
+	    Tcl_Obj *tmpObj;
+
+	    TclNewObj(tmpObj);
+	    Tcl_SetObjResult(interp, tmpObj);
 	    Tcl_AppendResult(interp, "key \"", TclGetString(OBJ_AT_TOS),
 		    "\" not known in dictionary", NULL);
 	    TRACE_WITH_OBJ(("%u => ERROR ", opnd), Tcl_GetObjResult(interp));
