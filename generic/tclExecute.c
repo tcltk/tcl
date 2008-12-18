bd0f9912124aa482f498bd4e8cb2086dc04354cb
dgp <dgp@users.sourceforge.net>
2008-12-18 23:19:06 +0000

    merge updates from HEAD

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -14,7 +14,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclExecute.c,v 1.101.2.103 2008/12/17 06:02:47 dgp Exp $
+ * RCS: @(#) $Id: tclExecute.c,v 1.101.2.104 2008/12/18 23:19:07 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -2044,7 +2044,9 @@ TclExecuteByteCode(
 	 * reset, now process the return.
 	 */
 
-	NRE_ASSERT(iPtr->cmdFramePtr == bcFramePtr);
+	/* Disabled the following assertion to solve the trouble reported
+	 * in Tcl Bug 2415422.  Needs review.  */
+	/*NRE_ASSERT(iPtr->cmdFramePtr == bcFramePtr);*/
 	iPtr->cmdFramePtr = bcFramePtr->nextPtr;
 
 	if (iPtr->execEnvPtr->rewind) {
