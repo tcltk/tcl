ca5ce04256216055e2f325110f72896d40b7766c
dgp <dgp@users.sourceforge.net>
2008-12-19 03:37:12 +0000

    merge updates from HEAD

--- generic/tclIO.h
+++ generic/tclIO.h
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclIO.h,v 1.5.6.8 2008/10/11 03:37:27 dgp Exp $
+ * RCS: @(#) $Id: tclIO.h,v 1.5.6.9 2008/12/19 03:37:12 dgp Exp $
  */
 
 /*
@@ -339,6 +339,9 @@ typedef struct ChannelState {
 					 * Used by Channel Tcl_Obj type to
 					 * determine if we have to revalidate
 					 * the channel. */
+#define CHANNEL_CLOSEDWRITE	(1<<21)	/* Channel write side has been closed.
+					 * No further Tcl-level write IO on
+					 * the channel is allowed. */
 
 /*
  * For each channel handler registered in a call to Tcl_CreateChannelHandler,
