d4748c8b78688cff0a50c687eefc7835475aeb3f
nijtmans <nijtmans>
2009-01-22 06:42:33 +0000

    CONSTify TclPrintInstruction and TclpNativeJoinPath (TIP #27) {unix win} in *.decls is equivalent to {generic} tclGetDate.y, tclDate.c:   single internal const decoration

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -15,7 +15,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclInt.h,v 1.413 2009/01/13 20:30:03 dkf Exp $
+ * RCS: @(#) $Id: tclInt.h,v 1.414 2009/01/22 06:42:33 nijtmans Exp $
  */
 
 #ifndef _TCLINT
@@ -2792,7 +2792,7 @@ MODULE_SCOPE int	TclpMatchFiles(Tcl_Interp *interp, char *separators,
 			    Tcl_DString *dirPtr, char *pattern, char *tail);
 MODULE_SCOPE int	TclpObjNormalizePath(Tcl_Interp *interp,
 			    Tcl_Obj *pathPtr, int nextCheckpoint);
-MODULE_SCOPE void	TclpNativeJoinPath(Tcl_Obj *prefix, char *joining);
+MODULE_SCOPE void	TclpNativeJoinPath(Tcl_Obj *prefix, const char *joining);
 MODULE_SCOPE Tcl_Obj *	TclpNativeSplitPath(Tcl_Obj *pathPtr, int *lenPtr);
 MODULE_SCOPE Tcl_PathType TclpGetNativePathType(Tcl_Obj *pathPtr,
 			    int *driveNameLengthPtr, Tcl_Obj **driveNameRef);
