d9b1b123ba64711277d9443aa6d1431db4f819bb
dgp <dgp@users.sourceforge.net>
2009-03-06 17:09:22 +0000

    merge updates from HEAD

--- generic/tclZlib.c
+++ generic/tclZlib.c
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclZlib.c,v 1.4.2.17 2009/02/17 14:28:03 dgp Exp $
+ * RCS: @(#) $Id: tclZlib.c,v 1.4.2.18 2009/03/06 17:09:22 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -1692,7 +1692,8 @@ TclZlibCmd(
 	    start = Tcl_ZlibAdler32(0, NULL, 0);
 	}
 	data = Tcl_GetByteArrayFromObj(objv[2], &dlen);
-	Tcl_SetIntObj(obj, (int) Tcl_ZlibAdler32(start, data, dlen));
+	Tcl_SetWideIntObj(obj,
+		(Tcl_WideInt) Tcl_ZlibAdler32(start, data, dlen));
 	return TCL_OK;
     case z_crc32:			/* crc32 str ?startvalue?
 					 * -> checksum */
@@ -1708,7 +1709,8 @@ TclZlibCmd(
 	    start = Tcl_ZlibCRC32(0, NULL, 0);
 	}
 	data = Tcl_GetByteArrayFromObj(objv[2], &dlen);
-	Tcl_SetIntObj(obj, (int) Tcl_ZlibCRC32(start, data, dlen));
+	Tcl_SetWideIntObj(obj,
+		(Tcl_WideInt) Tcl_ZlibCRC32(start, data, dlen));
 	return TCL_OK;
     case z_deflate:			/* deflate data ?level?
 					 * -> rawCompressedData */
@@ -2262,7 +2264,7 @@ ZlibStreamCmd(
 	    Tcl_WrongNumArgs(interp, 2, objv, NULL);
 	    return TCL_ERROR;
 	}
-	Tcl_SetIntObj(obj, Tcl_ZlibStreamChecksum(zstream));
+	Tcl_SetWideIntObj(obj, (Tcl_WideInt) Tcl_ZlibStreamChecksum(zstream));
 	return TCL_OK;
     case zs_reset:		/* $strm reset */
 	if (objc != 2) {
@@ -2527,7 +2529,7 @@ ChanGetOption(
 	    crc = cd->inStream.adler;
 	}
 
-	sprintf(buf, "0x%lx", crc);
+	sprintf(buf, "%lu", crc);
 	if (optionName == NULL) {
 	    Tcl_DStringAppendElement(dsPtr, "-checksum");
 	    Tcl_DStringAppendElement(dsPtr, buf);
@@ -2836,7 +2838,7 @@ Tcl_ZlibStreamEof(
 }
 
 int
-Tcl_ZlibStreamAdler32(
+Tcl_ZlibStreamChecksum(
     Tcl_ZlibStream zshandle)
 {
     return 0;
