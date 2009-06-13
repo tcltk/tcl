fdb97a3d7dd85152f33c8232bc038c32e995f8af
dgp <dgp@users.sourceforge.net>
2009-06-13 14:31:54 +0000

    * generic/tclCompile.c: The value stashed in iPtr->compiledProcPtr         * generic/tclProc.c:    when compiling a proc survives too long.  We         * tests/execute.test:   only need it there long enough for the right         TclInitCompileEnv() call to re-stash it into envPtr->procPtr.  Once         that is done, the CompileEnv controls.  If we let the value of         iPtr->compiledProcPtr linger, though, then any other bytecode compile         operation that takes place will also have its CompileEnv initialized         with it, and that's not correct.  The value is meant to control the         compile of the proc body only, not other compile tasks that happen         along.  Thanks to Carlos Tasada for discovering and reporting the         problem.  [Bug 2802881].

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCompile.c,v 1.166 2009/02/09 22:55:44 nijtmans Exp $
+ * RCS: @(#) $Id: tclCompile.c,v 1.167 2009/06/13 14:31:54 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -868,6 +868,7 @@ TclInitCompileEnv(
     envPtr->source = stringPtr;
     envPtr->numSrcBytes = numBytes;
     envPtr->procPtr = iPtr->compiledProcPtr;
+    iPtr->compiledProcPtr = NULL;
     envPtr->numCommands = 0;
     envPtr->exceptDepth = 0;
     envPtr->maxExceptDepth = 0;
