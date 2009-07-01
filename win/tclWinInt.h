e5a498cdb501b70c379cb5ce2ae0e325844ae3cb
patthoyts@users.sourceforge.net <patthoyts>
2009-07-01 14:05:19 +0000

    Handle the GetUserName API call appropriately for wide/narrow versions. [Bug 2806622]

--- win/tclWinInt.h
+++ win/tclWinInt.h
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclWinInt.h,v 1.29 2005/11/03 01:16:07 patthoyts Exp $
+ * RCS: @(#) $Id: tclWinInt.h,v 1.29.10.1 2009/07/01 14:05:19 patthoyts Exp $
  */
 
 #ifndef _TCLWININT
@@ -144,6 +144,7 @@ typedef struct TclWinProcs {
       LPDWORD lpNumberOfCharsWritten,
       LPVOID lpReserved
     );
+    BOOL (WINAPI *getUserName)(LPTSTR lpBuffer, LPDWORD lpnSize);
 } TclWinProcs;
 
 MODULE_SCOPE TclWinProcs *tclWinProcs;
