9302cb822e80f3566365632db5bdbb88469866e1
das <das>
2009-08-25 23:20:36 +0000

    fix warnings

--- generic/tclParse.c
+++ generic/tclParse.c
@@ -2167,7 +2167,7 @@ TclSubstTokens(
     int code = TCL_OK;
 #define NUM_STATIC_POS 20
     int isLiteral, maxNumCL, numCL, i, adjust;
-    int* clPosition;
+    int *clPosition = NULL;
     Interp* iPtr = (Interp*) interp;
     int inFile = iPtr->evalFlags & TCL_EVAL_FILE;
 
