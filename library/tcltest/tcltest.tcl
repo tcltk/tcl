187e3d67722574fa7fa0e48ddd1f2c3188bad928
dgp@users.sourceforge.net <dgp>
2009-09-01 14:13:01 +0000

    * library/tcltest/tcltest.tcl:  Bump to tcltest 2.3.2 after revision         * library/tcltest/pkgIndex.tcl: to verbose error message.         * unix/Makefile.in:         * win/Makefile.in:

--- library/tcltest/tcltest.tcl
+++ library/tcltest/tcltest.tcl
@@ -16,7 +16,7 @@
 # Contributions from Don Porter, NIST, 2002.  (not subject to US copyright)
 # All rights reserved.
 #
-# RCS: @(#) $Id: tcltest.tcl,v 1.103.2.2 2009/08/30 19:18:41 das Exp $
+# RCS: @(#) $Id: tcltest.tcl,v 1.103.2.3 2009/09/01 14:13:02 dgp Exp $
 
 package require Tcl 8.5		;# -verbose line uses [info frame]
 namespace eval tcltest {
@@ -24,7 +24,7 @@ namespace eval tcltest {
     # When the version number changes, be sure to update the pkgIndex.tcl file,
     # and the install directory in the Makefiles.  When the minor version
     # changes (new feature) be sure to update the man page as well.
-    variable Version 2.3.1
+    variable Version 2.3.2
 
     # Compatibility support for dumb variables defined in tcltest 1
     # Do not use these.  Call [package provide Tcl] and [info patchlevel]
