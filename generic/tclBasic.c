9fc4264a28206f88cbe255a0a1f9ebfbddc202ab
dgp <dgp>
2009-09-04 17:33:11 +0000

    * generic/tclCompCmds.c (TclCompileSubstCmd):   Added a bytecode         * generic/tclBasic.c:   compiler routine for the [subst] command.         * generic/tclCmdMZ.c:   This is a partial solution to the need to         * generic/tclCompile.c: NR-enable [subst] since bytecode execution is         * generic/tclCompile.h: already NR-enabled.  [Bug 2314561]  Two new         * generic/tclExecute.c: bytecode instructions, INST_NOP and         * generic/tclInt.h:     INST_RETURN_CODE_BRANCH were added to support         * generic/tclParse.c:   the new routine.  INST_RETURN_CODE_BRANCH is         * tests/basic.test:     likely to be useful in any future effort to         * tests/info.test:      add a bytecode compiler routine for [try].         * tests/parse.test:

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -16,7 +16,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclBasic.c,v 1.402 2009/08/25 21:03:25 andreas_kupries Exp $
+ * RCS: @(#) $Id: tclBasic.c,v 1.403 2009/09/04 17:33:11 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -213,7 +213,7 @@ static const CmdInfo builtInCmds[] = {
     {"scan",		Tcl_ScanObjCmd,		NULL,			NULL,	1},
     {"set",		Tcl_SetObjCmd,		TclCompileSetCmd,	NULL,	1},
     {"split",		Tcl_SplitObjCmd,	NULL,			NULL,	1},
-    {"subst",		Tcl_SubstObjCmd,	NULL,			NULL,	1},
+    {"subst",		Tcl_SubstObjCmd,	TclCompileSubstCmd,	NULL,	1},
     {"switch",		Tcl_SwitchObjCmd,	TclCompileSwitchCmd,	TclNRSwitchObjCmd, 1},
     {"throw",		Tcl_ThrowObjCmd,	NULL,			NULL,	1},
     {"trace",		Tcl_TraceObjCmd,	NULL,			NULL,	1},
