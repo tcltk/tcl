c08fad764e9e41b392144dd2f8f85a8b9a22daca
dgp <dgp@users.sourceforge.net>
2009-09-22 01:36:41 +0000

    merge updates from HEAD

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCompile.c,v 1.49.2.73 2009/09/17 18:12:50 dgp Exp $
+ * RCS: @(#) $Id: tclCompile.c,v 1.49.2.74 2009/09/22 01:36:42 dgp Exp $
  */
 
 #include "tclInt.h"
@@ -899,8 +899,10 @@ Tcl_SubstObj(
     Tcl_Obj *objPtr,            /* The value to be substituted. */
     int flags)                  /* What substitutions to do. */
 {
+    TEOV_callback *rootPtr = TOP_CB(interp);
+
     if (TclNRRunCallbacks(interp, TclNRSubstObj(interp, objPtr, flags),
-	    TOP_CB(interp), 0) != TCL_OK) {
+	    rootPtr, 0) != TCL_OK) {
 	return NULL;
     }
     return Tcl_GetObjResult(interp);
