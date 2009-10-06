ad1c92dae820a7fb5f422665d93d7ab742060db4
dgp@users.sourceforge.net <dgp>
2009-10-06 16:31:01 +0000

    * generic/tclTomMathInt.h (new): Public header tclTomMath.h had         * generic/tclTomMath.h: dependence on private headers, breaking use         * generic/tommath.h:    by extensions [Bug 1941434].

--- generic/tclTomMath.h
+++ generic/tclTomMath.h
@@ -26,8 +26,6 @@
 #include <ctype.h>
 #include <limits.h>
 
-#include <tommath_class.h>
-
 #ifndef MIN
    #define MIN(x,y) ((x)<(y)?(x):(y))
 #endif
@@ -840,6 +838,6 @@ MODULE_SCOPE const char *mp_s_rmap;
 
 /* $Source: /root/tcl/repos-to-convert/tcl/generic/tclTomMath.h,v $ */
 /* Based on Tom's version 1.8 */
-/* $Revision: 1.10 $ */
-/* $Date: 2007/02/14 17:59:21 $ */
+/* $Revision: 1.11 $ */
+/* $Date: 2009/10/06 16:31:01 $ */
 
