36f84cf1239f4a474e4e70b46393a132542c5811
das <das>
2009-11-26 17:37:26 +0000

    fix warning on LP64

--- generic/tclThreadAlloc.c
+++ generic/tclThreadAlloc.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclThreadAlloc.c,v 1.30 2009/09/29 05:03:46 dgp Exp $
+ * RCS: @(#) $Id: tclThreadAlloc.c,v 1.31 2009/11/26 17:37:26 das Exp $
  */
 
 #include "tclInt.h"
@@ -297,6 +297,7 @@ TclpAlloc(
     register int bucket;
     size_t size;
 
+#ifndef __LP64__
     if (sizeof(int) >= sizeof(size_t)) {
 	/* An unsigned int overflow can also be a size_t overflow */
 	const size_t zero = 0;
@@ -307,6 +308,7 @@ TclpAlloc(
 	    return NULL;
 	}
     }
+#endif
 
     cachePtr = TclpGetAllocCache();
     if (cachePtr == NULL) {
@@ -440,6 +442,7 @@ TclpRealloc(
 	return TclpAlloc(reqSize);
     }
 
+#ifndef __LP64__
     if (sizeof(int) >= sizeof(size_t)) {
 	/* An unsigned int overflow can also be a size_t overflow */
 	const size_t zero = 0;
@@ -450,6 +453,7 @@ TclpRealloc(
 	    return NULL;
 	}
     }
+#endif
 
     cachePtr = TclpGetAllocCache();
     if (cachePtr == NULL) {
