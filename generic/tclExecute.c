5e126fd43fcba3e54f62f4e92afb95fcf492e683
Miguel Sofer <miguel.sofer@gmail.com>
2009-12-10 17:48:39 +0000

    * generic/tclBasic.c:   Reducing the # of moving parts for 	* generic/tclExecute.c: coroutines by delegating more to tebc; 	eliminate the special coroutine CallFrame.

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -14,7 +14,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclExecute.c,v 1.459 2009/12/10 16:54:01 msofer Exp $
+ * RCS: @(#) $Id: tclExecute.c,v 1.460 2009/12/10 17:48:39 msofer Exp $
  */
 
 #include "tclInt.h"
@@ -2040,6 +2040,7 @@ TclExecuteByteCode(
 	    
 	    corPtr->base.cmdFramePtr = bcFramePtr;
 	    BP->prevBottomPtr = NULL;
+	    iPtr->varFramePtr->callerPtr = iPtr->rootFramePtr;
 	}
 	
 	if (!corPtr->stackLevel) {
