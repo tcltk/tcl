22c184beaf0ff21d013eab21ec0a7b3475eaa765
dgp <dgp>
2009-12-17 16:28:21 +0000

    bump to msgcat 1.4.3 to account for commits

--- library/msgcat/msgcat.tcl
+++ library/msgcat/msgcat.tcl
@@ -10,12 +10,12 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: msgcat.tcl,v 1.28 2009/12/16 09:26:24 dkf Exp $
+# RCS: @(#) $Id: msgcat.tcl,v 1.29 2009/12/17 16:28:21 dgp Exp $
 
 package require Tcl 8.5
 # When the version number changes, be sure to update the pkgIndex.tcl file,
 # and the installation directory in the Makefiles.
-package provide msgcat 1.4.2
+package provide msgcat 1.4.3
 
 namespace eval msgcat {
     namespace export mc mcload mclocale mcmax mcmset mcpreferences mcset \
