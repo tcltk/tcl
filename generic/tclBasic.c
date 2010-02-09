1637ac189af67a8b0beeb64614125375371e0503
donal.k.fellows@manchester.ac.uk <dkf>
2010-02-09 20:51:53 +0000

    Compilation of [try] now enabled!

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -16,7 +16,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclBasic.c,v 1.442 2010/02/05 22:39:44 dkf Exp $
+ * RCS: @(#) $Id: tclBasic.c,v 1.443 2010/02/09 20:51:54 dkf Exp $
  */
 
 #include "tclInt.h"
@@ -240,7 +240,7 @@ static const CmdInfo builtInCmds[] = {
     {"tailcall",	NULL,			NULL,			TclNRTailcallObjCmd,	1},
     {"throw",		Tcl_ThrowObjCmd,	NULL,			NULL,	1},
     {"trace",		Tcl_TraceObjCmd,	NULL,			NULL,	1},
-    {"try",		Tcl_TryObjCmd,		NULL,			TclNRTryObjCmd,	1},
+    {"try",		Tcl_TryObjCmd,		TclCompileTryCmd,	TclNRTryObjCmd,	1},
     {"unset",		Tcl_UnsetObjCmd,	TclCompileUnsetCmd,	NULL,	1},
     {"uplevel",		Tcl_UplevelObjCmd,	NULL,			TclNRUplevelObjCmd,	1},
     {"upvar",		Tcl_UpvarObjCmd,	TclCompileUpvarCmd,	NULL,	1},
