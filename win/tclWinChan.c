c15b8ae2f202e33cfe617d13dce7af23013964b8
nijtmans <nijtmans>
2010-03-16 16:18:35 +0000

    don't cast away "const" without reason.

--- win/tclWinChan.c
+++ win/tclWinChan.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclWinChan.c,v 1.55 2010/01/10 22:58:39 nijtmans Exp $
+ * RCS: @(#) $Id: tclWinChan.c,v 1.56 2010/03/16 16:18:35 nijtmans Exp $
  */
 
 #include "tclWinInt.h"
@@ -856,7 +856,7 @@ TclpOpenFileChannel(
     char channelName[16 + TCL_INTEGER_SPACE];
     TclFile readFile = NULL, writeFile = NULL;
 
-    nativeName = (TCHAR *) Tcl_FSGetNativePath(pathPtr);
+    nativeName = (const TCHAR *) Tcl_FSGetNativePath(pathPtr);
     if (nativeName == NULL) {
 	return NULL;
     }
