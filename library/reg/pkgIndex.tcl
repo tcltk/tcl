1c85f3d3251bf3a169c2009632a5c091d04b61d8
kennykb <kennykb>
2010-04-02 19:27:44 +0000

    * library/reg/pkgIndex.tcl:     [TIP #362]: Fixed first round of * tests/registry.test:          bugs resulting from the recent commits * win/tclWinReg.c:              of changes in support of the referenced                                 TIP.

--- library/reg/pkgIndex.tcl
+++ library/reg/pkgIndex.tcl
@@ -2,8 +2,8 @@ if {![package vsatisfies [package provide Tcl] 8]} {return}
 if {[string compare $::tcl_platform(platform) windows]} {return}
 if {[info exists ::tcl_platform(debug)]} {
     package ifneeded registry 1.3 \
-            [list load [file join $dir tclreg12g.dll] registry]
+            [list load [file join $dir tclreg13g.dll] registry]
 } else {
     package ifneeded registry 1.3 \
-            [list load [file join $dir tclreg12.dll] registry]
+            [list load [file join $dir tclreg13.dll] registry]
 }
