76d9c3b8bbb3560b7d08993bfd07ad7827f38f7c
nijtmans <nijtmans>
2010-04-13 13:37:28 +0000

    Fix [Patch 2986105]: conditionally defining strcasecmp/strncasecmp Fix gcc warning: comparison of unsigned expression	>= 0 is always true

--- win/tclWinLoad.c
+++ win/tclWinLoad.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclWinLoad.c,v 1.27 2010/04/02 21:21:06 kennykb Exp $
+ * RCS: @(#) $Id: tclWinLoad.c,v 1.28 2010/04/13 13:37:29 nijtmans Exp $
  */
 
 #include "tclWinInt.h"
@@ -305,14 +305,13 @@ TclpTempFileNameForLibrary(Tcl_Interp* interp, /* Tcl interpreter */
     if (dllDirectoryName == NULL) {
 	Tcl_MutexLock(&loadMutex);
 	if (dllDirectoryName == NULL) {
-	    if ((nameLen = GetTempPathW(MAX_PATH, name)) >= 0) {
-		if (nameLen >= MAX_PATH-12) {
-		    Tcl_SetErrno(ENAMETOOLONG);
-		    nameLen = 0;
-		} else {
-		    wcscpy(name+nameLen, L"TCLXXXXXXXX");
-		    nameLen += 11;
-		}
+	    nameLen = GetTempPathW(MAX_PATH, name);
+	    if (nameLen >= MAX_PATH-12) {
+		Tcl_SetErrno(ENAMETOOLONG);
+		nameLen = 0;
+	    } else {
+		wcscpy(name+nameLen, L"TCLXXXXXXXX");
+		nameLen += 11;
 	    }
 	    status = 1;
 	    if (nameLen != 0) {
