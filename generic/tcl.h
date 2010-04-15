78452e7487441b936ac7f0fdfbb937472e413907
nijtmans <nijtmans>
2010-04-15 14:56:32 +0000

    Added comment

--- generic/tcl.h
+++ generic/tcl.h
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tcl.h,v 1.303 2010/04/15 13:58:44 nijtmans Exp $
+ * RCS: @(#) $Id: tcl.h,v 1.304 2010/04/15 14:56:32 nijtmans Exp $
  */
 
 #ifndef _TCL
@@ -2375,6 +2375,12 @@ EXTERN const char *	Tcl_PkgInitStubsCheck(Tcl_Interp *interp,
 EXTERN void		Tcl_GetMemoryInfo(Tcl_DString *dsPtr);
 #endif
 
+/*
+ * Unfortunately, TCHAR is needed in tclPlatDecls.h for
+ * win32, so if TCHAR is not defined yet do it here.
+ * This way, we don't need to include <tchar.h> just
+ * for one define.
+ */
 #if !defined(_TCHAR_DEFINED)
 #   if defined(__WIN32__)
 	typedef char TCHAR;
