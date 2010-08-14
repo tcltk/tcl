2d0b731556c625e2e01b9c0d30c411d9e556b45b
nijtmans <nijtmans>
2010-08-14 17:13:02 +0000

    [Patch #2994165] Change signature of Tcl_FSGetNativePath and TclpDeleteFile

--- generic/tclIOUtil.c
+++ generic/tclIOUtil.c
@@ -17,7 +17,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclIOUtil.c,v 1.176 2010/04/27 08:20:00 nijtmans Exp $
+ * RCS: @(#) $Id: tclIOUtil.c,v 1.177 2010/08/14 17:13:02 nijtmans Exp $
  */
 
 #include "tclInt.h"
@@ -4564,7 +4564,7 @@ Tcl_FSGetFileSystemForPath(
  *---------------------------------------------------------------------------
  */
 
-const char *
+const void *
 Tcl_FSGetNativePath(
     Tcl_Obj *pathPtr)
 {
