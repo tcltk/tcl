70e626efe931f5cfd9329040d42385aa3c74298e
Miguel Sofer <miguel.sofer@gmail.com>
2010-09-27 19:42:37 +0000

    * generic/tclBasic.c:    [Patch 3072080] (minus the itcl 	* generic/tclCmdIL.c:    update): a saner NRE. 	* generic/tclCompExpr.c: 	* generic/tclCompile.c:  This makes TclNRExecuteByteCode 	* generic/tclCompile.h:  (ex TEBC) to be a normal NRE 	* generic/tclExecute.c:  citizen: it loses its special status. 	* generic/tclInt.decls:  The logic flow within the BC engine is 	* generic/tclInt.h:      simplified considerably. 	* generic/tclIntDecls.h: 	* generic/tclObj.c: 	* generic/tclProc.c: 	* generic/tclTest.c:

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclCompile.c,v 1.187 2010/08/22 18:53:26 nijtmans Exp $
+ * RCS: @(#) $Id: tclCompile.c,v 1.188 2010/09/27 19:42:38 msofer Exp $
  */
 
 #include "tclInt.h"
@@ -915,7 +915,7 @@ Tcl_SubstObj(
     TEOV_callback *rootPtr = TOP_CB(interp);
 
     if (TclNRRunCallbacks(interp, Tcl_NRSubstObj(interp, objPtr, flags),
-	    rootPtr, 0) != TCL_OK) {
+	    rootPtr) != TCL_OK) {
 	return NULL;
     }
     return Tcl_GetObjResult(interp);
@@ -949,9 +949,7 @@ Tcl_NRSubstObj(
 
     /* TODO: Confirm we do not need this. */
     /* Tcl_ResetResult(interp); */
-    Tcl_NRAddCallback(interp, NRCallTEBC, INT2PTR(TCL_NR_BC_TYPE), codePtr,
-	    NULL, NULL);
-    return TCL_OK;
+    return TclNRExecuteByteCode(interp, codePtr);
 }
 
 /*
@@ -1651,7 +1649,7 @@ TclCompileScript(
 			 * length will be updated later. There is no need to
 			 * do this for the first bytecode in the compile env,
 			 * as the check is done before calling
-			 * TclExecuteByteCode(). Do emit an INST_START_CMD in
+			 * TclNRExecuteByteCode(). Do emit an INST_START_CMD in
 			 * special cases where the first bytecode is in a
 			 * loop, to insure that the corresponding command is
 			 * counted properly. Compilers for commands able to
