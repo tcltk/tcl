cfcfa7c32e49d6a3e3613f260e715ac9f4dba79a
nijtmans <nijtmans>
2010-10-12 14:55:33 +0000

    Spacing and comments: let uniClass.tcl generation match better the current (hand-modified) regc_locale.c

--- generic/regc_locale.c
+++ generic/regc_locale.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: regc_locale.c,v 1.21 2008/10/16 22:34:18 nijtmans Exp $
+ * RCS: @(#) $Id: regc_locale.c,v 1.22 2010/10/12 14:55:33 nijtmans Exp $
  */
 
 /* ASCII character-name table */
@@ -131,7 +131,9 @@ typedef struct crange {
  *	and used in generic/regc_locale.c.  Do not modify by hand.
  */
 
-/* Unicode: alphabetic characters */
+/*
+ * Unicode: alphabetic characters.
+ */
 
 static const crange alphaRangeTable[] = {
     {0x0041, 0x005a}, {0x0061, 0x007a}, {0x00c0, 0x00d6}, {0x00d8, 0x00f6},
@@ -200,7 +202,7 @@ static const chr alphaCharTable[] = {
 #define NUM_ALPHA_CHAR (sizeof(alphaCharTable)/sizeof(chr))
 
 /*
- * Unicode: decimal digit characters
+ * Unicode: decimal digit characters.
  */
 
 static const crange digitRangeTable[] = {
@@ -262,7 +264,7 @@ static const chr spaceCharTable[] = {
 #define NUM_SPACE_CHAR (sizeof(spaceCharTable)/sizeof(chr))
 
 /*
- * Unicode: lowercase characters
+ * Unicode: lowercase characters.
  */
 
 static const crange lowerRangeTable[] = {
