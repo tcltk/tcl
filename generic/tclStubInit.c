a6521a20c024e8313c1e3ba15c14e7b88cbc3ed5
Kevin B Kenny <kennykb@acm.org>
2010-11-28 23:20:10 +0000

    2010-11-29  Kevin B. Kenny  <kennykb@acm.org>
    	* generic/tclInt.decls:
    	* generic/tclInt.h:
    	* generic/tclStrToD.c:
    	* generic/tclTest.c:
    	* generic/tclTomMath.decls:
    	* generic/tclUtil.c:
    	* tests/util.test:
    	* unix/Makefile.in:
    	* win/Makefile.in:
    	* win/makefile.vc: Rewrite of Tcl_PrintDouble and TclDoubleDigits
    	that (a) fixes a severe performance problem with floating point
    	shimmering reported by Karl Lehenbauer, (b) allows TclDoubleDigits
    	to generate the digit strings for 'e' and 'f' format, so that it
    	can be used for tcl_precision != 0 (and possibly later for [format]),
    	(c) fixes [Bug 3120139] by making TclPrintDouble inherently
    	locale-independent, (d) adds test cases to util.test for
    	correct rounding in difficult cases of TclDoubleDigits where fixed-
    	precision results are requested. (e) adds test cases to util.test for
    	the controversial aspects of [Bug 3105247]. As a side effect, two
    	more modules from libtommath (bn_mp_set_int.c and bn_mp_init_set_int.c)
    	are brought into the build, since the new code uses them.

--- generic/tclStubInit.c
+++ generic/tclStubInit.c
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclStubInit.c,v 1.197 2010/09/16 14:49:37 nijtmans Exp $
+ * RCS: @(#) $Id: tclStubInit.c,v 1.198 2010/11/28 23:20:11 kennykb Exp $
  */
 
 #include "tclInt.h"
@@ -305,6 +305,7 @@ static const TclIntStubs tclIntStubs = {
     TclInitRewriteEnsemble, /* 246 */
     TclResetRewriteEnsemble, /* 247 */
     TclCopyChannel, /* 248 */
+    TclDoubleDigits, /* 249 */
 };
 
 static const TclIntPlatStubs tclIntPlatStubs = {
@@ -460,6 +461,8 @@ const TclTomMathStubs tclTomMathStubs = {
     TclBN_s_mp_mul_digs, /* 58 */
     TclBN_s_mp_sqr, /* 59 */
     TclBN_s_mp_sub, /* 60 */
+    TclBN_mp_init_set_int, /* 61 */
+    TclBN_mp_set_int, /* 62 */
 };
 
 static const TclStubHooks tclStubHooks = {
