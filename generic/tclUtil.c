a171da6ca2db3f68f827e2cabcdf7ea0c32decfc
Kevin B Kenny <kennykb@acm.org>
2010-11-29 02:27:11 +0000

    added missing casts that MSVC complained about and deleted unused variable

--- generic/tclUtil.c
+++ generic/tclUtil.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tclUtil.c,v 1.119 2010/11/28 23:20:11 kennykb Exp $
+ * RCS: @(#) $Id: tclUtil.c,v 1.120 2010/11/29 02:27:12 kennykb Exp $
  */
 
 #include "tclInt.h"
@@ -2236,7 +2236,6 @@ Tcl_PrintDouble(
     int signum;
     char* digits;
     char* end;
-    Tcl_UniChar ch;
 
     int *precisionPtr = Tcl_GetThreadData(&precisionKey, (int)sizeof(int));
 
