15e37d25b60a8ced57e2b5de164abcdf1619bc40
nijtmans@users.sourceforge.net <jan.nijtmans>
2011-03-07 21:56:39 +0000

    MSVC 6 does not have the %I modifier in sprintf

--- win/tclWinConsole.c
+++ win/tclWinConsole.c
@@ -1360,7 +1360,7 @@ TclWinOpenConsoleChannel(
      * for instance).
      */
 
-    sprintf(channelName, "file%Ix", (size_t) infoPtr);
+    sprintf(channelName, "file%" TCL_I_MODIFIER "x", (size_t) infoPtr);
 
     infoPtr->channel = Tcl_CreateChannel(&consoleChannelType, channelName,
 	    infoPtr, permissions);
