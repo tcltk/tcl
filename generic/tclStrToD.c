21d5d45a355b8a3d58ef79049d4023a0d069fedf
dgp@users.sourceforge.net <dgp>
2011-06-13 13:48:41 +0000

    3315098 Mem leak fix from Gustaf Neumann.

--- generic/tclStrToD.c
+++ generic/tclStrToD.c
@@ -3790,7 +3790,7 @@ ShorteningBignumConversion(Double* dPtr,
     if (m2plus > m2minus) {
 	mp_clear(&mplus);
     }
-    mp_clear_multi(&b, &mminus, &temp, NULL);
+    mp_clear_multi(&b, &mminus, &temp, &dig, &S, NULL);
     *s = '\0';
     *decpt = k;
     if (endPtr) {
