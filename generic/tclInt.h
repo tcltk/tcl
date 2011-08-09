5af8ab7abe6838b76193e9b0b61ca6c0149ed2a7
ferrieux@users.sourceforge.net <ferrieux>
2011-08-09 17:01:16 +0000

    [Bug 3386417] avoid a reference loop between the bytecode and its companion errostack when compiling a syntax error.

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -3016,6 +3016,7 @@ MODULE_SCOPE int	TclMaxListLength(const char *bytes, int numBytes,
 MODULE_SCOPE int	TclMergeReturnOptions(Tcl_Interp *interp, int objc,
 			    Tcl_Obj *const objv[], Tcl_Obj **optionsPtrPtr,
 			    int *codePtr, int *levelPtr);
+MODULE_SCOPE Tcl_Obj *  TclNoErrorStack(Tcl_Interp *interp, Tcl_Obj *options);
 MODULE_SCOPE int	TclNokia770Doubles(void);
 MODULE_SCOPE void	TclNsDecrRefCount(Namespace *nsPtr);
 MODULE_SCOPE void	TclObjVarErrMsg(Tcl_Interp *interp, Tcl_Obj *part1Ptr,
