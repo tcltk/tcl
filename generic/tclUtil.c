eba0aa0b63589c49ebad74cf7f784cc39afb5ef1
dgp <dgp@users.sourceforge.net>
2011-09-13 20:02:51 +0000

    3390638 Workaround broken solaris studio cc optimizer. Thanks to Wolfgang S. Kechel.

--- generic/tclUtil.c
+++ generic/tclUtil.c
@@ -2160,7 +2160,8 @@ TclFormatInt(buffer, n)
      * negating it produces the same value.
      */
 
-    if (n == -n) {
+    intVal = -n;			/* [Bug 3390638] Workaround for*/
+    if (n == -n || intVal == n) {	/* broken compiler optimizers. */
 	sprintf(buffer, "%ld", n);
 	return strlen(buffer);
     }
