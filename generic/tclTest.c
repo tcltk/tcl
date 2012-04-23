090d6db114b8f16d4b40d96de057daba9f49a97e
nijtmans@users.sourceforge.net <jan.nijtmans>
2012-04-23 12:30:28 +0000

    make some more internal tables const On cygwin, install dll's in /usr/bin, not in /usr/lib

--- generic/tclTest.c
+++ generic/tclTest.c
@@ -2777,7 +2777,7 @@ TestlocaleCmd(clientData, interp, objc, objv)
     	"ctype", "numeric", "time", "collate", "monetary", 
 	"all",	NULL
     };
-    static int lcTypes[] = {
+    static CONST int lcTypes[] = {
 	LC_CTYPE, LC_NUMERIC, LC_TIME, LC_COLLATE, LC_MONETARY,
 	LC_ALL
     };
--- win/tclWinTime.c
+++ win/tclWinTime.c
@@ -26,11 +26,11 @@
  * each month, where index 1 is January.
  */
 
-static int normalDays[] = {
+static const int normalDays[] = {
     -1, 30, 58, 89, 119, 150, 180, 211, 242, 272, 303, 333, 364
 };
 
-static int leapDays[] = {
+static const int leapDays[] = {
     -1, 30, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365
 };
 
@@ -668,7 +668,7 @@ ComputeGMT(tp)
     struct tm *tmPtr;
     long tmp, rem;
     int isLeap;
-    int *days;
+    const int *days;
     ThreadSpecificData *tsdPtr = TCL_TSD_INIT(&dataKey);
 
     tmPtr = &tsdPtr->tm;
