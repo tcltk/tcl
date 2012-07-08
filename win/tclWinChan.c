144e5850fb84e5be697789d2c35577a63f0b12dd
jan.nijtmans <nijtmans@users.sourceforge.net>
2012-07-08 10:26:20 +0000

    and don't forget the %

--- win/tclWinChan.c
+++ win/tclWinChan.c
@@ -1337,7 +1337,7 @@ TclWinOpenFileChannel(
     infoPtr->flags = appendMode;
     infoPtr->handle = handle;
     infoPtr->dirty = 0;
-    sprintf(channelName, "file" TCL_I_MODIFIER "x", (size_t)infoPtr);
+    sprintf(channelName, "file%" TCL_I_MODIFIER "x", (size_t)infoPtr);
 
     infoPtr->channel = Tcl_CreateChannel(&fileChannelType, channelName,
 	    (ClientData) infoPtr, permissions);
