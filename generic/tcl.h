65ded9c13217139c90cd83ad6b11f16848b7b3e8
dgp@users.sourceforge.net <dgp>
2012-07-11 17:09:44 +0000

    Release candidate branch for Tcl 8.6b3.

--- generic/tcl.h
+++ generic/tcl.h
@@ -58,10 +58,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   6
 #define TCL_RELEASE_LEVEL   TCL_BETA_RELEASE
-#define TCL_RELEASE_SERIAL  2
+#define TCL_RELEASE_SERIAL  3
 
 #define TCL_VERSION	    "8.6"
-#define TCL_PATCH_LEVEL	    "8.6b2"
+#define TCL_PATCH_LEVEL	    "8.6b3"
 
 /*
  *----------------------------------------------------------------------------
