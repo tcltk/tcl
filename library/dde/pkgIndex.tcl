7e29a3954cd8b9737118ccb07a48412eedf98da9
jan.nijtmans <jan.nijtmans>
2012-07-12 08:09:29 +0000

    dde version: 1.4.0b1

--- library/dde/pkgIndex.tcl
+++ library/dde/pkgIndex.tcl
@@ -1,7 +1,7 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} return
 if {[info sharedlibextension] ne ".dll"} return
 if {[::tcl::pkgconfig get debug]} {
-    package ifneeded dde 1.4.0 [list load [file join $dir tcldde14g.dll] dde]
+    package ifneeded dde 1.4.0b1 [list load [file join $dir tcldde14g.dll] dde]
 } else {
-    package ifneeded dde 1.4.0 [list load [file join $dir tcldde14.dll] dde]
+    package ifneeded dde 1.4.0b1 [list load [file join $dir tcldde14.dll] dde]
 }
