bcc2dc5577c3203e2d2c0635b81b4cb30756bc86
mig <mig>
2013-01-11 12:42:14 +0000

    Name functions according to 'what' instead of 'how' in the [tailcall] machinery, in view of making public some parts of it.

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -3049,7 +3049,7 @@ TEBCresume(
 	TEBC_YIELD();
 
 	TclNRAddCallback(interp, TclClearRootEnsemble, NULL,NULL,NULL,NULL);
-	TclDeferCallbacks(interp, /*skip tailcalls */ 1);
+	TclSkipTailcall(interp);
 	return TclNREvalObjEx(interp, objPtr, TCL_EVAL_INVOKE, NULL, INT_MIN);
 
     /*
