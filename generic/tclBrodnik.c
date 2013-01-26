65a906946be0ff900dd5cbf9c1a966621f77a6ea
dgp <dgp@users.sourceforge.net>
2013-01-26 05:06:03 +0000

    Redo the 64-bit TclMSB() using 8 bit blocks instead of 9 bit blocks.

--- generic/tclBrodnik.c
+++ generic/tclBrodnik.c
@@ -54,36 +54,82 @@ TclMSB(
     if (M == 64) {
 
 	/* First the formula.  Continue on for the explanation. */
-	const size_t C1 = 0xC020100804020100;
-	const size_t C2 = 0x3FDFEFF7FBFDFEFF;
-	const size_t  Q = 0x0001010101010101;
-	const size_t  P = 0x0040201008040201;
-	const size_t  B = 0x3F9F8F8783818080;
-
+	const size_t C1 = 0x8080808080808080;
+	const size_t C2 = 0x7F7F7F7F7F7F7F7F;
+	const size_t  Q = 0x0000040810204081;
+	const size_t  P = 0x0001010101010101;
+	const size_t  B = 0x007F7E7C78706040;
+	
 #define LEAD(n) (C1 & (n | (C2 + (n & C2))))
-#define SUM(t) (0x7 & (int)((LEAD(t) >> 8) * P >> 54));
+#define SUM(t) (0x7 & (int)((LEAD(t) >> 7) * P >> 48));
 
-	size_t b, t = B & P * (Q * LEAD(n) >> 56);
-	int k = 9 * SUM(t);
+	size_t t = B & P * ((Q * LEAD(n)) >> 57);
+	int k = 8 * SUM(t);
 
-	b = n >> k >> 1 & 0xff;
-	t = B & P * b;
-	return k + (b != 0) + SUM(t);
+	t = B & P * (n >> k >> 1 & 0x7f);
+	return k + SUM(t);
 
-	/* Total operations:
-	 * 11 bit-ands, 6 multiplies, 5 adds, 7 rightshifts,
-	 * 4 assignments, 3 bit-ors, 2 typecasts, and 1 test for nonzero.
+	/* Total operations: 36
+	 * 11 bit-ands, 6 multiplies, 4 adds, 7 rightshifts,
+	 * 3 assignments, 3 bit-ors, 2 typecasts.
 	 *
 	 * The whole task is one direct computation.
 	 * No branches. No loops.
 	 *
-	 * 39 operations cannot beat one instruction, so assembly
+	 * 36 operations cannot beat one instruction, so assembly
 	 * wins and should be used wherever possible, but this isn't bad.
 	 */
 
 #undef SUM
+    } else if (M == 32) {
+
+	/* Same scheme as above, with adjustments to the 32-bit size */
+	const size_t C1 = 0xA0820820;
+	const size_t C2 = 0x5F7DF7DF;
+	const size_t C3 = 0xC0820820;
+	const size_t C4 = 0x20000000;
+	const size_t Q  = 0x00010841;
+	const size_t P = 0x01041041;
+	const size_t B = 0x1F79C610;
+
+#define SUM(t) (0x7 & ((LEAD(t) >> 5) * P >> 24));
+
+	size_t b, t = B & P * ((Q * (C3 & (LEAD(n) + C4))) >> 27);
+	int k = 6 * SUM(t);
+
+	t = B & P * (n >> k >> 1 & 0x1f);
+	return k + SUM(t);
+
+	/* Total operations: 36
+	 * 12 bit-ands, 6 multiplies, 5 adds, 7 rightshifts,
+	 * 3 assignments, 3 bit-ors.
+	 */
+
+#undef SUM
+#undef LEAD
+
 
 #if 0
+	/* 
+	 * C1 =	10000000| 10000000| 10000000| 10000000|
+	 *	10000000| 10000000| 10000000| 10000000
+	 */
+
+	/* 
+	 *  1 +2^7 +2^14 +2^21 +2^28 +2^35 +2^42
+	 *  Q =	00000000| 00000000| 00000100| 00001000|
+	 *	00010000| 00100000| 01000000| 10000001
+	 */
+
+	/*
+	 *  P =	00000000| 00000001| 00000001| 00000001|
+	 *	00000001| 00000001| 00000001| 00000001
+	 */
+
+	/*
+	 *  B =	00000000| 01111111| 01111110| 01111100|
+	 *	01111000| 01110000| 01100000| 01000000
+	 */
 	/*
 	 * Let S = ceil(1 + sqrt(M)), that is, S = 9.
 	 * Let T = ceil(M/S), that is, T = 8.
@@ -193,35 +239,6 @@ TclMSB(
 	 */
 	return k + (block != 0) + sum;
 #endif
-    } else if (M == 32) {
-
-	/* Same scheme as above, with adjustments to the 32-bit size */
-
-	const size_t C1 = 0xA0820820;
-	const size_t C2 = 0x5F7DF7DF;
-	const size_t C3 = 0xC0820820;
-	const size_t C4 = 0x20000000;
-	const size_t Q  = 0x00010841;
-	const size_t P = 0x01041041;
-	const size_t B = 0x1F79C610;
-
-#define SUM(t) (0x7 & ((LEAD(t) >> 5) * P >> 24));
-
-	size_t b, t = B & P * ((Q * (C3 & (LEAD(n) + C4))) >> 27);
-	int k = 6 * SUM(t);
-
-	b = n >> k >> 1 & 0x1f;	
-	t = B & P * b;
-	return k + SUM(t);
-
-	/* Total operations: 37
-	 * 12 bit-ands, 6 multiplies, 5 adds, 7 rightshifts,
-	 * 4 assignments, 3 bit-ors.
-	 */
-
-#undef SUM
-#undef LEAD
-
     } else {
 	/* Simple and slow fallback for cases we haven't done yet. */
 	int k = 0;
