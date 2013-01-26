a2b7ee0d37063c1cfc871a635ed5838172d2e021
jan.nijtmans <jan.nijtmans>
2013-01-26 16:50:22 +0000

    [Bug 3601804]: platformCPUID segmentation fault on Darwin

--- unix/tclUnixCompat.c
+++ unix/tclUnixCompat.c
@@ -693,11 +693,19 @@ TclWinCPUID(
 
     /* See: <http://en.wikipedia.org/wiki/CPUID> */
 #if defined(HAVE_CPUID)
+#if defined(__x86_64__) || defined(_M_AMD64) || defined (_M_X64)
+    __asm__ __volatile__("movq %%rbx, %%rsi     \n\t" /* save %rbx */
+                 "cpuid            \n\t"
+                 "xchgq %%rsi, %%rbx   \n\t" /* restore the old %rbx */
+                 : "=a"(regsPtr[0]), "=S"(regsPtr[1]), "=c"(regsPtr[2]), "=d"(regsPtr[3])
+                 : "a"(index));
+#else
     __asm__ __volatile__("mov %%ebx, %%esi     \n\t" /* save %ebx */
                  "cpuid            \n\t"
                  "xchg %%esi, %%ebx   \n\t" /* restore the old %ebx */
                  : "=a"(regsPtr[0]), "=S"(regsPtr[1]), "=c"(regsPtr[2]), "=d"(regsPtr[3])
                  : "a"(index));
+#endif
     status = TCL_OK;
 #endif
     return status;
