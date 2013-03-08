55fc772146009345fefa90aa1a771a354eb23159
dgp <dgp@users.sourceforge.net>
2013-03-08 21:24:01 +0000

    3607372 Correct literal refcounting.

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -2592,7 +2592,7 @@ TclInitByteCodeObj(
 
 	    codePtr->objArrayPtr[i] = Tcl_NewStringObj(bytes, numBytes);
 	    Tcl_IncrRefCount(codePtr->objArrayPtr[i]);
-	    Tcl_DecrRefCount(objPtr);
+	    TclReleaseLiteral((Tcl_Interp *)iPtr, objPtr);
 	} else {
 	    codePtr->objArrayPtr[i] = fetched;
 	}
