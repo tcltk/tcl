d7e85ffec0b2ae5b8a0d990099a7e39d0ba7ac79
nijtmans@users.sourceforge.net <jan.nijtmans>
2013-04-04 07:03:05 +0000

    Make  Tcl_EvalObj/Tcl_GlobalEvalObj a macro always, not only when using stubs.

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -4990,6 +4990,7 @@ Tcl_Eval(
  *----------------------------------------------------------------------
  */
 
+#undef Tcl_EvalObj
 int
 Tcl_EvalObj(
     Tcl_Interp *interp,
@@ -4998,6 +4999,7 @@ Tcl_EvalObj(
     return Tcl_EvalObjEx(interp, objPtr, 0);
 }
 
+#undef Tcl_GlobalEvalObj
 int
 Tcl_GlobalEvalObj(
     Tcl_Interp *interp,
