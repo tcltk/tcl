7760138ceca71962d84b138ae32f936a89f9bf17
dgp@users.sourceforge.net <dgp>
2013-07-11 04:00:57 +0000

    Revert the revised macros used in developing the new tests.

--- generic/tclCompile.h
+++ generic/tclCompile.h
@@ -1537,12 +1537,9 @@ MODULE_SCOPE Tcl_Obj	*TclNewInstNameObj(unsigned char inst);
     ExtCmdLoc *mapPtr = envPtr->extCmdMapPtr;				\
     int eclIndex = mapPtr->nuloc - 1
 
-#define LineInformation(word) \
+#define SetLineInformation(word) \
     envPtr->line = mapPtr->loc[eclIndex].line[(word)];			\
     envPtr->clNext = mapPtr->loc[eclIndex].next[(word)]
-
-#define SetLineInformation(word)
-
 
 #define PushVarNameWord(i,v,e,f,l,sc,word) \
     TclPushVarName(i,v,e,f,l,sc,						\
