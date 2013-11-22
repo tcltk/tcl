da13f6ea8ce3f01699903c9125b1f90ad0ae2189
jan.nijtmans <nijtmans@users.sourceforge.net>
2013-11-22 13:21:52 +0000

    revert accidental part of previous commit

--- generic/regex.h
+++ generic/regex.h
@@ -106,11 +106,7 @@ extern "C" {
 #endif
 /* interface types */
 #define	__REG_WIDE_T	Tcl_UniChar
-#ifdef __CYGWIN__
-#define	__REG_REGOFF_T	int 	/* not really right, but good enough... */
-#else
 #define	__REG_REGOFF_T	long	/* not really right, but good enough... */
-#endif
 #define	__REG_VOID_T	void
 #define	__REG_CONST	const
 /* names and declarations */
