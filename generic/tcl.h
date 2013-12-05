bffb5abcfc0e1651e797c7a0f9f51e8c03e81359
mig <mig>
2013-12-05 15:15:23 +0000

    New compiler and bytecodes for foreach and lmap: 70% faster
    * speed as measured by http://wiki.tcl.tk/39021: runs in <1/3 the time
      the time
    * still need to adapt array-set to use this
    * assemble.test-16.5 or 16.6 bombs in a purify/symbols build (?)
    * removing the old opcodes would force old .tbc files to be recompiled, or
      an adaptation of tbcload to rwrite all foreachs

--- generic/tcl.h
+++ generic/tcl.h
@@ -848,6 +848,10 @@ typedef struct Tcl_Obj {
 	    void *ptr;
 	    unsigned long value;
 	} ptrAndLongRep;
+	struct {
+	    long int1;
+	    long int2;
+	} twoIntValue;
     } internalRep;
 } Tcl_Obj;
 
