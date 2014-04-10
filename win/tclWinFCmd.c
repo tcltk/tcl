12c74f265182110ee519d62fd3604b3523c94a1c
nijtmans@users.sourceforge.net <jan.nijtmans>
2014-04-10 13:32:14 +0000

    [792641f95b]: Normalized win32 paths should never contain backslash.

--- win/tclWinFCmd.c
+++ win/tclWinFCmd.c
@@ -1156,7 +1156,12 @@ DoRemoveJustDirectory(
 
   end:
     if (errorPtr != NULL) {
+	char *p;
 	Tcl_WinTCharToUtf(nativePath, -1, errorPtr);
+	p = Tcl_DStringValue(errorPtr);
+	for (; *p; ++p) {
+	    if (*p == '\\') *p = '/';
+	}
     }
     return TCL_ERROR;
 
