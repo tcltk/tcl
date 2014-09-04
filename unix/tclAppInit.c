19a6a43f1e4b25162846e4a08d6ac7bce69dbcf1
hypnotoad <yoda@etoyoc.com>
2014-09-04 01:23:45 +0000

    Removed some debugging code of mine...

--- unix/tclAppInit.c
+++ unix/tclAppInit.c
@@ -81,7 +81,6 @@ main(
     TCL_LOCAL_MAIN_HOOK(&argc, &argv);
 #endif
 #ifdef TCL_KIT
-    printf("Running Kit Mode\n");
     /* This voodoo ensures that Tcl_Main does not eat the first argument */
     Tcl_FindExecutable(argv[0]);
     Tcl_SetStartupScript(Tcl_NewStringObj("/zvfs/main.tcl",-1),NULL);
