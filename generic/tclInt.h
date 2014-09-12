ee30b1c0abf416e9e6b409e713c1309f2de7eb1b
hypnotoad <yoda@etoyoc.com>
2014-09-12 16:03:20 +0000

    Developed an improved bootloader built around TclSetPreInitScript. The new bootloader now mounts the VFS before the interpreter is initialized, and gives it enough hints to point to the VFS for init.tcl and main.tcl (If present)
    Also, Tcl_ZVfs_Boot now takes and additional argument: the name of the file to mount.
    
    As the difference between a normal shell and a zvfs enabled shell is one again, several
    lines of code, the example shells is folded back into tclAppInit.c and controlled with
    macros.
    
    The ZVFS commands are now loaded in as a static package.
    
    Removed the Stubs entry for Tcl_Boot_ZVFS, it's now intended that shells build
    their own copy of tclZipVfs.o

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -3153,6 +3153,7 @@ MODULE_SCOPE double	TclpWideClicksToNanoseconds(Tcl_WideInt clicks);
 #endif
 MODULE_SCOPE Tcl_Obj *	TclDisassembleByteCodeObj(Tcl_Obj *objPtr);
 MODULE_SCOPE int	TclZlibInit(Tcl_Interp *interp);
+MODULE_SCOPE int	TclZvfsInit(Tcl_Interp *interp);
 MODULE_SCOPE void *	TclpThreadCreateKey(void);
 MODULE_SCOPE void	TclpThreadDeleteKey(void *keyPtr);
 MODULE_SCOPE void	TclpThreadSetMasterTSD(void *tsdKeyPtr, void *ptr);
