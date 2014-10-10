c2be2a28e0541de49216935f4245f53884d17ee0
dgp <dgp@users.sourceforge.net>
2014-10-10 19:54:19 +0000

    [bf7135428c] Restore the Tcl_Write() return value logic.

--- generic/tclIO.c
+++ generic/tclIO.c
@@ -3484,7 +3484,10 @@ Tcl_Write(
     if (srcLen < 0) {
 	srcLen = strlen(src);
     }
-    return WriteBytes(chanPtr, src, srcLen);
+    if (WriteBytes(chanPtr, src, srcLen) < 0) {
+	return -1;
+    }
+    return srcLen;
 }
 
 /*
