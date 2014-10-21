80eb226a4cd11f29ef5edbb116522735d1a106c9
hypnotoad <yoda@etoyoc.com>
2014-10-21 01:20:10 +0000

    Fixes to allow a standard tclsh build to do the zip file encoding, instead of having to do it all through the zip enabled shell.

--- tools/mkzip.tcl
+++ tools/mkzip.tcl
@@ -1,6 +1,5 @@
 ###
 # Wrapper to allow access to Tcl's zvfs::mkzip command from Makefiles
 ###
-package require zvfs
 source [file join [file dirname [file normalize [info script]]] .. library zvfstools zvfstools.tcl]
 zvfs::mkzip {*}$argv
