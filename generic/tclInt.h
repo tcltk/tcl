ea57ae2c0d51fb75149e12dc52e1300adebeae9d
dgp@users.sourceforge.net <dgp>
2014-12-23 02:12:59 +0000

    Eliminate TclIsLocalScalar(). No callers left.

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -2985,7 +2985,6 @@ MODULE_SCOPE void	TclInitNotifier(void);
 MODULE_SCOPE void	TclInitObjSubsystem(void);
 MODULE_SCOPE void	TclInitSubsystems(void);
 MODULE_SCOPE int	TclInterpReady(Tcl_Interp *interp);
-MODULE_SCOPE int	TclIsLocalScalar(const char *src, int len);
 MODULE_SCOPE int	TclIsSpaceProc(char byte);
 MODULE_SCOPE int	TclIsBareword(char byte);
 MODULE_SCOPE Tcl_Obj *	TclJoinPath(int elements, Tcl_Obj * const objv[]);
