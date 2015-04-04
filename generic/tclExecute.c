ccb8508a5278acbfa96c69f077d8c2774e5d1acf
donal.k.fellows@manchester.ac.uk <dkf>
2015-04-04 11:47:53 +0000

    Don't fetch values we don't need.

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -5591,7 +5591,7 @@ TEBCresume(
 	    TclNewObj(objResultPtr);
 	} else if (TclIsPureByteArray(valuePtr)) {
 	    objResultPtr = Tcl_NewByteArrayObj(
-		    Tcl_GetByteArrayFromObj(valuePtr, &length)+index, 1);
+		    Tcl_GetByteArrayFromObj(valuePtr, NULL)+index, 1);
 	} else if (valuePtr->bytes && length == valuePtr->length) {
 	    objResultPtr = Tcl_NewStringObj((const char *)
 		    valuePtr->bytes+index, 1);
