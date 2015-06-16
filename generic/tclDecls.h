d4bb7bdf65e391348d97f1464a3700c839693e39
jan.nijtmans <nijtmans@users.sourceforge.net>
2015-06-16 15:24:51 +0000

    Change Tcl_MutexUnlockAndFinalize() to internal function TclpMutexUnlockAndFinalize(). This does not require a TIP, and still should fix the reported bug.

--- generic/tclDecls.h
+++ generic/tclDecls.h
@@ -1815,8 +1815,6 @@ EXTERN int		Tcl_FSUnloadFile(Tcl_Interp *interp,
 EXTERN void		Tcl_ZlibStreamSetCompressionDictionary(
 				Tcl_ZlibStream zhandle,
 				Tcl_Obj *compressionDictionaryObj);
-/* 631 */
-EXTERN void		Tcl_MutexUnlockAndFinalize(Tcl_Mutex *mutex);
 
 typedef struct {
     const struct TclPlatStubs *tclPlatStubs;
@@ -2483,7 +2481,6 @@ typedef struct TclStubs {
     void * (*tcl_FindSymbol) (Tcl_Interp *interp, Tcl_LoadHandle handle, const char *symbol); /* 628 */
     int (*tcl_FSUnloadFile) (Tcl_Interp *interp, Tcl_LoadHandle handlePtr); /* 629 */
     void (*tcl_ZlibStreamSetCompressionDictionary) (Tcl_ZlibStream zhandle, Tcl_Obj *compressionDictionaryObj); /* 630 */
-    void (*tcl_MutexUnlockAndFinalize) (Tcl_Mutex *mutex); /* 631 */
 } TclStubs;
 
 extern const TclStubs *tclStubsPtr;
@@ -3776,8 +3773,6 @@ extern const TclStubs *tclStubsPtr;
 	(tclStubsPtr->tcl_FSUnloadFile) /* 629 */
 #define Tcl_ZlibStreamSetCompressionDictionary \
 	(tclStubsPtr->tcl_ZlibStreamSetCompressionDictionary) /* 630 */
-#define Tcl_MutexUnlockAndFinalize \
-	(tclStubsPtr->tcl_MutexUnlockAndFinalize) /* 631 */
 
 #endif /* defined(USE_TCL_STUBS) */
 
--- generic/tclStubInit.c
+++ generic/tclStubInit.c
@@ -1412,7 +1412,6 @@ const TclStubs tclStubs = {
     Tcl_FindSymbol, /* 628 */
     Tcl_FSUnloadFile, /* 629 */
     Tcl_ZlibStreamSetCompressionDictionary, /* 630 */
-    Tcl_MutexUnlockAndFinalize, /* 631 */
 };
 
 /* !END!: Do not edit above this line. */
