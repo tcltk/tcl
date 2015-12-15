d9891f3998fa9d5b58ddff110ef4603749553763
dgp <dgp@users.sourceforge.net>
2015-12-15 14:40:01 +0000

    Suppress additions to the -errorinfo from a tailcall-invoked command.

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -8377,7 +8377,7 @@ TclNRTailcallEval(
     TclMarkTailcall(interp);
     TclNRAddCallback(interp, TailcallCleanup, listPtr, NULL, NULL,NULL);
     iPtr->lookupNsPtr = (Namespace *) nsPtr;
-    return TclNREvalObjv(interp, objc-1, objv+1, 0, NULL);
+    return TclNREvalObjv(interp, objc-1, objv+1, TCL_EVAL_NOERR, NULL);
 }
 
 static int
