3f0217f6ef047d021b2b7e6b042450d80f4b8632
dgp <dgp@users.sourceforge.net>
2016-02-01 21:39:44 +0000

    Backout the contributed patch memaccounting from Postgres since it changes the protections incompatibly and causes established tests to crash.

--- generic/regex.h
+++ generic/regex.h
@@ -277,7 +277,7 @@ typedef struct {
 #define	REG_INVARG	16	/* invalid argument to regex function */
 #define	REG_MIXED	17	/* character widths of regex and string differ */
 #define	REG_BADOPT	18	/* invalid embedded option */
-#define	REG_ETOOBIG	19	/* regular expression is too complex */
+#define	REG_ETOOBIG	19	/* nfa has too many states */
 #define	REG_ECOLORS	20	/* too many colors */
 /* two specials for debugging and testing */
 #define	REG_ATOI	101	/* convert error-code name to number */
