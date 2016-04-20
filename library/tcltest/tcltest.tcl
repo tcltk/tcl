46069c41c59226ee28c31faca3a66afc9582bc31
gahr <gahr@gahr.ch>
2016-04-20 19:37:31 +0000

    Bump tcltest version to 2.3.9

--- library/tcltest/tcltest.tcl
+++ library/tcltest/tcltest.tcl
@@ -22,7 +22,7 @@ namespace eval tcltest {
     # When the version number changes, be sure to update the pkgIndex.tcl file,
     # and the install directory in the Makefiles.  When the minor version
     # changes (new feature) be sure to update the man page as well.
-    variable Version 2.3.8
+    variable Version 2.3.9
 
     # Compatibility support for dumb variables defined in tcltest 1
     # Do not use these.  Call [package provide Tcl] and [info patchlevel]
