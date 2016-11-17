44774dc3238ca1e269c6acedcbdfb449780c96f9
jan.nijtmans <nijtmans@users.sourceforge.net>
2016-11-17 15:30:32 +0000

    Restore bn_mp_radix_size.c to exact copy of libtommath-1.0 version: Since the radix_size of "9" should return 2, not 3. Add test-case to prove that.

--- libtommath/bn_mp_radix_size.c
+++ libtommath/bn_mp_radix_size.c
@@ -66,17 +66,8 @@ int mp_radix_size (mp_int * a, int radix, int *size)
   }
   mp_clear (&t);
 
-  /* 
-   * return digs + 1, the 1 is for the NULL byte that would be required.
-   * mp_toradix_n requires a minimum of 3 bytes, so never report less than
-   * that.
-   */
-
-  if ( digs >= 2 ) {
-      *size = digs + 1;
-  } else {
-      *size = 3;
-  }
+  /* return digs + 1, the 1 is for the NULL byte that would be required. */
+  *size = digs + 1;
   return MP_OKAY;
 }
 
