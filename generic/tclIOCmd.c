bbe8ae73e2e5c693a20ea16c964949279ff83045
limeboy <that.lemon+tcl@gmai.com>
2016-12-20 10:56:53 +0000

    Decouple the switch handling.

--- generic/tclIOCmd.c
+++ generic/tclIOCmd.c
@@ -1492,7 +1492,8 @@ Tcl_SocketObjCmd(
 	SKT_ASYNC, SKT_MYADDR, SKT_MYPORT, SKT_REUSEADDR, SKT_REUSEPORT,
 	SKT_SERVER
     };
-    int optionIndex, a, server = 0, myport = 0, async = 0, boolTmp;
+    int optionIndex, a, server = 0, myport = 0, async = 0, reusep = 0,
+	reusea = 0;
     unsigned int flags = 0;
     const char *host, *port, *myaddr = NULL;
     Tcl_Obj *script = NULL;
@@ -1552,7 +1553,8 @@ Tcl_SocketObjCmd(
 		return TCL_ERROR;
 	    }
 	    server = 1;
-	    flags |= TCL_TCPSERVER_REUSEADDR;
+	    /* [TIP#456] Set for backward-compatibility. */
+	    reusea = 1;
 	    a++;
 	    if (a >= objc) {
 		Tcl_SetObjResult(interp, Tcl_NewStringObj(
@@ -1568,13 +1570,8 @@ Tcl_SocketObjCmd(
 			"no argument given for -reuseaddr option", -1));
 		return TCL_ERROR;
 	    }
-	    if (Tcl_GetBooleanFromObj(interp, objv[a], &boolTmp) != TCL_OK) {
+	    if (Tcl_GetBooleanFromObj(interp, objv[a], &reusea) != TCL_OK) {
 		return TCL_ERROR;
-	    }
-	    if (boolTmp) {
-		flags |= TCL_TCPSERVER_REUSEADDR;
-	    } else {
-		flags &= ~TCL_TCPSERVER_REUSEADDR;
 	    }
 	    break;
 	case SKT_REUSEPORT:
@@ -1584,13 +1581,8 @@ Tcl_SocketObjCmd(
 			"no argument given for -reuseport option", -1));
 		return TCL_ERROR;
 	    }
-	    if (Tcl_GetBooleanFromObj(interp, objv[a], &boolTmp) != TCL_OK) {
+	    if (Tcl_GetBooleanFromObj(interp, objv[a], &reusep) != TCL_OK) {
 		return TCL_ERROR;
-	    }
-	    if (boolTmp) {
-		flags |= TCL_TCPSERVER_REUSEPORT;
-	    } else {
-		flags &= ~TCL_TCPSERVER_REUSEPORT;
 	    }
 	    break;
 	default:
@@ -1621,12 +1613,15 @@ Tcl_SocketObjCmd(
 	return TCL_ERROR;
     }
 
-    if (!server && flags != 0) {
+    if (!server && (reusea != 0 || reusep != 0)) {
 	Tcl_SetObjResult(interp, Tcl_NewStringObj(
 		"options -reuseaddr and -reuseport are only valid for servers",
 		-1));
 	return TCL_ERROR;
     }
+
+    flags |= reusea ? TCL_TCPSERVER_REUSEADDR : 0;
+    flags |= reusep ? TCL_TCPSERVER_REUSEPORT : 0;
 
     // All the arguments should have been parsed by now, 'a' points to the last
     // one, the port number.
