16f3f234e8500f5f71e4d9321689a8bdf9efc809
jan.nijtmans <nijtmans@users.sourceforge.net>
2017-06-08 08:26:58 +0000

    Better UTF-8 surrogate handling, only functional when TCL_UTF_MAX>3

--- generic/tclBinary.c
+++ generic/tclBinary.c
@@ -454,7 +454,7 @@ SetByteArrayFromAny(
     const char *src, *srcEnd;
     unsigned char *dst;
     ByteArray *byteArrayPtr;
-    Tcl_UniChar ch;
+    Tcl_UniChar ch = 0;
 
     if (objPtr->typePtr != &tclByteArrayType) {
 	src = TclGetStringFromObj(objPtr, &length);
@@ -1210,7 +1210,7 @@ BinaryFormatCmd(
 
  badField:
     {
-	Tcl_UniChar ch;
+	Tcl_UniChar ch = 0;
 	char buf[TCL_UTF_MAX + 1];
 
 	TclUtfToUniChar(errorString, &ch);
@@ -1580,7 +1580,7 @@ BinaryScanCmd(
 
  badField:
     {
-	Tcl_UniChar ch;
+	Tcl_UniChar ch = 0;
 	char buf[TCL_UTF_MAX + 1];
 
 	TclUtfToUniChar(errorString, &ch);
