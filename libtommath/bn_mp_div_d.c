432e75210fdb31c3bab2a57d04a6184b88c12c68
jan.nijtmans <nijtmans@users.sourceforge.net>
2017-08-31 13:32:52 +0000

    Undo tag removal (just get libtommath directly from GitHUB), so we don't have useless file changes with every new release

--- libtommath/bn_mp_div_d.c
+++ libtommath/bn_mp_div_d.c
@@ -110,6 +110,6 @@ int mp_div_d (mp_int * a, mp_digit b, mp_int * c, mp_digit * d)
 
 #endif
 
-/* ref:         tag: v1.0.1, master */
-/* git commit:  5953f62e42b24af93748b1ee5e1d062e242c2546 */
-/* commit time: 2017-08-29 22:27:36 +0200 */
+/* ref:         $Format:%D$ */
+/* git commit:  $Format:%H$ */
+/* commit time: $Format:%ai$ */
--- libtommath/bn_mp_import.c
+++ libtommath/bn_mp_import.c
@@ -68,6 +68,6 @@ int mp_import(mp_int* rop, size_t count, int order, size_t size,
 
 #endif
 
-/* ref:         tag: v1.0.1, master */
-/* git commit:  5953f62e42b24af93748b1ee5e1d062e242c2546 */
-/* commit time: 2017-08-29 22:27:36 +0200 */
+/* ref:         $Format:%D$ */
+/* git commit:  $Format:%H$ */
+/* commit time: $Format:%ai$ */
