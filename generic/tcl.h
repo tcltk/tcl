d03a4dcc88f0ffb3e882124dc89f33f138419653
dgp <dgp@users.sourceforge.net>
2017-09-08 13:13:15 +0000

    Bump to 8.7a2 to distinguish trunk from 8.7a1 release.

--- generic/tcl.h
+++ generic/tcl.h
@@ -55,10 +55,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   7
 #define TCL_RELEASE_LEVEL   TCL_ALPHA_RELEASE
-#define TCL_RELEASE_SERIAL  1
+#define TCL_RELEASE_SERIAL  2
 
 #define TCL_VERSION	    "8.7"
-#define TCL_PATCH_LEVEL	    "8.7a1"
+#define TCL_PATCH_LEVEL	    "8.7a2"
 
 #if !defined(TCL_NO_DEPRECATED) || defined(RC_INVOKED)
 /*
