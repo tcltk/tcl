1ed5a135edb9b30eae6aa9dcc091929aba2c3864
dgp <dgp@users.sourceforge.net>
2017-11-14 18:35:22 +0000

    TIP 484 proposes a re-implementation of Tcl's "int" Tcl_ObjType. Since the new implementation is not consistent with the former one, any callers of Tcl_GetObjType("int") who are expecting the return value to point them at something playing by the former rules are at risk of having their code broken.  In this branch I remove the registation of Tcl's "int" type, so such callers will get NULL (which they should already be able to handle) instead of something misleading them into breakage.
    Examine this branch and decide if it should be incorporated into TIP 484.

--- generic/tclObj.c
+++ generic/tclObj.c
@@ -383,7 +383,6 @@ TclInitObjSubsystem(void)
     Tcl_RegisterObjType(&tclByteArrayType);
     Tcl_RegisterObjType(&tclDoubleType);
     Tcl_RegisterObjType(&tclEndOffsetType);
-    Tcl_RegisterObjType(&tclIntType);
     Tcl_RegisterObjType(&tclStringType);
     Tcl_RegisterObjType(&tclListType);
     Tcl_RegisterObjType(&tclDictType);
