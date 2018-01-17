608373fcb170fe76512b46c7ce07cc0691720ae5
hypnotoad <yoda@etoyoc.com>
2018-01-17 15:56:32 +0000

    Modifications to allow the mounting of zip file systems from data blocks
    Added a new stubs entry to mount a data block as a zip file system

--- generic/tclDecls.h
+++ generic/tclDecls.h
@@ -1839,14 +1839,17 @@ EXTERN Tcl_Channel	Tcl_OpenTcpServerEx(Tcl_Interp *interp,
 				Tcl_TcpAcceptProc *acceptProc,
 				ClientData callbackData);
 /* 632 */
-EXTERN int		TclZipfs_Mount(Tcl_Interp *interp,
-				const char *zipname, const char *mntpt,
-				const char *passwd);
+EXTERN int		TclZipfs_Mount(Tcl_Interp *interp, const char *mntpt,
+				const char *zipname, const char *passwd);
 /* 633 */
 EXTERN int		TclZipfs_Unmount(Tcl_Interp *interp,
 				const char *zipname);
 /* 634 */
 EXTERN Tcl_Obj *	TclZipfs_TclLibrary(void);
+/* 635 */
+EXTERN int		TclZipfs_Mount_Buffer(Tcl_Interp *interp,
+				const char *mntpt, unsigned char *data,
+				size_t datalen, int copy);
 
 typedef struct {
     const struct TclPlatStubs *tclPlatStubs;
@@ -2514,9 +2517,10 @@ typedef struct TclStubs {
     int (*tcl_FSUnloadFile) (Tcl_Interp *interp, Tcl_LoadHandle handlePtr); /* 629 */
     void (*tcl_ZlibStreamSetCompressionDictionary) (Tcl_ZlibStream zhandle, Tcl_Obj *compressionDictionaryObj); /* 630 */
     Tcl_Channel (*tcl_OpenTcpServerEx) (Tcl_Interp *interp, const char *service, const char *host, unsigned int flags, Tcl_TcpAcceptProc *acceptProc, ClientData callbackData); /* 631 */
-    int (*tclZipfs_Mount) (Tcl_Interp *interp, const char *zipname, const char *mntpt, const char *passwd); /* 632 */
+    int (*tclZipfs_Mount) (Tcl_Interp *interp, const char *mntpt, const char *zipname, const char *passwd); /* 632 */
     int (*tclZipfs_Unmount) (Tcl_Interp *interp, const char *zipname); /* 633 */
     Tcl_Obj * (*tclZipfs_TclLibrary) (void); /* 634 */
+    int (*tclZipfs_Mount_Buffer) (Tcl_Interp *interp, const char *mntpt, unsigned char *data, size_t datalen, int copy); /* 635 */
 } TclStubs;
 
 extern const TclStubs *tclStubsPtr;
@@ -3817,6 +3821,8 @@ extern const TclStubs *tclStubsPtr;
 	(tclStubsPtr->tclZipfs_Unmount) /* 633 */
 #define TclZipfs_TclLibrary \
 	(tclStubsPtr->tclZipfs_TclLibrary) /* 634 */
+#define TclZipfs_Mount_Buffer \
+	(tclStubsPtr->tclZipfs_Mount_Buffer) /* 635 */
 
 #endif /* defined(USE_TCL_STUBS) */
 
