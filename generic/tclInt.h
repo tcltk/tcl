93fa41f6c79f481b1325108db444ed1de7d3cfd4
jan.nijtmans <nijtmans@users.sourceforge.net>
2018-01-19 15:17:45 +0000

    Now really remove Tcl_EvalTokens/Tcl_Backslash as promised in the TIP. And remove library/tzdata/SystemV/* which contains non-standard timezones. See: [d3c5f2eb2ff63f10] They were originally gone since the tzdata2014f update, but it's unwise to do that in a patch release. Some documentation updates.

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -178,9 +178,6 @@ typedef struct Tcl_ResolverInfo {
  *    - Bug #696893 - variable is either proc-local or in the current
  *	namespace; never follow the second (global) resolution path
  *    - Bug #631741 - do not use special namespace or interp resolvers
- *
- * It should also not collide with the (deprecated) TCL_PARSE_PART1 flag
- * (Bug #835020)
  */
 
 #define TCL_AVOID_RESOLVERS 0x40000
