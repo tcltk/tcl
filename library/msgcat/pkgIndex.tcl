8bd4a8cf2c0d70bf09729c5f6b203b75f0e6fd52
dgp <dgp@users.sourceforge.net>
2018-03-12 14:41:20 +0000

    Revert change that broke usage with Tcl 9

--- library/msgcat/pkgIndex.tcl
+++ library/msgcat/pkgIndex.tcl
@@ -1,3 +1,2 @@
-if {![package vsatisfies [package provide Tcl] 8.5]} {return}
 if {![package vsatisfies [package provide Tcl] 8.6-]} {return}
 package ifneeded msgcat 1.7.0 [list source [file join $dir msgcat.tcl]]
