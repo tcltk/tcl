7c3266a3179ee7b0895c0e4a311cd8a6be78923b
sebres <sebres@users.sourceforge.net>
2018-06-08 13:28:11 +0000

    fixes [815e246806], extend Tcl_RegisterChannel/Tcl_UnregisterChannel with preserving of ref-count of the channel also to avoid unexpected removal of channel during the transfer-process between threads (could temporary share the channel in two threads, especially if initiated from event-handler like TclChannelEventScriptInvoker).

--- generic/tclIO.c
+++ generic/tclIO.c
@@ -1093,7 +1093,15 @@ Tcl_RegisterChannel(
 	}
 	Tcl_SetHashValue(hPtr, chanPtr);
     }
+
+    /*
+     * Increase ref-count of the state and channel (note the transfer-process to
+     * other thread could temporary share the channel in two threads (especially 
+     * if initiated from event-handler like TclChannelEventScriptInvoker,
+     * see [815e246806]).
+     */
     statePtr->refCount++;
+    TclChannelPreserve(chan);
 }
 
 /*
@@ -1294,6 +1302,8 @@ DetachChannel(
 	CleanupChannelHandlers(interp, chanPtr);
     }
 
+    /* Opposite of TclChannelPreserve in Tcl_RegisterChannel */
+    TclChannelRelease(chan);
     statePtr->refCount--;
 
     return TCL_OK;
