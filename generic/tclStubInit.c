5aad33e97229a63fa00f1b130f4ac64c123e1feb
dkf <donal.k.fellows@manchester.ac.uk>
2018-10-06 16:49:40 +0000

    More regularizing of function names, field names and testing styles

--- generic/tclStubInit.c
+++ generic/tclStubInit.c
@@ -1574,7 +1574,7 @@ const TclStubs tclStubs = {
     TclZipfs_Mount, /* 632 */
     TclZipfs_Unmount, /* 633 */
     TclZipfs_TclLibrary, /* 634 */
-    TclZipfs_Mount_Buffer, /* 635 */
+    TclZipfs_MountBuffer, /* 635 */
 };
 
 /* !END!: Do not edit above this line. */
