4b7747b505d177db96c71d43bae93c77f303c568
jan.nijtmans <nijtmans@users.sourceforge.net>
2018-10-08 20:47:26 +0000

    Fix MSVC warning: warning C4146: unary minus operator applied to unsigned type, result still unsigned (hopefully)

--- generic/tclObj.c
+++ generic/tclObj.c
@@ -3087,7 +3087,7 @@ Tcl_GetWideIntFromObj(
 			value = (value << CHAR_BIT) | *bytes++;
 		    }
 		    if (big.sign) {
-			if (value <= -(Tcl_WideUInt)WIDE_MIN) {
+			if (value <= 1 + ~(Tcl_WideUInt)WIDE_MIN) {
 			    *wideIntPtr = - (Tcl_WideInt) value;
 			    return TCL_OK;
 			}
