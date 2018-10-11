0016deab0d4f64724309943a3ede8c3482b70733
dkf <donal.k.fellows@manchester.ac.uk>
2018-10-11 07:24:40 +0000

    Put documented [zipfs find] in ensemble. Add examples to docs.

--- generic/tclZipfs.c
+++ generic/tclZipfs.c
@@ -4737,8 +4737,8 @@ TclZipfs_Init(
 	"    }\n"
 	"    return $result\n"
 	"}\n"
-	"proc ::tcl::zipfs::find dir {\n"
-	"    return [lsort [Find $dir]]\n"
+	"proc ::tcl::zipfs::find {directoryName} {\n"
+	"    return [lsort [Find $directoryName]]\n"
 	"}\n";
 
     /*
@@ -4753,11 +4753,22 @@ TclZipfs_Init(
     Unlock();
 
     if (interp) {
+	Tcl_Command ensemble;
+	Tcl_Obj *mapObj;
+
 	Tcl_EvalEx(interp, findproc, -1, TCL_EVAL_GLOBAL);
 	Tcl_LinkVar(interp, "::tcl::zipfs::wrmax", (char *) &ZipFS.wrmax,
 		TCL_LINK_INT);
-	TclMakeEnsemble(interp, "zipfs",
+	ensemble = TclMakeEnsemble(interp, "zipfs",
 		Tcl_IsSafe(interp) ? (initMap + 4) : initMap);
+
+	/*
+	 * Add the [zipfs find] subcommand.
+	 */
+
+	Tcl_GetEnsembleMappingDict(NULL, ensemble, &mapObj);
+	Tcl_DictObjPut(NULL, mapObj, Tcl_NewStringObj("find", -1),
+		Tcl_NewStringObj("::tcl::zipfs::find", -1));
 	Tcl_PkgProvide(interp, "zipfs", "2.0");
     }
     return TCL_OK;
