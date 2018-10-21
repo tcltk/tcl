e9fb51bda8c0f036ab0d60276e94a62969330f0d
dkf <donal.k.fellows@manchester.ac.uk>
2018-10-21 10:16:05 +0000

    Disable windows builds. Make test failures fatal.

--- library/tcltest/tcltest.tcl
+++ library/tcltest/tcltest.tcl
@@ -2696,7 +2696,7 @@ proc tcltest::GetMatchingDirectories {rootdir} {
 #	shell being tested
 #
 # Results:
-#	None.
+#	Whether there were any failures.
 #
 # Side effects:
 #	None.
@@ -2842,7 +2842,7 @@ proc tcltest::runAllTests { {shell ""} } {
 	puts [outputChannel] ""
 	puts [outputChannel] [string repeat ~ 44]
     }
-    return
+    return [info exists testFileFailures]
 }
 
 #####################################################################
