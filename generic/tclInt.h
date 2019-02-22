68c0005c844ff18403f6c38537ec1782de92ff2f
dgp <dgp@users.sourceforge.net>
2019-02-22 17:32:49 +0000

    Revert recent commit that breaks the encapsulation interface of TIP 445.
    That encapsulation was put in place to support feature evolution such as
    that in the dgp-properbytearry branch, which is now a nightmare of merge
    conflicts.
    
    TIP 445 was approved. Please respect it. Where it needs revision or improvement
    let's work together to TIP those changes.

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -2748,7 +2748,6 @@ MODULE_SCOPE ClientData tclTimeClientData;
 MODULE_SCOPE const Tcl_ObjType tclBignumType;
 MODULE_SCOPE const Tcl_ObjType tclBooleanType;
 MODULE_SCOPE const Tcl_ObjType tclByteArrayType;
-MODULE_SCOPE const Tcl_ObjType tclPureByteArrayType;
 MODULE_SCOPE const Tcl_ObjType tclByteCodeType;
 MODULE_SCOPE const Tcl_ObjType tclDoubleType;
 MODULE_SCOPE const Tcl_ObjType tclIntType;
@@ -4621,8 +4620,7 @@ MODULE_SCOPE void	TclDbInitNewObj(Tcl_Obj *objPtr, const char *file,
  *----------------------------------------------------------------
  */
 
-#define TclIsPureByteArray(objPtr) \
-	((objPtr)->typePtr==&tclPureByteArrayType)
+MODULE_SCOPE int	TclIsPureByteArray(Tcl_Obj *objPtr);
 #define TclIsPureDict(objPtr) \
 	(((objPtr)->bytes==NULL) && ((objPtr)->typePtr==&tclDictType))
 #define TclFetchIntRep(objPtr, type) \
