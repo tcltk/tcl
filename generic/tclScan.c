0801e742abbbe74e43b87b0ea139e700c7094627
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-03-02 16:35:26 +0000

    Fix some "scan.test" test-cases when TCL_UTF_MAX=4.  Wrongly resolved merge-conflict in previous cherry-pick merge.

--- generic/tclScan.c
+++ generic/tclScan.c
@@ -888,8 +888,8 @@ Tcl_ScanObjCmd(
 	    offset = TclUtfToUniChar(string, &sch);
 	    i = (int)sch;
 #if TCL_UTF_MAX == 4
-	    if (!offset) {
-		offset = TclUtfToUniChar(string, &sch);
+	    if ((sch >= 0xD800) && (offset < 3)) {
+		offset += TclUtfToUniChar(string+offset, &sch);
 		i = (((i<<10) & 0x0FFC00) + 0x10000) + (sch & 0x3FF);
 	    }
 #endif
