ec4d7da07dc725f100fb8d1ef0421b352ddd6b96
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-03-18 22:14:24 +0000

    enlarge a few small buffers, which could overflow using Unicode characters > /UFFFF.  Eliminate some end-of-line spacing

--- generic/tclCmdMZ.c
+++ generic/tclCmdMZ.c
@@ -1445,7 +1445,7 @@ StringIndexCmd(
 
 	    Tcl_SetObjResult(interp, Tcl_NewByteArrayObj(&uch, 1));
 	} else {
-	    char buf[TCL_UTF_MAX] = "";
+	    char buf[4] = "";
 
 	    length = Tcl_UniCharToUtf(ch, buf);
 	    if ((ch >= 0xD800) && (length < 3)) {
