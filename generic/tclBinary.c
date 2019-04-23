ba3eca6d7e1ad5c6a643052f7cc496d25272e3a5
dkf <donal.k.fellows@manchester.ac.uk>
2019-04-23 13:47:52 +0000

    Minor code style cleanup.

--- generic/tclBinary.c
+++ generic/tclBinary.c
@@ -639,7 +639,10 @@ TclAppendBytesToByteArray(
 		"TclAppendBytesToByteArray");
     }
     if (len == 0) {
-	/* Append zero bytes is a no-op. */
+	/*
+	 * Append zero bytes is a no-op.
+	 */
+
 	return;
     }
     if (objPtr->typePtr != &tclByteArrayType) {
