c25716f893a629a61c5651222482fcd0310e1ef9
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-07-10 21:02:58 +0000

    little tweaks, makeing it possible to use either _USE_64BIT_TIME_T or __MINGW_USE_VC2005_COMPAT interchangably. Put some more remarks, making it more clear what the effect of this is.

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -413,20 +413,23 @@ Tcl_CreateInterp(void)
 	Tcl_Panic("Tcl_CallFrame must not be smaller than CallFrame");
     }
 
-#if defined(_WIN32) && !defined(_WIN64)
-    if (sizeof(time_t) == 4 || sizeof(time_t) == 8) {
-	Tcl_StatBuf buf;
-	if (
-	    sizeof(buf.st_atime) != sizeof(time_t) ||
-	    sizeof(buf.st_mtime) != sizeof(time_t) ||
-	    sizeof(buf.st_ctime) != sizeof(time_t)
-	) {
-	    /*NOTREACHED*/
-	    Tcl_Panic("<sys/stat.h> is not compatible with MSVC");
-	}
-    } else {
+#if defined(_WIN32) && !defined(_WIN64) && !defined(_USE_64BIT_TIME_T) \
+	    && !defined(__MINGW_USE_VC2005_COMPAT)
+    /* If Tcl is compiled on Win32 using -D_USE_64BIT_TIME_T or
+     * -D__MINGW_USE_VC2005_COMPAT, the result is a binary incompatible
+     * with the 'standard' build of Tcl: All extensions using Tcl_StatBuf
+     * or interal functions like TclpGetDate() need to be recompiled in
+     * the same way. Therefore, this is not officially supported.
+     * In stead, it is recommended to use Win64 or Tcl 9.0 (not released yet)
+     */
+    if (sizeof(time_t) != 4) {
 	/*NOTREACHED*/
 	Tcl_Panic("<time.h> is not compatible with MSVC");
+    }
+    if ((TclOffset(Tcl_StatBuf,st_atime) != 32)
+	    || (TclOffset(Tcl_StatBuf,st_ctime) != 40)) {
+	/*NOTREACHED*/
+	Tcl_Panic("<sys/stat.h> is not compatible with MSVC");
     }
 #endif
 
