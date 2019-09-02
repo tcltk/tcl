3ba3d3bf65e0ebae194da207014355d8c322568d
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-09-02 11:33:06 +0000

    Tcl_UntraceVar() -> Tcl_UntraceVar2() and similar changes. Add @runstatedir@ to Makefile.in's (not used yet)

--- generic/tclEvent.c
+++ generic/tclEvent.c
@@ -1472,7 +1472,8 @@ VwaitVarProc(
     int *donePtr = clientData;
 
     *donePtr = 1;
-    Tcl_UntraceVar(interp, name1, TCL_TRACE_WRITES|TCL_TRACE_UNSETS,
+    Tcl_UntraceVar2(interp, name1, NULL,
+	    TCL_GLOBAL_ONLY|TCL_TRACE_WRITES|TCL_TRACE_UNSETS,
 	    VwaitVarProc, clientData);
     return NULL;
 }
