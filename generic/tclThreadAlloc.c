459a612caae54bcc4bd8d36895de2af9e3d719ba
jan.nijtmans <nijtmans@users.sourceforge.net>
2020-03-31 20:28:37 +0000

    Fix a few warning-message in --enable-symbols-mem mode

--- generic/tclThreadAlloc.c
+++ generic/tclThreadAlloc.c
@@ -1172,7 +1172,7 @@ TclFinalizeThreadAllocThread(void)
 
 void
 Tcl_GetMemoryInfo(
-    Tcl_DString *dsPtr)
+    TCL_UNUSED(Tcl_DString *))
 {
     Tcl_Panic("Tcl_GetMemoryInfo called when threaded memory allocator not in use");
 }
