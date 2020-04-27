c58164c7e5c7b56847b18efac346878b2657f04c
dgp <dgp@users.sourceforge.net>
2020-04-27 13:18:16 +0000

    silence compiler warning

--- generic/tclTest.c
+++ generic/tclTest.c
@@ -7171,7 +7171,7 @@ TestUtfNextCmd(
 	return TCL_OK;
     }
 
-    first = Tcl_UtfNext(buffer + 1);
+    first = result = Tcl_UtfNext(buffer + 1);
     while ((buffer[0] = *p++) != '\0') {
 	/* Run Tcl_UtfNext with many more possible bytes at src[-1], all should give the same result */
 	result = Tcl_UtfNext(buffer + 1);
