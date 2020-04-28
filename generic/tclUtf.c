73569ea5882b264c68b7e5f09f25ed22f04a8117
jan.nijtmans <nijtmans@users.sourceforge.net>
2020-04-28 09:46:07 +0000

    Change test expectations to what desired. Mark failing tests with "knownBug". 10 test-cases are affected. Still work do to ....

--- generic/tclUtf.c
+++ generic/tclUtf.c
@@ -1008,7 +1008,7 @@ Tcl_UtfPrev(
 	if (byte >= 0xC0) {
 	    /* Non-trail byte; May be multibyte lead. */
 
-	    if ((trailBytesSeen == 0)
+	    if ((trailBytesSeen == 0) ||
 		/*
 		 * We've seen no trailing context to use to check
 		 * anything. From what we know, this non-trail byte
@@ -1016,7 +1016,10 @@ Tcl_UtfPrev(
 		 * it (the fallback) is correct.
 		 */
 
-		    || (trailBytesSeen >= totalBytes[byte])) {
+#if 0
+		    ((byte < 0xF0) || (byte > 0xF4)) &&
+#endif
+		    (trailBytesSeen >= totalBytes[byte])) {
 		/*
 		 * That is, (1 + trailBytesSeen > needed).
 		 * We've examined more bytes than needed to complete
