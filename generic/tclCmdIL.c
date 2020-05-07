74330f28739f9f3c3020e7245ccf6710251534ae
jan.nijtmans <nijtmans@users.sourceforge.net>
2020-05-07 11:17:26 +0000

    Fix [fad64a857e76f98e]:  "lsearch" provides wrong errorCode with bad -stride option

--- generic/tclCmdIL.c
+++ generic/tclCmdIL.c
@@ -3305,7 +3305,7 @@ Tcl_LsearchObjCmd(
 	    if (groupSize < 1) {
 		Tcl_SetObjResult(interp, Tcl_NewStringObj(
 			"stride length must be at least 1", -1));
-		Tcl_SetErrorCode(interp, "TCL", "OPERATION", "LSORT",
+		Tcl_SetErrorCode(interp, "TCL", "OPERATION", "LSEARCH",
 			"BADSTRIDE", NULL);
 		result = TCL_ERROR;
 		goto done;
