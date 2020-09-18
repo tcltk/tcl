880d377a1018ddc26964c8040dd7e290b28a67fc
jan.nijtmans <nijtmans@users.sourceforge.net>
2020-09-18 08:33:51 +0000

    New macro TclNewIndexObj() which does the same as TclNewWideIntObjFromSize() but optimized the same way as TclNewIntObj().

--- generic/tclScan.c
+++ generic/tclScan.c
@@ -1089,7 +1089,7 @@ Tcl_ScanObjCmd(
     if (code == TCL_OK) {
 	if (underflow && (nconversions == 0)) {
 	    if (numVars) {
-		TclNewIntObj(objPtr, -1);
+		TclNewIndexObj(objPtr, -1);
 	    } else {
 		if (objPtr) {
 		    Tcl_SetListObj(objPtr, 0, NULL);
