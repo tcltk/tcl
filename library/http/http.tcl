1f4f47ccf50b4d6518c3dbfd9fd09c7bbb785929
jan.nijtmans <nijtmans@users.sourceforge.net>
2020-09-30 14:49:15 +0000

    (slightly) better error-message for invalid http -headers option. This works for plain 8.6 too

--- library/http/http.tcl
+++ library/http/http.tcl
@@ -733,7 +733,7 @@ proc http::geturl {url args} {
 	-strict		boolean
 	-timeout	integer
 	-validate	boolean
-	-headers	list
+	-headers	dict
     }
     set state(charset)	$defaultCharset
     set options {
@@ -747,10 +747,8 @@ proc http::geturl {url args} {
     foreach {flag value} $args {
 	if {[regexp -- $pat $flag]} {
 	    # Validate numbers
-	    if {
-		([info exists type($flag)] &&
-		![string is $type($flag) -strict $value]) ||
-		$flag eq "-headers" && [llength $value] %2 != 0
+	    if {($flag eq "-headers") ? [catch {dict size $value}] :
+		([info exists type($flag)] && ![string is $type($flag) -strict $value])
 	    } {
 		unset $token
 		return -code error \
