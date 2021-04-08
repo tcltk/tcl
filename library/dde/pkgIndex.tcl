6c72d33ef125ca3d477f90241f92be52c6132c58
jan.nijtmans <nijtmans@users.sourceforge.net>
2021-04-08 07:56:45 +0000

    Make "registry" and "dde" dll's for 8.x and 9.0 installable in the same directory. See TIP #595

--- library/dde/pkgIndex.tcl
+++ library/dde/pkgIndex.tcl
@@ -1,3 +1,9 @@
 if {![package vsatisfies [package provide Tcl] 8.5-]} return
 if {[info sharedlibextension] != ".dll"} return
-package ifneeded dde 1.4.4 [list load [file join $dir tcldde14.dll] Dde]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded dde 1.4.4 \
+	    [list load [file join $dir tcl9dde14.dll] Dde]
+} else {
+    package ifneeded dde 1.4.4 \
+	    [list load [file join $dir tcldde14.dll] Dde]
+}
