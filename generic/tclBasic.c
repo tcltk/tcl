5bdefd0c25145655f82ef005e51bf77df5fafbe6
pooryorick <com.digitalsmarties@pooryorick.com>
2021-05-17 21:34:25 +0000

    Remove unnecessary refCount decrement.

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -3389,7 +3389,6 @@ CallCommandTraces(
      */
 
     cmdPtr->flags &= ~CMD_TRACE_ACTIVE;
-    cmdPtr->refCount--;
     iPtr->activeCmdTracePtr = active.nextPtr;
     Tcl_Release(iPtr);
     return result;
