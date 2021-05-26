dbcca14632f0785d07bc62b42412abd56d1ee050
jan.nijtmans <nijtmans@users.sourceforge.net>
2021-05-26 10:45:17 +0000

    Fix [a73c79081e]: Doc fix in tcl.h, by not suggesting wchar_t any more for Tcl_UniChar.

--- generic/tcl.h
+++ generic/tcl.h
@@ -2159,10 +2159,8 @@ typedef struct Tcl_Parse {
 
 #if TCL_UTF_MAX > 3
     /*
-     * unsigned int isn't 100% accurate as it should be a strict 4-byte value
-     * (perhaps wchar_t). 64-bit systems may have troubles. The size of this
-     * value must be reflected correctly in regcustom.h and
-     * in tclEncoding.c.
+     * unsigned int isn't 100% accurate as it should be a strict 4-byte value.
+     * The size of this value must be reflected correctly in regcustom.h.
      * XXX: Tcl is currently UCS-2 and planning UTF-16 for the Unicode
      * XXX: string rep that Tcl_UniChar represents.  Changing the size
      * XXX: of Tcl_UniChar is /not/ supported.
