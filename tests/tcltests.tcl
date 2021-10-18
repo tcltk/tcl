d9f0f7a35d71e67c3e7cd0032a3d2581f2d1fee3
jan.nijtmans <nijtmans@users.sourceforge.net>
2021-10-18 08:56:26 +0000

    separate testConstraint nodep

--- tests/tcltests.tcl
+++ tests/tcltests.tcl
@@ -3,6 +3,7 @@
 package require tcltest 2.5
 namespace import ::tcltest::*
 testConstraint exec          [llength [info commands exec]]
+testConstraint nodep [tcl::build-info no-deprecate]
 testConstraint debug [tcl::build-info debug]
 testConstraint purify [tcl::build-info purify]
 testConstraint debugpurify [
