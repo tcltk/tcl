c3bcd951b8dd8bf57202915b9d914bcddc73b9bb
jan.nijtmans <nijtmans@users.sourceforge.net>
2022-01-17 17:26:37 +0000

    Possible fix for [e9a2715d91]: Tcl 8.6.11: Incompatible Tcl_GetRange()

--- generic/tclTest.c
+++ generic/tclTest.c
@@ -3918,7 +3918,7 @@ TestregexpObjCmd(
 	    if (ii == -1) {
 		TclRegExpRangeUniChar(regExpr, ii, &start, &end);
 		newPtr = Tcl_GetRange(objPtr, start, end);
-	    } else if (ii > info.nsubs) {
+	    } else if (ii > info.nsubs || info.matches[ii].end <= 0) {
 		newPtr = Tcl_NewObj();
 	    } else {
 		newPtr = Tcl_GetRange(objPtr, info.matches[ii].start,
