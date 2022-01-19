b97786c85dbd70fd4445f8161b205d5dbc56e844
jan.nijtmans <nijtmans@users.sourceforge.net>
2022-01-19 14:21:38 +0000

    (partial) fix for [https://core.tcl-lang.org/tk/tktview?name=a9929f112a|a9929f112a]. WIP

--- generic/tclUtil.c
+++ generic/tclUtil.c
@@ -3647,12 +3647,15 @@ GetWideForIndex(
 	if (numType == TCL_NUMBER_INT) {
 	    /* objPtr holds an integer in the signed wide range */
 	    *widePtr = *(Tcl_WideInt *)cd;
+	    if (*widePtr < -1) {
+		*widePtr = -1;
+	    }
 	    return TCL_OK;
 	}
 	if (numType == TCL_NUMBER_BIG) {
 	    /* objPtr holds an integer outside the signed wide range */
 	    /* Truncate to the signed wide range. */
-	    *widePtr = ((mp_isneg((mp_int *)cd)) ? WIDE_MIN : WIDE_MAX);
+	    *widePtr = ((mp_isneg((mp_int *)cd)) ? -1 : WIDE_MAX);
 	    return TCL_OK;
 	}
     }
