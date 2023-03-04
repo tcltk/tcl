6285c1336732a6a7db1fc3627dad6fe6176fbee6
jan.nijtmans <nijtmans@users.sourceforge.net>
2023-03-04 10:08:18 +0000

    Fix [1b8df10110]: Tcl_GetTime returns wrong usec value on Windows

--- generic/tclDecls.h
+++ generic/tclDecls.h
@@ -4035,7 +4035,7 @@ extern const TclStubs *tclStubsPtr;
 /* Handle Win64 tk.dll being loaded in Cygwin64. */
 #	define Tcl_GetTime(t) \
 		do { \
-		    union { \
+		    struct { \
 			Tcl_Time now; \
 			__int64 reserved; \
 		    } _t; \
