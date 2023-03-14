18a99f2522b77516b62a0d44dca1c90b3479bda1
jan.nijtmans <nijtmans@users.sourceforge.net>
2023-03-14 10:05:24 +0000

    Add "ucs-2" constraint to encoding-bug-183a1adcc0-5 testcase, otherwise it fails with TCL_UTF_MAX>3. Broken by [47857515422b8519|this] commit

--- win/tclWinTest.c
+++ win/tclWinTest.c
@@ -419,7 +419,7 @@ TestplatformChmod(
     const char *nativePath,
     int pmode)
 {
-    /* 
+    /*
      * Note FILE_DELETE_CHILD missing from dirWriteMask because we do
      * not want overriding of child's delete setting when testing
      */
@@ -427,7 +427,7 @@ TestplatformChmod(
 	FILE_WRITE_ATTRIBUTES | FILE_WRITE_EA |
 	FILE_ADD_FILE | FILE_ADD_SUBDIRECTORY | STANDARD_RIGHTS_WRITE | DELETE |
 	SYNCHRONIZE;
-    static const DWORD dirReadMask = 
+    static const DWORD dirReadMask =
 	FILE_READ_ATTRIBUTES | FILE_READ_EA | FILE_LIST_DIRECTORY |
 	STANDARD_RIGHTS_READ | SYNCHRONIZE;
     /* Note - default user privileges allow ignoring TRAVERSE setting */
@@ -437,7 +437,7 @@ TestplatformChmod(
     static const DWORD fileWriteMask =
 	FILE_WRITE_ATTRIBUTES | FILE_WRITE_EA | FILE_WRITE_DATA |
 	FILE_APPEND_DATA | STANDARD_RIGHTS_WRITE | DELETE | SYNCHRONIZE;
-    static const DWORD fileReadMask = 
+    static const DWORD fileReadMask =
 	FILE_READ_ATTRIBUTES | FILE_READ_EA | FILE_READ_DATA |
 	STANDARD_RIGHTS_READ | SYNCHRONIZE;
     static const DWORD fileExecuteMask =
@@ -471,7 +471,7 @@ TestplatformChmod(
     if (!OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
 	goto done;
     }
-    
+
     /* Get process SID */
     if (!GetTokenInformation(hToken, TokenUser, NULL, 0, &dw) &&
 	GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
@@ -489,7 +489,7 @@ TestplatformChmod(
 	ckfree(aceEntry[nSids].pSid); /* Since we have not ++'ed nSids */
 	goto done;
     }
-    /* 
+    /*
      * Always include DACL modify rights so we don't get locked out
      */
     aceEntry[nSids].mask = READ_CONTROL | WRITE_DAC | WRITE_OWNER | SYNCHRONIZE |
