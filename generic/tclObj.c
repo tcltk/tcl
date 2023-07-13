0900f426bd5dfc7335a664daccd8d35959255080
pooryorick <com.digitalsmarties@pooryorick.com>
2023-07-13 12:06:14 +0000

    Remove incorrect lines that were added to SetDuplicatePureObj(). SetDuplicatePureObj() is specifically not about preserving immutable string value semantics from the orginal object to the new object.

--- generic/tclObj.c
+++ generic/tclObj.c
@@ -1653,18 +1653,12 @@ int SetDuplicatePureObj(
      *
      * Perhaps in the future this can be remedied and this special treatment
      * removed.
-     *
-     * Similar problem with the integer (0x0A vs 10), double (1e-1 vs 0.1) and
-     * index types ("coord" vs "coords", see bug [a34733451b])
      */
 
 
     if (bytes && (dupPtr->typePtr == NULL
 	|| dupPtr->typePtr->updateStringProc == NULL
 	|| useTypePtr == &tclStringType
-	|| useTypePtr == &tclDoubleType
-	|| useTypePtr == &tclIntType
-	|| useTypePtr == &tclIndexType
 	)
     ) {
 	if (!TclAttemptInitStringRep(dupPtr, bytes, objPtr->length)) {
