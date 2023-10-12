2a8151fd1a52f6e52b76fb628af9d699cc580b35
jan.nijtmans <nijtmans@users.sourceforge.net>
2023-10-12 14:46:42 +0000

    Those type-casts don't work well in C++

--- generic/tclDecls.h
+++ generic/tclDecls.h
@@ -3973,12 +3973,14 @@ extern const TclStubs *tclStubsPtr;
 	    (tclStubsPtr->tcl_SetVar(interp, varName, newValue, flags))
 #   define Tcl_ObjSetVar2(interp, part1, part2, newValue, flags) \
 	    (tclStubsPtr->tcl_ObjSetVar2(interp, part1, part2, newValue, flags))
+#ifndef __cplusplus
 #   undef Tcl_EventuallyFree
 #   define Tcl_EventuallyFree \
 	   ((void (*)(void *,void *))(void *)(tclStubsPtr->tcl_EventuallyFree)) /* 132 */
 #   undef Tcl_SetResult
 #   define Tcl_SetResult \
 	   ((void (*)(Tcl_Interp *, char *, void *))(void *)(tclStubsPtr->tcl_SetResult)) /* 232 */
+#endif
 #endif
 
 #if defined(_WIN32) && defined(UNICODE)
