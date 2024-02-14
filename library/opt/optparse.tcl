6e1b2262d9d1d4a4bbd41e751259ec4325cdd61b
dgp <dgp@users.sourceforge.net>
2024-02-14 18:54:29 +0000

    There have been changes to the package opt.  Bump to opt 0.4.9

--- library/opt/optparse.tcl
+++ library/opt/optparse.tcl
@@ -11,7 +11,7 @@
 package require Tcl 8.5-
 # When this version number changes, update the pkgIndex.tcl file
 # and the install directory in the Makefiles.
-package provide opt 0.4.8
+package provide opt 0.4.9
 
 namespace eval ::tcl {
 
