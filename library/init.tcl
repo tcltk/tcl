6d676d38f1a54795f9ae06e4c002674faed56874
jan.nijtmans <nijtmans@users.sourceforge.net>
2024-03-05 16:12:04 +0000

    b1 -> b2 (preparation for release)

--- library/init.tcl
+++ library/init.tcl
@@ -15,7 +15,7 @@
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
 
-package require -exact tcl 9.0b1
+package require -exact tcl 9.0b2
 
 # Compute the auto path to use in this interpreter.
 # The values on the path come from several locations:
