615c4be9ca12ae3c5e726b007b5a8dd111d2c08e
jan.nijtmans <nijtmans@users.sourceforge.net>
2024-03-27 20:54:34 +0000

    Libtommath 1.3

--- libtommath/bn_mp_expt_n.c
+++ libtommath/bn_mp_expt_n.c
@@ -1,13 +1,22 @@
 #include "tommath_private.h"
-#ifdef BN_MP_EXPT_U32_C
+#ifdef BN_MP_EXPT_N_C
 /* LibTomMath, multiple-precision integer library -- Tom St Denis */
 /* SPDX-License-Identifier: Unlicense */
 
-/* calculate c = a**b  using a square-multiply algorithm */
+#ifdef BN_MP_EXPT_U32_C
 mp_err mp_expt_u32(const mp_int *a, uint32_t b, mp_int *c)
 {
-   mp_err err;
+   if (b > MP_MIN(MP_DIGIT_MAX, INT_MAX)) {
+      return MP_VAL;
+   }
+   return mp_expt_n(a, (int)b, c);
+}
+#endif
 
+/* calculate c = a**b  using a square-multiply algorithm */
+mp_err mp_expt_n(const mp_int *a, int b, mp_int *c)
+{
+   mp_err err;
    mp_int  g;
 
    if ((err = mp_init_copy(&g, a)) != MP_OKAY) {
@@ -17,16 +26,16 @@ mp_err mp_expt_u32(const mp_int *a, uint32_t b, mp_int *c)
    /* set initial result */
    mp_set(c, 1uL);
 
-   while (b > 0u) {
+   while (b > 0) {
       /* if the bit is set multiply */
-      if ((b & 1u) != 0u) {
+      if ((b & 1) != 0) {
          if ((err = mp_mul(c, &g, c)) != MP_OKAY) {
             goto LBL_ERR;
          }
       }
 
       /* square */
-      if (b > 1u) {
+      if (b > 1) {
          if ((err = mp_sqr(&g, &g)) != MP_OKAY) {
             goto LBL_ERR;
          }
@@ -35,8 +44,6 @@ mp_err mp_expt_u32(const mp_int *a, uint32_t b, mp_int *c)
       /* shift to next bit */
       b >>= 1;
    }
-
-   err = MP_OKAY;
 
 LBL_ERR:
    mp_clear(&g);
