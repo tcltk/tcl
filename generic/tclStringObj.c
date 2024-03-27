6f10e3c7f3d3bd12692703316f2bd8e62cc932c0
apnadkarni <apnmbx-wits@yahoo.com>
2024-03-27 16:14:38 +0000

    Proposed fix for [edb4b065f4]

--- generic/tclStringObj.c
+++ generic/tclStringObj.c
@@ -3717,7 +3717,7 @@ TclStringCmp(
 	    if (empty > 0) {
 		switch (TclCheckEmptyString(value2Ptr)) {
 		case -1:
-		    s1 = 0;
+		    s1 = "";
 		    s1len = 0;
 		    s2 = TclGetStringFromObj(value2Ptr, &s2len);
 		    break;
@@ -3732,7 +3732,7 @@ TclStringCmp(
 	    } else if (TclCheckEmptyString(value2Ptr) > 0) {
 		switch (empty) {
 		case -1:
-		    s2 = 0;
+		    s2 = "";
 		    s2len = 0;
 		    s1 = TclGetStringFromObj(value1Ptr, &s1len);
 		    break;
