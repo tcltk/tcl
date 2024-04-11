3d40f7759711c4b7d96527d9e17715064fafe7ee
jan.nijtmans <nijtmans@users.sourceforge.net>
2024-04-11 12:35:07 +0000

    Use Tcl_NewBooleanObj for booleans; we should say what we mean. Fix sentinels and type-casts.

--- generic/tclNotify.c
+++ generic/tclNotify.c
@@ -276,7 +276,7 @@ Tcl_CreateEventSource(
 				 * checkProc. */
 {
     ThreadSpecificData *tsdPtr = TCL_TSD_INIT(&dataKey);
-    EventSource *sourcePtr = ckalloc(sizeof(EventSource));
+    EventSource *sourcePtr = (EventSource *)ckalloc(sizeof(EventSource));
 
     sourcePtr->setupProc = setupProc;
     sourcePtr->checkProc = checkProc;
