4dad6a40586ac4c41fa501687980e22341350e3d
jan.nijtmans <nijtmans@users.sourceforge.net>
2024-06-13 08:57:12 +0000

    Use tabs for indenting in stead of 8 spaces

--- library/reg/pkgIndex.tcl
+++ library/reg/pkgIndex.tcl
@@ -2,8 +2,8 @@ if {![package vsatisfies [package provide Tcl] 8.5]} return
 if {[info sharedlibextension] != ".dll"} return
 if {[::tcl::pkgconfig get debug]} {
     package ifneeded registry 1.3.5 \
-            [list load [file join $dir tclreg13g.dll] Registry]
+	    [list load [file join $dir tclreg13g.dll] Registry]
 } else {
     package ifneeded registry 1.3.5 \
-            [list load [file join $dir tclreg13.dll] Registry]
+	    [list load [file join $dir tclreg13.dll] Registry]
 }
