000ec97ca2930a1a8a003d97239db12794c0317d
dkf <donal.k.fellows@manchester.ac.uk>
2024-08-17 19:09:40 +0000

    Define TclNewNamespaceObj() to capture common idiom

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -3535,6 +3535,7 @@ MODULE_SCOPE Tcl_Obj *	TclNewArithSeriesObj(Tcl_Interp *interp,
 			    Tcl_Obj *stepObj, Tcl_Obj *lenObj);
 MODULE_SCOPE Tcl_Obj *	TclNewFSPathObj(Tcl_Obj *dirPtr, const char *addStrRep,
 			    Tcl_Size len);
+MODULE_SCOPE Tcl_Obj *	TclNewNamespaceObj(Tcl_Namespace *namespacePtr);
 MODULE_SCOPE void	TclpAlertNotifier(void *clientData);
 MODULE_SCOPE void *	TclpNotifierData(void);
 MODULE_SCOPE void	TclpServiceModeHook(int mode);
