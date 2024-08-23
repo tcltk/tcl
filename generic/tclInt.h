3fcdcb78b8886afa6230579b2d3f3cc7a0cf95ec
jan.nijtmans <nijtmans@users.sourceforge.net>
2024-08-23 12:12:52 +0000

    Documentation/comment fixes [dcc8645349]

--- generic/tclInt.h
+++ generic/tclInt.h
@@ -3439,7 +3439,7 @@ MODULE_SCOPE char *	TclGetStringStorage(Tcl_Obj *objPtr,
 			    Tcl_Size *sizePtr);
 MODULE_SCOPE int	TclGetLoadedLibraries(Tcl_Interp *interp,
 				const char *targetName,
-				const char *packageName);
+				const char *prefix);
 MODULE_SCOPE int	TclGetWideBitsFromObj(Tcl_Interp *, Tcl_Obj *,
 				Tcl_WideInt *);
 MODULE_SCOPE int	TclCompareStringKeys(void *keyPtr, Tcl_HashEntry *hPtr);
