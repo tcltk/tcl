a680486042c9cb4f65899abb15dbc6bdcec5ec0a
sebres <sebres@users.sourceforge.net>
2024-09-17 14:19:53 +0000

    improve comments (no functional changes)

--- unix/tclUnixChan.c
+++ unix/tclUnixChan.c
@@ -1423,6 +1423,8 @@ TclpOpenFileChannel(
 	     * We need this just to ensure we return the correct error messages under
 	     * some circumstances (relative paths only), so because the normalization 
 	     * is very expensive, don't invoke it for native or absolute paths.
+         * Note: since paths starting with ~ are absolute, it also considers tilde expansion,
+         * (proper error message of tests *io-40.17 "tilde substitution in open")
 	     */
 	    if (
 		(
--- win/tclWinChan.c
+++ win/tclWinChan.c
@@ -871,7 +871,7 @@ TclpOpenFileChannel(
 	     * We need this just to ensure we return the correct error messages under
 	     * some circumstances (relative paths only), so because the normalization 
 	     * is very expensive, don't invoke it for native or absolute paths.
-	     * Note: TODO - check one needs consider tilde expansion after TIP#602,
+	     * Note: since paths starting with ~ are absolute, it also considers tilde expansion,
 	     * (proper error message of tests *io-40.17 "tilde substitution in open")
 	     */
 	    if (
