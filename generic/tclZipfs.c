906c3058eab5d871756de4de703f2d75d7363574
sebres <sebres@users.sourceforge.net>
2024-09-17 15:17:44 +0000

    fixes further normalization bottleneck of 8.7+ [02d5d65d70adab97], guess [411f52ed87e313dd49e2] too (zipfs now)

--- generic/tclZipfs.c
+++ generic/tclZipfs.c
@@ -5858,9 +5858,26 @@ ZipFSPathInFilesystemProc(
     Tcl_Size len;
     char *path;
 
-    pathPtr = Tcl_FSGetNormalizedPath(NULL, pathPtr);
-    if (!pathPtr) {
-	return -1;
+    if (TclFSCwdIsNative() || Tcl_FSGetPathType(pathPtr) == TCL_PATH_ABSOLUTE) {
+	/*
+	 * The cwd is native (or path is absolute), use the translated path
+	 * without worrying about normalization (this will also usually be
+	 * shorter so the utf-to-external conversion will be somewhat faster).
+	 */
+
+	pathPtr = Tcl_FSGetTranslatedPath(NULL, pathPtr);
+	if (pathPtr == NULL) {
+	    return -1;
+	}
+    } else {
+	/*
+	 * Make sure the normalized path is set.
+	 */
+
+	pathPtr = Tcl_FSGetNormalizedPath(NULL, pathPtr);
+	if (!pathPtr) {
+	    return -1;
+	}
     }
     path = TclGetStringFromObj(pathPtr, &len);
 
