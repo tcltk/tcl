02c98f87b83fc1d519cec52d08182a8392341fa7
sebres <sebres@users.sourceforge.net>
2025-03-06 19:47:56 +0000

    amend to [ba68d1e9484a3a92]: rewrite fix of Tcl_GetReturnOptions less invasive (-errorline was always in return options dict previously, shall not be missed now, and reset line to 1 instead of 0, as less unexpected value)

--- generic/tclResult.c
+++ generic/tclResult.c
@@ -1610,7 +1610,7 @@ Tcl_GetReturnOptions(
 	     * interp), because we have anyway nothing excepting message
 	     * in the backtrace.
 	     */
-	    iPtr->errorLine = 0;
+	    iPtr->errorLine = 1;
 	}
 	Tcl_AddErrorInfo(interp, "");
 	Tcl_DictObjPut(NULL, options, keys[KEY_ERRORSTACK], iPtr->errorStack);
@@ -1620,10 +1620,8 @@ Tcl_GetReturnOptions(
     }
     if (iPtr->errorInfo) {
 	Tcl_DictObjPut(NULL, options, keys[KEY_ERRORINFO], iPtr->errorInfo);
-	if (iPtr->errorLine) {
-	    Tcl_DictObjPut(NULL, options, keys[KEY_ERRORLINE],
-		    Tcl_NewIntObj(iPtr->errorLine));
-	}
+	Tcl_DictObjPut(NULL, options, keys[KEY_ERRORLINE],
+		Tcl_NewIntObj(iPtr->errorLine));
     }
     return options;
 }
