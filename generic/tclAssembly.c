bf691d3163e7359f714b72eabb44afe00058c30e
dkf <donal.k.fellows@manchester.ac.uk>
2025-04-17 09:07:25 +0000

    Add INST_TCLOO_ID/tclooId to TAL

--- generic/tclAssembly.c
+++ generic/tclAssembly.c
@@ -470,6 +470,7 @@ static const TalInstDesc TalInstructionTable[] = {
     {"sub",		ASSEM_1BYTE,	INST_SUB,		2,	1},
     {"swap",		ASSEM_1BYTE,	INST_SWAP,		2,	2},
     {"tclooClass",	ASSEM_1BYTE,	INST_TCLOO_CLASS,	1,	1},
+    {"tclooId",		ASSEM_1BYTE,	INST_TCLOO_ID,		1,	1},
     {"tclooIsObject",	ASSEM_1BYTE,	INST_TCLOO_IS_OBJECT,	1,	1},
     {"tclooNamespace",	ASSEM_1BYTE,	INST_TCLOO_NS,		1,	1},
     {"tclooSelf",	ASSEM_1BYTE,	INST_TCLOO_SELF,	0,	1},
