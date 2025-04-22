5ea2ee0bfbf5deb8a7ea50eabddb391c3dcd54c9
dgp <dgp@users.sourceforge.net>
2025-04-22 16:30:58 +0000

    Restore TclLog2() status to be available only for --enable-symbols=compile

--- generic/tclCompile.h
+++ generic/tclCompile.h
@@ -1154,6 +1154,7 @@ MODULE_SCOPE void	TclFinalizeLoopExceptionRange(CompileEnv *envPtr,
 			    int range);
 #ifdef TCL_COMPILE_STATS
 MODULE_SCOPE char *	TclLiteralStats(LiteralTable *tablePtr);
+MODULE_SCOPE int	TclLog2(long long value);
 #endif
 MODULE_SCOPE size_t	TclLocalScalar(const char *bytes, size_t numBytes,
 			    CompileEnv *envPtr);
