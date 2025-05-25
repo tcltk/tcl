f9f766a0384a734b1bb2c0ecdc2e0d728b98b17e
apnadkarni <apnmbx-wits@yahoo.com>
2025-05-25 17:27:37 +0000

    Start on [7346adc50f]. Now raise error on truncated encoding. Still have to handle replace and tcl8 profiles.

--- generic/tclEncoding.c
+++ generic/tclEncoding.c
@@ -1228,7 +1228,7 @@ Tcl_ExternalToUtfDStringEx(
 	 * and loop. Otherwise, return the result we got.
 	 */
 	if ((result != TCL_CONVERT_NOSPACE) &&
-		!(result == TCL_CONVERT_MULTIBYTE && (flags & TCL_ENCODING_END))) {
+		(result != TCL_CONVERT_MULTIBYTE || (flags & TCL_ENCODING_END))) {
 	    Tcl_Size nBytesProcessed = (src - srcStart);
 
 	    Tcl_DStringSetLength(dstPtr, soFar);
