3ec641850ec3b54abfcfbec94798c97feb59c6ff
dkf <donal.k.fellows@manchester.ac.uk>
2025-06-10 13:53:59 +0000

    Define an instruction to build an arithmetic series. Not yet issued

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -955,6 +955,17 @@ InstructionDesc const tclInstructionTable[] = {
 	 * Stack:  ... value => ...
 	 * Note that the jump table contains offsets relative to the PC when
 	 * it points to this instruction; the code is relocatable. */
+    TCL_INSTRUCTION_ENTRY1(
+	"arithSeries",	  2,	-3,	  OPERAND_UINT1),
+	/* Push a new arithSeries object on the stack. The opnd is a bit mask
+	 * stating which values are valid; bit 0 -> from, bit 1 -> to,
+	 * bit 2 -> step, bit 3 -> count. Invalid values are passed to
+	 * TclNewArithSeriesObj() as NULL (and the corresponding values on the
+	 * stack simply are ignored). If bit 4 is set, the useDoubles flag will
+	 * be true, and if bit 5 is set, the useDoubles flag will be false. If
+	 * neither is set, the type of the arguments will be examined to
+	 * determine what to pass for the flag.
+	 * Stack:  ... from to step count => ... series */
 
     {NULL, 0, 0, 0, {OPERAND_NONE}}
 };
