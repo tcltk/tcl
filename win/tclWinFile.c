a3d0c99f90160622ddd46eec293b47ae18781939
sebres <sebres@users.sourceforge.net>
2025-07-18 17:36:53 +0000

    cherry-pick to 8.7: fixes [61c01e0edb08a9ed] (TclpMatchInDirectory, windows only), increment ref-count to avoid use-after-free for interim normalized path object, what may cause crash (SF/BO), usage of wrong path, etc.

--- win/tclWinFile.c
+++ win/tclWinFile.c
@@ -974,6 +974,7 @@ TclpMatchInDirectory(
 	if (fileNamePtr == NULL) {
 	    return TCL_ERROR;
 	}
+	Tcl_IncrRefCount(fileNamePtr); /* ensure it'd be alive, while used. */
 
 	/*
 	 * Verify that the specified path exists and is actually a directory.
@@ -981,12 +982,14 @@ TclpMatchInDirectory(
 
 	native = (const WCHAR *)Tcl_FSGetNativePath(pathPtr);
 	if (native == NULL) {
+	    Tcl_DecrRefCount(fileNamePtr);
 	    return TCL_OK;
 	}
 	attr = GetFileAttributesW(native);
 
 	if ((attr == INVALID_FILE_ATTRIBUTES)
 		|| ((attr & FILE_ATTRIBUTE_DIRECTORY) == 0)) {
+	    Tcl_DecrRefCount(fileNamePtr);
 	    return TCL_OK;
 	}
 
@@ -1004,6 +1007,7 @@ TclpMatchInDirectory(
 	    TclDStringAppendLiteral(&dsOrig, "/");
 	    dirLength++;
 	}
+	Tcl_DecrRefCount(fileNamePtr);
 	dirName = Tcl_DStringValue(&dsOrig);
 
 	/*
