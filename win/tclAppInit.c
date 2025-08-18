3147d6bfd8d3d34b5a6225f12471129c45da079d
sebres <sebres@users.sourceforge.net>
2025-08-18 21:46:13 +0000

    circumvent for [8a13caa31dd8e3a5]: load tcltest if needed (using `load {} Tcltest`) - as a result tcltest executable behaves more similar to tclsh and shalln't cause any side effects anymore; this would also speedup interpreter creation from tcltest executable (where tcltest module isn't needed).

--- win/tclAppInit.c
+++ win/tclAppInit.c
@@ -176,25 +176,6 @@ Tcl_AppInit(
 	return TCL_ERROR;
     }
 
-#if defined(STATIC_BUILD)
-    if (Registry_Init(interp) == TCL_ERROR) {
-	return TCL_ERROR;
-    }
-    Tcl_StaticLibrary(interp, "Registry", Registry_Init, NULL);
-
-    if (Dde_Init(interp) == TCL_ERROR) {
-	return TCL_ERROR;
-    }
-    Tcl_StaticLibrary(interp, "Dde", Dde_Init, Dde_SafeInit);
-#endif
-
-#ifdef TCL_TEST
-    if (Tcltest_Init(interp) == TCL_ERROR) {
-	return TCL_ERROR;
-    }
-    Tcl_StaticLibrary(interp, "Tcltest", Tcltest_Init, Tcltest_SafeInit);
-#endif /* TCL_TEST */
-
     /*
      * Call the init procedures for included packages. Each call should look
      * like this:
