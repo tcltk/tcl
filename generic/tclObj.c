866ce7330cbe056cb99e222a3a3a25d988daee19
dgp <dgp@users.sourceforge.net>
2025-09-04 17:57:13 +0000

    [5927f87da3] Remove outdated limit on argument size.

--- generic/tclObj.c
+++ generic/tclObj.c
@@ -3812,14 +3812,6 @@ Tcl_GetNumber(
     if (numBytes < 0) {
 	numBytes = strlen(bytes);
     }
-    if (numBytes > INT_MAX) {
-	if (interp) {
-	    Tcl_SetObjResult(interp, Tcl_ObjPrintf(
-		    "max size for a Tcl value (%d bytes) exceeded", INT_MAX));
-	    Tcl_SetErrorCode(interp, "TCL", "MEMORY", (char *)NULL);
-	}
-	return TCL_ERROR;
-    }
 
     objPtr->bytes = (char *) bytes;
     objPtr->length = numBytes;
