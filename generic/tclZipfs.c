0a4432724f5eb188ad697e5623fd4c5d22adf02a
apnadkarni <apnmbx-wits@yahoo.com>
2025-09-18 06:41:34 +0000

    Fix leak in zipfs encoding initialization

--- generic/tclZipfs.c
+++ generic/tclZipfs.c
@@ -6420,16 +6420,17 @@ TclZipfsInitEncodingDirs(void)
     if (zipfs_literal_tcl_library == NULL) {
 	return TCL_ERROR;
     }
-    Tcl_Obj *libDirObj = Tcl_NewStringObj(zipfs_literal_tcl_library, -1);
     Tcl_Obj *subDirObj, *searchPathObj;
-
+    Tcl_Obj *libDirObj = Tcl_NewStringObj(zipfs_literal_tcl_library, -1);
+    Tcl_IncrRefCount(libDirObj);
     TclNewLiteralStringObj(subDirObj, "encoding");
     Tcl_IncrRefCount(subDirObj);
     TclNewObj(searchPathObj);
     Tcl_ListObjAppendElement(NULL, searchPathObj,
 	    Tcl_FSJoinToPath(libDirObj, 1, &subDirObj));
-    Tcl_DecrRefCount(subDirObj);
     Tcl_IncrRefCount(searchPathObj);
+    Tcl_DecrRefCount(subDirObj);
+    Tcl_DecrRefCount(libDirObj);
     Tcl_SetEncodingSearchPath(searchPathObj);
     Tcl_DecrRefCount(searchPathObj);
     /* Reinit system encoding after setting search path */
