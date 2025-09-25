621440c424218e3432eb6f5f280cab62937594e3
dkf <donal.k.fellows@manchester.ac.uk>
2025-09-25 08:03:25 +0000

    Updates to the cookiejar code to use features of 9.0

--- library/cookiejar/cookiejar.tcl
+++ library/cookiejar/cookiejar.tcl
@@ -8,7 +8,7 @@
 # this file, and for a DISCLAIMER OF ALL WARRANTIES.
 
 # Dependencies
-package require Tcl 8.6-
+package require Tcl 9.0-
 package require http 2.8.4
 package require sqlite3
 package require tcl::idna 1.0
@@ -21,116 +21,125 @@ package require tcl::idna 1.0
 if {![llength [info commands ::http::cookiejar]]} {
     ::oo::class create ::http::cookiejar
 }
-
-namespace eval [info object namespace ::http::cookiejar] {
-    proc setInt {*var val} {
-	upvar 1 ${*var} var
-	if {[catch {incr dummy $val} msg]} {
-	    return -code error $msg
-	}
-	set var $val
-    }
-    proc setInterval {trigger *var val} {
-	upvar 1 ${*var} var
-	if {![string is integer -strict $val] || $val < 1} {
-	    return -code error "expected positive integer but got \"$val\""
-	}
-	set var $val
-	{*}$trigger
-    }
-    proc setBool {*var val} {
-	upvar 1 ${*var} var
-	if {[catch {if {$val} {}} msg]} {
-	    return -code error $msg
-	}
-	set var [expr {!!$val}]
-    }
-
-    proc setLog {*var val} {
-	upvar 1 ${*var} var
-	set var [::tcl::prefix match -message "log level" \
-		{debug info warn error} $val]
-    }
-
-    # Keep this in sync with pkgIndex.tcl and with the install directories in
-    # Makefiles
-    variable version 0.2.0
-
-    variable domainlist \
-	https://publicsuffix.org/list/public_suffix_list.dat
-    variable domainfile \
-	[file join [file dirname [info script]] public_suffix_list.dat.gz]
-    # The list is directed to from http://publicsuffix.org/list/
-    variable loglevel info
-    variable vacuumtrigger 200
-    variable retainlimit 100
-    variable offline false
-    variable purgeinterval 60000
-    variable refreshinterval 10000000
-    variable domaincache {}
-
-    # Some support procedures, none particularly useful in general
-    namespace eval support {
-	# Set up a logger if the http package isn't actually loaded yet.
-	if {![llength [info commands ::http::Log]]} {
-	    proc ::http::Log args {
-		# Do nothing by default...
-	    }
-	}
-
-	namespace export *
-	proc locn {secure domain path {key ""}} {
-	    if {$key eq ""} {
-		format "%s://%s%s" [expr {$secure?"https":"http"}] \
-		    [::tcl::idna encode $domain] $path
-	    } else {
-		format "%s://%s%s?%s" \
-		    [expr {$secure?"https":"http"}] [::tcl::idna encode $domain] \
-		    $path $key
-	    }
-	}
-	proc splitDomain domain {
-	    set pieces [split $domain "."]
-	    for {set i [llength $pieces]} {[incr i -1] >= 0} {} {
-		lappend result [join [lrange $pieces $i end] "."]
-	    }
-	    return $result
-	}
-	proc splitPath path {
-	    set pieces [split [string trimleft $path "/"] "/"]
-	    set result /
-	    for {set j 0} {$j < [llength $pieces]} {incr j} {
-		lappend result /[join [lrange $pieces 0 $j] "/"]
-	    }
-	    return $result
-	}
-	proc isoNow {} {
-	    set ms [clock milliseconds]
-	    set ts [expr {$ms / 1000}]
-	    set ms [format %03d [expr {$ms % 1000}]]
-	    clock format $ts -format "%Y%m%dT%H%M%S.${ms}Z" -gmt 1
-	}
-	proc log {level msg args} {
-	    namespace upvar [info object namespace ::http::cookiejar] \
-		loglevel loglevel
-	    set who [uplevel 1 self class]
-	    set mth [uplevel 1 self method]
-	    set map {debug 0 info 1 warn 2 error 3}
-	    if {[string map $map $level] >= [string map $map $loglevel]} {
-		set msg [format $msg {*}$args]
-		set LVL [string toupper $level]
-		::http::Log "[isoNow] $LVL $who $mth - $msg"
-	    }
-	}
-    }
-}
-
-# Now we have enough information to provide the package.
-package provide cookiejar \
-    [set [info object namespace ::http::cookiejar]::version]
 
 # The implementation of the cookiejar package
 ::oo::define ::http::cookiejar {
+    initialise {
+	# Keep this in sync with pkgIndex.tcl and with the install directories in
+	# Makefiles
+	const version 0.2.1
+
+	proc setInt {*var val} {
+	    upvar 1 ${*var} var
+	    if {[catch {incr dummy $val} msg]} {
+		return -code error $msg
+	    }
+	    set var $val
+	}
+	proc setInterval {trigger *var val} {
+	    upvar 1 ${*var} var
+	    if {![string is integer -strict $val] || $val < 1} {
+		return -code error "expected positive integer but got \"$val\""
+	    }
+	    set var $val
+	    {*}$trigger
+	}
+	proc setBool {*var val} {
+	    upvar 1 ${*var} var
+	    if {[catch {if {$val} {}} msg]} {
+		return -code error $msg
+	    }
+	    set var [expr {!!$val}]
+	}
+
+	proc setLog {*var val} {
+	    upvar 1 ${*var} var
+	    set var [::tcl::prefix match -message "log level" \
+		    {debug info warn error} $val]
+	}
+
+	variable domainlist \
+		https://publicsuffix.org/list/public_suffix_list.dat
+	variable domainfile \
+		[file join [file dirname [info script]] public_suffix_list.dat.gz]
+	# The list is directed to from http://publicsuffix.org/list/
+	variable loglevel info
+	variable vacuumtrigger 200
+	variable retainlimit 100
+	variable offline false
+	variable purgeinterval 60000
+	variable refreshinterval 10000000
+	variable domaincache {}
+
+	# Some support procedures, none particularly useful in general
+	namespace eval support {
+	    # Set up a logger if the http package isn't actually loaded yet.
+	    if {![llength [info commands ::http::Log]]} {
+		proc ::http::Log args {
+		    # Do nothing by default...
+		}
+	    }
+
+	    namespace export *
+	    proc locn {secure domain path {key ""}} {
+		if {$key eq ""} {
+		    format "%s://%s%s" [expr {$secure?"https":"http"}] \
+			[::tcl::idna encode $domain] $path
+		} else {
+		    format "%s://%s%s?%s" \
+			[expr {$secure?"https":"http"}] [::tcl::idna encode $domain] \
+			$path $key
+		}
+	    }
+	    proc splitDomain domain {
+		set pieces [split $domain "."]
+		for {set i [llength $pieces]} {[incr i -1] >= 0} {} {
+		    lappend result [join [lrange $pieces $i end] "."]
+		}
+		return $result
+	    }
+	    proc splitPath path {
+		set pieces [split [string trimleft $path "/"] "/"]
+		set result /
+		for {set j 0} {$j < [llength $pieces]} {incr j} {
+		    lappend result /[join [lrange $pieces 0 $j] "/"]
+		}
+		return $result
+	    }
+	    proc isoNow {} {
+		set ms [clock milliseconds]
+		set ts [expr {$ms / 1000}]
+		set ms [format %03d [expr {$ms % 1000}]]
+		clock format $ts -format "%Y%m%dT%H%M%S.${ms}Z" -gmt 1
+	    }
+	    proc log {level msg args} {
+		namespace upvar [info object namespace ::http::cookiejar] \
+			loglevel loglevel
+		set who [uplevel 1 self class]
+		set mth [uplevel 1 self method]
+		set map {debug 0 info 1 warn 2 error 3}
+		if {[string map $map $level] >= [string map $map $loglevel]} {
+		    set msg [format $msg {*}$args]
+		    set LVL [string toupper $level]
+		    ::http::Log "[isoNow] $LVL $who $mth - $msg"
+		}
+	    }
+	    proc names {line} {
+		set idna [string tolower [::tcl::idna encode $line]]
+		set utf [::tcl::idna decode [string tolower $line]]
+		if {$utf ne [::tcl::idna decode [string tolower $idna]]} {
+		    uplevel 1 [list log warn \
+			    "mismatch in IDNA handling for %s (%d, %s, %s)" \
+			    $idna $line $utf [::tcl::idna decode $idna]]
+		}
+		return [lsort -unique [list $idna $utf]]
+	    }
+	}
+
+	# Now we have enough information to provide the package.
+	package provide cookiejar $version
+    }
+
     self {
 	method configure {{optionName "\x00\x00"} {optionValue "\x00\x00"}} {
 	    set tbl {
@@ -143,12 +152,8 @@ package provide cookiejar \
 		-retain        {retainlimit setInt}
 		-vacuumtrigger {vacuumtrigger setInt}
 	    }
-	    dict lappend tbl -domainrefresh [namespace code {
-		my IntervalTrigger PostponeRefresh
-	    }]
-	    dict lappend tbl -purgeold [namespace code {
-		my IntervalTrigger PostponePurge
-	    }]
+	    dict lappend tbl -domainrefresh [callback IntervalTrigger PostponeRefresh]
+	    dict lappend tbl -purgeold [callback IntervalTrigger PostponePurge]
 	    if {$optionName eq "\x00\x00"} {
 		return [dict keys $tbl]
 	    }
@@ -173,6 +178,8 @@ package provide cookiejar \
     variable purgeTimer deletions refreshTimer
     constructor {{path ""}} {
 	namespace import [info object namespace [self class]]::support::*
+	set purgeTimer {}
+	set refreshTimer {}
 
 	if {$path eq ""} {
 	    sqlite3 [namespace current]::db :memory:
@@ -282,22 +289,31 @@ package provide cookiejar \
     }
 
     method PostponePurge {} {
-	namespace upvar [info object namespace [self class]] \
-	    purgeinterval interval
-	catch {after cancel $purgeTimer}
-	set purgeTimer [after $interval [namespace code {my PurgeCookies}]]
+	classvariable purgeinterval
+	after cancel $purgeTimer
+	set purgeTimer [after $purgeinterval [callback PurgeCookies]]
     }
 
     method PostponeRefresh {} {
-	namespace upvar [info object namespace [self class]] \
-	    refreshinterval interval
-	catch {after cancel $refreshTimer}
-	set refreshTimer [after $interval [namespace code {my RefreshDomains}]]
+	classvariable refreshinterval
+	after cancel $refreshTimer
+	set refreshTimer [after $refreshinterval [callback RefreshDomains]]
     }
 
     method RefreshDomains {} {
-	# TODO: domain list refresh policy
+	classvariable offline
 	my PostponeRefresh
+	if {!$offline} {
+	    try {
+		set data [my GetDomainListOnline metadata]
+	    } on error {} {
+		log warn "failed to retreve domain list"
+	    } on ok {} {
+		if {$data ne ""} {
+		    my InstallDomainData $data $metadata true
+		}
+	    }
+	}
     }
 
     method HttpGet {url {timeout 0} {maxRedirects 5}} {
@@ -324,59 +340,56 @@ package provide cookiejar \
     }
     method GetDomainListOnline {metaVar} {
 	upvar 1 $metaVar meta
-	namespace upvar [info object namespace [self class]] \
-	    domainlist url domaincache cache
-	lassign $cache when data
+	classvariable domainlist domaincache
+	lassign $domaincache when data
 	if {$when > [clock seconds] - 3600} {
 	    log debug "using cached value created at %s" \
 		[clock format $when -format {%Y%m%dT%H%M%SZ} -gmt 1]
 	    dict set meta retrievalDate $when
 	    return $data
 	}
-	log debug "loading domain list from %s" $url
+	log debug "loading domain list from %s" $domainlist
 	try {
 	    set when [clock seconds]
-	    set data [my HttpGet $url]
-	    set cache [list $when $data]
+	    set data [my HttpGet $domainlist]
+	    set domaincache [list $when $data]
 	    # TODO: Should we use the Last-Modified header instead?
 	    dict set meta retrievalDate $when
 	    return $data
 	} on error msg {
 	    log error "failed to fetch list of forbidden cookie domains from %s: %s" \
-		    $url $msg
+		    $domainlist $msg
 	    return {}
 	}
     }
     method GetDomainListOffline {metaVar} {
 	upvar 1 $metaVar meta
-	namespace upvar [info object namespace [self class]] \
-	    domainfile filename
-	log debug "loading domain list from %s" $filename
+	classvariable domainfile
+	log debug "loading domain list from %s" $domainfile
 	try {
-	    set f [open $filename]
+	    set f [open $domainfile]
 	    try {
-		if {[string match *.gz $filename]} {
+		if {[string match *.gz $domainfile]} {
 		    zlib push gunzip $f
 		}
 		fconfigure $f -encoding utf-8
-		dict set meta retrievalDate [file mtime $filename]
+		dict set meta retrievalDate [file mtime $domainfile]
 		return [read $f]
 	    } finally {
 		close $f
 	    }
 	} on error {msg opt} {
 	    log error "failed to read list of forbidden cookie domains from %s: %s" \
-		    $filename $msg
+		    $domainfile $msg
 	    return -options $opt $msg
 	}
     }
     method InitDomainList {} {
-	namespace upvar [info object namespace [self class]] \
-	    offline offline
+	classvariable offline
 	if {!$offline} {
 	    try {
 		set data [my GetDomainListOnline metadata]
-		if {[string length $data]} {
+		if {$data ne ""} {
 		    my InstallDomainData $data $metadata
 		    return
 		}
@@ -388,61 +401,52 @@ package provide cookiejar \
 	my InstallDomainData $data $metadata
     }
 
-    method InstallDomainData {data meta} {
+    # You should hold a transaction before calling this!
+    private method PurgeDomainList {} {
+	db eval {
+	    DELETE FROM domains;
+	    DELETE FROM forbiddenSuper;
+	    INSERT OR REPLACE INTO domainCacheMetadata
+		(id, retrievalDate, installDate)
+	    VALUES (1, -1, -1);
+	}
+    }
+
+    method InstallDomainData {data meta {purge false}} {
 	set n [db total_changes]
 	db transaction {
+	    if {$purge} {
+		my PurgeDomainList
+	    }
 	    foreach line [split $data "\n"] {
 		if {[string trim $line] eq ""} {
 		    continue
 		} elseif {[string match //* $line]} {
 		    continue
 		} elseif {[string match !* $line]} {
-		    set line [string range $line 1 end]
-		    set idna [string tolower [::tcl::idna encode $line]]
-		    set utf [::tcl::idna decode [string tolower $line]]
-		    db eval {
-			INSERT OR REPLACE INTO domains (domain, forbidden)
-			VALUES ($utf, 0);
-		    }
-		    if {$idna ne $utf} {
+		    foreach name [names [string range $line 1 end]] {
 			db eval {
 			    INSERT OR REPLACE INTO domains (domain, forbidden)
-			    VALUES ($idna, 0);
+			    VALUES ($utf, 0);
 			}
 		    }
-		} else {
-		    if {[string match {\*.*} $line]} {
-			set line [string range $line 2 end]
-			set idna [string tolower [::tcl::idna encode $line]]
-			set utf [::tcl::idna decode [string tolower $line]]
+		    continue
+		} elseif {[string match {\*.*} $line]} {
+		    set names [names [string range $line 2 end]]
+		    foreach name $names {
 			db eval {
 			    INSERT OR REPLACE INTO forbiddenSuper (domain)
 			    VALUES ($utf);
 			}
-			if {$idna ne $utf} {
-			    db eval {
-				INSERT OR REPLACE INTO forbiddenSuper (domain)
-				VALUES ($idna);
-			    }
-			}
-		    } else {
-			set idna [string tolower [::tcl::idna encode $line]]
-			set utf [::tcl::idna decode [string tolower $line]]
 		    }
+		} else {
+		    set names [names $line]
+		}
+		foreach name $names {
 		    db eval {
 			INSERT OR REPLACE INTO domains (domain, forbidden)
 			VALUES ($utf, 1);
 		    }
-		    if {$idna ne $utf} {
-			db eval {
-			    INSERT OR REPLACE INTO domains (domain, forbidden)
-			    VALUES ($idna, 1);
-			}
-		    }
-		}
-		if {$utf ne [::tcl::idna decode [string tolower $idna]]} {
-		    log warn "mismatch in IDNA handling for %s (%d, %s, %s)" \
-			    $idna $line $utf [::tcl::idna decode $idna]
 		}
 	    }
 
@@ -462,24 +466,14 @@ package provide cookiejar \
     # This forces the rebuild of the domain data, loading it from
     method forceLoadDomainData {} {
 	db transaction {
-	    db eval {
-		DELETE FROM domains;
-		DELETE FROM forbiddenSuper;
-		INSERT OR REPLACE INTO domainCacheMetadata
-		    (id, retrievalDate, installDate)
-		VALUES (1, -1, -1);
-	    }
+	    my PurgeDomainList
 	    my InitDomainList
 	}
     }
 
     destructor {
-	catch {
-	    after cancel $purgeTimer
-	}
-	catch {
-	    after cancel $refreshTimer
-	}
+	after cancel $purgeTimer
+	after cancel $refreshTimer
 	catch {
 	    db close
 	}
@@ -665,8 +659,7 @@ package provide cookiejar \
     }
 
     method PurgeCookies {} {
-	namespace upvar [info object namespace [self class]] \
-	    vacuumtrigger trigger  retainlimit retain
+	classvariable vacuumtrigger retainlimit
 	my PostponePurge
 	set now [clock seconds]
 	log debug "purging cookies that expired before %s" [clock format $now]
@@ -678,20 +671,20 @@ package provide cookiejar \
 	    db eval {
 		DELETE FROM persistentCookies WHERE id IN (
 		    SELECT id FROM persistentCookies ORDER BY lastuse ASC
-		    LIMIT -1 OFFSET $retain)
+		    LIMIT -1 OFFSET $retainlimit)
 	    }
 	    incr deletions [db changes]
 	    db eval {
 		DELETE FROM sessionCookies WHERE id IN (
 		    SELECT id FROM sessionCookies ORDER BY lastuse
-		    LIMIT -1 OFFSET $retain)
+		    LIMIT -1 OFFSET $retainlimit)
 	    }
 	    incr deletions [db changes]
 	}
 
 	# Once we've deleted a fair bit, vacuum the database. Must be done
 	# outside a transaction.
-	if {$deletions > $trigger} {
+	if {$deletions > $vacuumtrigger} {
 	    set deletions 0
 	    log debug "vacuuming cookie database"
 	    catch {
