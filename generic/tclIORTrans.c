81149520ffb39af367dd703fc1a7acfd484c880c
jan.nijtmans <nijtmans@users.sourceforge.net>
2025-09-26 15:41:50 +0000

    Further fix [https://core.tcl-lang.org/tk/tktview/832c2047bb|832c2047bb]: Windows: new compiler, new warnings...

--- generic/tclIORTrans.c
+++ generic/tclIORTrans.c
@@ -352,7 +352,7 @@ TCL_DECLARE_MUTEX(rtForwardMutex)
  */
 
 static void		ForwardOpToOwnerThread(ReflectedTransform *rtPtr,
-			    ForwardedOperation op, const void *param);
+			    ForwardedOperation op, void *param);
 static int		ForwardProc(Tcl_Event *evPtr, int mask);
 static void		SrcExitProc(void *clientData);
 
@@ -2345,7 +2345,7 @@ static void
 ForwardOpToOwnerThread(
     ReflectedTransform *rtPtr,	/* Channel instance */
     ForwardedOperation op,	/* Forwarded driver operation */
-    const void *param)		/* Arguments */
+    void *param)		/* Arguments */
 {
     Tcl_ThreadId dst = rtPtr->thread;
     ForwardingEvent *evPtr;
