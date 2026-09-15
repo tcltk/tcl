a14edf8a397382929e4d5b51400c991ed504bb1b
jan.nijtmans <nijtmans@users.sourceforge.net>
2026-09-15 20:13:25 +0000

    (cherry-pick) Additional fix for [282495fbbd]: valgrind reports uninitialized value problem in oo-15.10

--- generic/tclProc.c
+++ generic/tclProc.c
@@ -2164,7 +2164,7 @@ TclProcCleanupProc(
 	localPtr = nextPtr;
     }
 
-    if ( procPtr->cmdPtr && (procPtr->flags & PROC_CMD_OWNED)
+    if ( (procPtr->flags & PROC_CMD_OWNED) && procPtr->cmdPtr
       && procPtr->cmdPtr->refCount-- <= 1
     ) {
 	/* cmdPtr owned by procPtr (lambda) */
